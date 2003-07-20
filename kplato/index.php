bdbbbad6138cfd4ebf0d9e9161ad89a4c944783e
howells <howells@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2003-07-20 19:45:14 +0000

    done
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@238209 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ kplato/index.php
@@ -0,0 +1,34 @@
+<?php
+  $page_title='KPlato';
+  $site_root='../';
+  include("koffice.inc");
+  include("../variables.inc");
+  include("header.inc");
+?>
+<?php
+  $appinfo = new AppInfo("KPlato");
+  //$appinfo->setIcon( "pics/hi48-app-kword.png", "48", "48");
+  $appinfo->setVersion($kplatoversion);
+  $appinfo->setLicense("lgpl");
+  $appinfo->setCopyright("2001", "the KPlato Team");
+  $appinfo->addAuthor("Thomas Zander", "zander@kde.org");
+  $appinfo->addAuthor("Jim Sabatke", "jsabatke@execpc.com");
+  $appinfo->addAuthor("Bo Thorsten", "bo@suse.de");
+  $appinfo->show();
+?>
+
+<h2>Description</h2>
+
+      <p><b>Description:</b><br />
+         KPlato is a project management application, allowing for the
+         planning and scheduling of projects.  It is in the very early
+         stages of development.
+      </p>
+
+      <p><b>Mailing List:</b><br />
+         The KPlato mailing list is at <a href="mailto:kplato@kde.org">kplato@kde.org</a><br />
+         Subscription information can be found <a href="http://master.kde.org/mailman/listinfo/kplato">here</a>.<br />
+         Online archives at: <a href="http://lists.kde.org/?l=kplato&r=1&w=2">lists.kde.org</a>
+      </p>
+
+<?php include("footer.inc"); ?>
---
+++ kplato/status.php
@@ -0,0 +1,35 @@
+<?php
+  $title='KPlato Status';
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+    <p>First a screenshot: <a href="basic-diag.png">Current</a>
+
+    <p>A list of TODO items we have decided will have to be present at least before an 1.0 release.
+    <ul>
+     <li> Requirments specification (DONE)</li>
+     <li> Data structuring and class creation (DONE)</li>
+     <li> Loading/saving the data structure. (Framework present, file gets created/loaded, but is quite empty)</li>
+     <li> Basic GUI (DONE)</li>
+     <li> Implementing basic view (DONE)</li>
+     <li> Implementing basic task/subtask etc. creation/deletion via the GUI</li>
+     <li> Implementing Gantt view (Calculation is DONE, but not debugged, view is TODO)</li>
+     <li> Implementing task editor-dialog</li>
+     <li> Implementing project editor-dialog</li>
+     <li> Implementing project editor-dialog</li>
+     <li> Implementing milestone editor-dialog</li>
+     <li> Implementing zooming support for the views</li>
+     <li> Merge <a href="http://www.suse.de/~freitag/taskjuggler/">taskjuggler</a> and KPlato</li>
+     <li> etc.</li>
+    </ul>
+
+    <p>
+    As we have chosen to use the KOffice framework there are a number of things that are amazingly easy to do;</p>
+    <li>Printing is automatically done if zooming is allowed (we can do without zooming, but that makes the output look ugly)</li>
+    <li>We can place any other KOffice document inside our view, and visa-versa. A Gantt in a KWord doc, a spreadsheet in a milestone...</li>
+    <li>Saving documents is done via QDomElements and the rest is allready in place.</li>
+    <li>Internationalisation is done automatically since it's a KDE project</li>
+
+<?php include("footer.inc"); ?>
