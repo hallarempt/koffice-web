8216260c3dd48577ca0dd2b1406ec126efff2d10
howells <howells@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2003-07-20 15:33:10 +0000

    done
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@238179 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ karbon/index.php
@@ -0,0 +1,29 @@
+<?php
+  $page_title="Karbon14";
+  $site_root='../';
+  include("../variables.inc");
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+<?php
+  $appinfo = new AppInfo("Karbon14");
+  //$appinfo->setIcon( "pics/hi48-app-karbon.png", "48", "48");
+  $appinfo->setVersion($karbonversion);
+  $appinfo->setLicense("gpl");
+  $appinfo->setCopyright("1998", "the Karbon Team");
+  $appinfo->addAuthor("Rob Buis", "buis@kde.org");
+  $appinfo->addAuthor("Lenny Kudling", "kudling@kde.org");
+  $appinfo->addContributor("Rob Buis", "buis@kde.org");
+  $appinfo->addContributor("Lenny Kudling", "kudling@kde.org");
+  $appinfo->addContributor("Benoit Vautrin", "benoit.vautrin@free.fr");
+  $appinfo->addContributor("Tomislav Lukman", "tomislav.lukman@ck.hinet.hr");
+  $appinfo->show();
+?>
+
+<h2>Description</h2>
+
+<p>Karbon14 is the vector drawing application in KDE. It is anticipated that it will be included with KOffice 1.3, when it is released.</p>
+
+<?php include("footer.inc"); ?>
+
---
+++ karbon/screenshots.php
@@ -0,0 +1,19 @@
+<?php
+  $page_title="Karbon14 Screenshots";
+  $site_root='../';
+  include("header.inc");
+?>
+
+<?php
+$gallery = new ImageGallery("Karbon14 Screenshots");
+$gallery->addImage("pics/basicgradsthumb.png", "pics/basicgrads.png", "211", "202", "[Screenshot]", 0 , "Some gradients");
+$gallery->addImage("pics/insert_knotsthumb.png", "pics/inset_knots.png", "203", "200", "[Screenshot]", 0 , "Insertion of extra nodes while keeping the overall shape of the path");
+$gallery->startNewRow();
+$gallery->addImage("pics/epsimport.png", "pics/epsimport.png", "266", "255", "[Screenshot]", 0 , "An imported EPS files");
+$gallery->show();
+?>
+
+<p>More screenshots can be found at <a href="http://www.xs4all.nl/~rwlbuis/karbon/">this</a> site.</p>
+
+<?php include("footer.inc"); ?>
+
