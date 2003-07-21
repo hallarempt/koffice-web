f0e4b48c43ad58b8244e1397fee5d0bd6dc1b547
howells <howells@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2003-07-21 15:23:13 +0000

    404 handler
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@238351 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+include("classes/class_handler404.inc");
+$handler = new Handler404();
+$handler->add("/downloads", "/addons");
+$handler->add("/contact.phtml", "/contact");
+$handler->add("/kpresenter.html", "/kpresenter");
+$handler->add("/developers.php", "/people.php");
+$handler->add("/install-binaries.php", "/download");
+$handler->add("/install-source.php", "/download/source.php");
+$handler->add("/signatures.php", "/download/signatures.php");
+$handler->add("/support.php", "/support");
+$handler->add("/supporters.php", "/support/supporters.php");
+$handler->execute();
+?>
+
