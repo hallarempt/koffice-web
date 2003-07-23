636b8452976435f6f13457057155983b8bf253d0
howells <howells@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2003-07-23 09:29:00 +0000

    Fix typo
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@238733 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- karbon/screenshots.php
+++ karbon/screenshots.php
@@ -7,7 +7,7 @@
 <?php
 $gallery = new ImageGallery("Karbon14 Screenshots");
 $gallery->addImage("pics/basicgradsthumb.png", "pics/basicgrads.png", "211", "202", "[Screenshot]", 0 , "Some gradients");
-$gallery->addImage("pics/insert_knotsthumb.png", "pics/inset_knots.png", "203", "200", "[Screenshot]", 0 , "Insertion of extra nodes while keeping the overall shape of the path");
+$gallery->addImage("pics/insert_knotsthumb.png", "pics/insert_knots.png", "203", "200", "[Screenshot]", 0 , "Insertion of extra nodes while keeping the overall shape of the path");
 $gallery->startNewRow();
 $gallery->addImage("pics/epsimport.png", "pics/epsimport.png", "266", "255", "[Screenshot]", 0 , "An imported EPS files");
 $gallery->show();
