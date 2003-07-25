9cd4f68b7d7a531cd604b864b5f6b4d537b11a29
howells <howells@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2003-07-25 09:27:52 +0000

    Adding my script here in the hope that people more familiar with doxygen can improve it.
    If you modify this, let me know so I can copy this to the right place for it to be executed since I don't blindly execute scripts from CVS!
    
    
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@239042 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ developer/makekofficedocs.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+DIR="
+example
+filters
+interfaces
+karbon
+kchart
+kdgantt
+kexi
+kformula
+kivio
+koshell
+kplato
+kpresenter
+krita
+kspread
+kugar
+kword
+lib
+"
+
+SOURCEDIR=/home/howells/cvs/koffice
+DESTDIR=/home/howells/public_html/apidocs
+TARDIR=/home/howells/public_html
+
+cd $SOURCEDIR && cvs up
+
+for dir in $DIR
+do
+  echo "switching to $SOURCEDIR/$dir"
+  cd $SOURCEDIR/$dir
+  DOXYFILE="$SOURCEDIR/$dir/Doxyfile"
+  echo "copying Doxyfile.temp to $DOXYFILE"
+  cp $SOURCEDIR/Doxyfile.temp $DOXYFILE
+  echo "PROJECT_NAME           = \"$dir\"" >> $DOXYFILE
+  echo "INPUT                  = \"$SOURCEDIR/$dir\"" >> $DOXYFILE
+  echo "OUTPUT_DIRECTORY       = \"$DESTDIR/$dir\"" >> $DOXYFILE
+  doxygen $DOXYFILE
+done
+
+INDEX=$DESTDIR/index.html
+
+echo "<html>" > $INDEX
+echo "<head><title>KOffice API Documentation</title></head>" >> $INDEX
+echo "<body>" >> $INDEX
+echo "<h1>KOffice API Documenation</h2>" >> $INDEX
+echo "<p>Select a component to view the documentation for that component</p>" >> $INDEX
+echo "<ul>" >> $INDEX
+for dir in $DIR
+do
+  echo "<li><a href=\"$dir/html\">$dir</a></li>" >> $INDEX
+done
+echo "</ul>" >> $INDEX
+echo "</body>" >> $INDEX
+echo "</html>" >> $INDEX
+
+cd $TARDIR && tar cvjf kofficeapidocs.tar.bz2 apidocs
