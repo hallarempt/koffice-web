231612748477e2895b5e21d6fb0da30bf078a382
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2003-09-03 20:01:57 +0000

    Recommend RTF for saving
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@248145 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- filters/1.2/kword/msword97.php
+++ filters/1.2/kword/msword97.php
@@ -7,7 +7,12 @@
 
 <p>KWord can read all text from Microsoft Word files.  Basic text formatting
 (bold, italics, etc) are preserved.  Numbered headings, lists, built-in colors, tables, and internationalization support
-are nearly complete.</p><p>Many other features (headers, footers, embedded objects, etc) are not supported.</p><p>KWord can not write Microsoft Word 97/2000 files.</p>
+are nearly complete.</p>
+
+<p>Many other features (headers, footers, embedded objects, etc) are not supported.</p>
+
+<p>KWord can not write Microsoft Word 97/2000 files. It is recommended to save
+in RTF (Rich Text Format) instead.</p>
 
 <?php
    include("footer.inc");
