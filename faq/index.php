656c365ebd348a2221fb21bf79dcb47b6744556c
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2003-09-07 18:58:37 +0000

    A few more converted questions and answers
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@249165 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- faq/index.php
+++ faq/index.php
@@ -23,6 +23,7 @@
 <?php
 $faq = new FAQ();
 
+
 $faq->addSection("General Questions");
 
 $faq->addQuestion("What is KOffice?",
@@ -78,11 +79,145 @@ $faq->addQuestions("I would like to see application XYZ in KOffice?",
 "<p>KOffice may lack some applications that people normally consider to be
 Office application. Some of these applications are in other parts of KDE,
 like for example PIM (Personal Information Management). Some of other
-are only planned for future KOffice releases."
+are only planned for future KOffice releases.</p>"
 );
 
 // FAQ: What is the current state of &KOffice;?
+/*
+<qandaentry><question><para> What is the current state of &KOffice;?</para></question><answer>
+<para>
+&KOffice; has been released as 1.2. KOffice is released seperately
+from the rest of KDE, so KOffice releases are not in sync with those of KDE.
+</para>
+</answer></qandaentry>
+*/
+
 // FAQ: Is &KOffice; stable?
+/*
+<qandaentry><question><para> Is &KOffice; stable? </para></question><answer>
+<para>
+The release versions of &KOffice; are supposed to be quite stable. While we try to offer
+ as good a product as possible, &KOffice; is still a young project and will certainly
+ mature in the next releases, when some parts are planned to be rewritten or replaced.
+</para>
+</answer></qandaentry>
+*/
+
+// CHECK!
+$faq->addQuestions("Are you going to implement feature XYZ?",
+"<p>First of all: certainly many features should be implemented in the future. However,
+KOffice is mainly based on the work of volunteers and thus it often lacks the manpower
+it needs to address many issues. If you are a developer and you lack a feature,
+consider helping us and talk to us on the mailing lists. Many features can be implemented
+independently of the KOffice main code base by implementing so-called plugins or
+data tools. KOffice developers are always willing to help you if you want to get
+your hands dirty!</p>"
+);
+
+// FAQ: What do I need for using &KOffice;?
+/*
+<qandaentry><question><para> What do I need for using &KOffice;?</para></question><answer>
+<para>
+You should use the latest Qt library, i.e. 3.0.5 (see <ulink url="ftp://ftp.trolltech.com/qt/source/">
+ftp://ftp.trolltech.com/qt/source/</ulink>) and the latest <ulink url="http://www.kde.org">KDE
+ libraries</ulink> (eg. 3.0.3 for &KOffice; 1.2; the next &KOffice; release will require
+ KDE 3.1 and Qt 3.1). If you are compiling a development version from CVS, this information
+ is not up-to-date anymore; consult the <ulink url="http://lists.kde.org">mailing list
+ archives</ulink> of the koffice-devel list for information on the needed libraries or ask
+ on the <ulink url="mailto:koffice at kde dot org">koffice</ulink> or
+ <ulink url="mailto:koffice-devel at kde dot org">koffice-devel</ulink> lists if you run into trouble.</para>
+</answer></qandaentry>
+*/
+
+$faq->addQuestions("Which license applies to KOffice?",
+"<p>KOffice is an open-source project and completely free. All licences are GPL-compatible, mostly
+GPL or LGPL version 2. Apart exceptions, libraries and filters are mostly LGPL, while applications are mostly GPL.</p>"
+);
+
+$faq->addQuestions("How do I report bugs?",
+"<p>Like for any KDE application, please report bugs to <a href=\"http://bugs.kde.org\">KDE Bugs.</a></p>
+<p>Please avoid bug reports with meaningless contents like: \"It does not work!\", \"KOffice is bad\",
+\"XYZ is much better than your KOffice\".</p>
+<p>A good bug report should first have a <em>title.</em> Try to give in a few keywords an overview of the problem.
+That makes it easier for the developers to find your bug again.</p>
+<p>Then please we need a <em>good description</em>
+of the bug. How can the bug be reproduced? Does it always happen? Does something very similar work without any problem?</p>
+<p>Then we need to about the <em>software</em> of your system. If you have only installed the packages of your
+distribution, then this point is not really important. But if you are using special software, like a non-gcc compiler,
+or some very new developement library, then please tell us in your bug report.</p>
+<p>If the bug is a message box that should not happen, then please tell us the <em>exact error message</em>.</p>
+<p>If the bug is triggered by the loading of a <em>file</em>, then please add the file to the bug report or
+even better a short example file that triggers the bug. (Please be careful that sending a file to KDE Bugs makes it
+public, so be careful not to send us files not meant for the public.</p>
+<p>Of course, if you are able to make a <em>patch</em> to fix the bug, it would be the optimum.</p>"
+);
+
+$faq->addQuestions("How do I report wishes?",
+"<p>You report wishes as you would do for bugs. Here too it is important to describe correctly the wish to
+avoid misunderstandings. If your wish comes from another application, please do not assume that the developers
+know that application. And please only <em>one</em> wish per report. However you can make as many reports as
+you want. One wish per report helps the developers to manage easilier the bugs and wishes.</p>"
+);
+
+$faq->addQuestions("Can I use KOffice on Windows?",
+"<p>There is on-going work of <a url=\"http://kde-cygwin.sourceforge.net\">KDE Cygwin project</a> to port
+KDE to MS Windows. It is also planned to port KOffice. Just keep in mind that this port is not done
+by the KOffice developers themselves, so please report bugs to the project.</p>"
+);
+
+
+$faq->addSection("How to get and install KOffice");
+
+// CHECK
+$faq->addQuestions("How do I get and compile the KOffice sources?",
+"<p>Please have a look at <a url=\"http://koffice.kde.org/install-source.phtml\">
+http://koffice.kde.org/install-source.phtml</a></p>"
+);
+
+// CHECK: URLs for anoncvs and cvs up
+$faq->addQuestions("I want to be on the bleeding edge. How can I get my sources up to date?",
+"<p>You need to checkout the module koffice of KDE's CVS either by anonymous CVS or by CVS UP.</p>
+<p>Please keep in mind that the versions in the development branch are not supposed to work stable!</p>"
+);
+
+
+$faq->addQuestions("How do I install &KOffice; if I'm not root?",
+"You've got to pass some directory where you've got write access (e.g. in your home directory)
+ as prefix to configure. So you should type something like: <code>./configure --prefix=/home/jdoe/foo/bar</code>
+ plus of course other options you might want to pass to configure. Then of course you
+ don't have to do <code>su -c "make install"</code>, but just <code>make install</code>.</p>"
+);
+
+// FAQ:  Where can I find information on compiling Qt or the KDE libs?
+// CHECK: linked Qt document is wrong!
+/*
+<qandaentry><question><para> Where can I find information on compiling Qt or the KDE libs?</para></question><answer>
+<para>
+For Qt, have a look at the file <filename>INSTALL</filename> in your Qt root directory.</para>
+<para>
+See
+<ulink url="http://www.kde.org/documentation/faq/index.html">http://www.kde.org/documentation/faq/index.html</ulink>
+ for details on KDE.</para>
+</answer></qandaentry>
+*/
+
+$faq->addQuestions("What binary distributions are there and where do I find them?",
+"<p>You can find up-to-date information on available binaries <a url=\"http://www.koffice.org/install-binaries.phtml\">here</a>.</p>
+<p>Please notice that KOffice only makes the source, binary distributions are made by third persons, even if they
+are distributed by KDE or by KOffice.</p>"
+);
+
+$faq->addQuestions("How do I install binary RPMs?",
+"<p>Type the following as root: <code>rpm -U koffice.rpm</code>. Please replace koffice.rpm
+ with the full path to and the name of your binary rpm.</p>"
+);
+
+$faq->addQuestions("Is KOffice available on CD?",
+"<p>Most linux distributions by now ship KOffice. So if you have an up-to-date
+ linux distribution, you probably are lucky.  I don't know of any other operating
+ system vendors who ship KOffice and I don't know about any separate CDs.
+ The KOffice project itself does not sell any CD-ROMs.</p>"
+);
 
 $faq->show();
 ?>
