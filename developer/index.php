ca67802f8215e93d7c5bcf51bfed659aa7cfefa4
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2003-10-10 22:48:22 +0000

    Do not forget to change the parent page or you do not get access to the children
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@257882 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- developer/index.php
+++ developer/index.php
@@ -5,6 +5,13 @@
   include("header.inc");
 ?>
    <p>This section provides links and useful information and resources for developers that are intending to work on KOffice.</p>
+
+   <h2>Filters</h2>
+   
+   <ul>
+   <li><a href="filters">Developing Filters</a></li>
+   </ul>
+   
    
    <h2>KOffice API Reference</h2>
    A programmers API Reference is available. It is generated three times a week from the KOffice source code, using KDoc. It can either be read online here, or read offline by downloading the tarball.</p>
@@ -16,10 +23,6 @@
     <ul>
     <li><a href="apidocs">Index</a></li>
     </ul>
-   <h2>Miscellaneous Links</h2>
-   <ul>
-   <li><a href="http://webcvs.kde.org/cgi-bin/cvsweb.cgi/~checkout~/koffice/filters/kword/libexport/DESIGN.html?content-type=text/html#import">KWord libexport mini-HOWTO</a></li>
-   </ul>
 
 <?php
    include("footer.inc");
