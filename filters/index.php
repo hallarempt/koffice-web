d5fc377b00888d686ad144ffca367c7cd02dc112
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2003-10-11 21:13:30 +0000

    Remove moved filter developement files
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@258092 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- filters/index.php
+++ filters/index.php
@@ -9,19 +9,15 @@
     <p>Welcome to the KOffice Filter Information Page.</p>
     <p>Various filters are available for most of the KOffice applications.</p>
    
-    <h2>Information for Users</h2>
-
     <p><a href="1.2/">Please click here for the filter status</a>
-        (currently of KOffice 1.2)!
+        (currently of KOffice 1.3 Beta)!
     </p>
+    
+    <p>(The filter status for KOffice 1.2 is not available anymore, as there
+    were too many errors in it.)</p>
 
-    <h2>Information for Developers</h2>
-    <ul>
-        <li><a href="status.php">Status of the developer version</a></li>
-        <li><a href="faq.php">Filter FAQ (also known as "How to write a filter?")</a></li>
-        <li><a href="useful.php">Useful links</a></li>
-        <li><a href="todo.php">Missing Filters</a></li>
-    </ul>
+    <p>(<a href="../developer/filters">The developer information about the
+    filters were moved.</a>)</p>
    
 <?php
     include("footer.inc");
