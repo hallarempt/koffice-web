cef5eeac86e21d9721c5ace4e97d3972d12ac3ad
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2003-10-20 19:22:19 +0000

    Move the apidoc out of the index, even if we cannot move it out of the directory.
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@260558 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ developer/apidocmain.php
@@ -0,0 +1,19 @@
+<?php
+  $page_title="KOffice API Reference";
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+   A programmers API Reference is available. It is generated from the KOffice source code, using Doxygen. It can either be read online here, or read offline by downloading the tarball.</p>
+   <h2>Download the tarball</h2>
+   <ul>
+   <li><a href="kofficeapidocs.tar.bz2">Download the tarball</a> <?php niceFileSize("./kofficeapidocs.tar.bz2"); ?></li>
+   </ul>
+   <h2>Read online</h2>
+    <ul>
+    <li><a href="apidocs">Index</a></li>
+    </ul>
+
+<?php
+   include("footer.inc");
+?>
--- developer/index.php
+++ developer/index.php
@@ -4,26 +4,15 @@
   include("koffice.inc");
   include("header.inc");
 ?>
-   <p>This section provides links and useful information and resources for developers that are intending to work on KOffice.</p>
+   <p>This section provides useful links, information and resources for developers that are intending to work on KOffice.</p>
 
    <h2>Resources</h2>
    
    <ul>
-   <li><a href="generalinfo">General Developer Information</a></li>
-   <li><a href="filters">Developing Filters</a></li>
+     <li><a href="generalinfo">General developer information</a></li>
+     <li><a href="filters">Developing filters</a></li>
+     <li><a href="apidocmain.php">KOffice API reference</a></li>
    </ul>
-   
-   
-   <h2>KOffice API Reference</h2>
-   A programmers API Reference is available. It is generated from the KOffice source code, using Doxygen. It can either be read online here, or read offline by downloading the tarball.</p>
-   <h3>Download the tarball</h3>
-   <ul>
-   <li><a href="kofficeapidocs.tar.bz2">Download the tarball</a> <?php niceFileSize("./kofficeapidocs.tar.bz2"); ?></li>
-   </ul>
-   <h3>Read online</h3>
-    <ul>
-    <li><a href="apidocs">Index</a></li>
-    </ul>
 
 <?php
  kde_general_news("./news.rdf", 5, true);
