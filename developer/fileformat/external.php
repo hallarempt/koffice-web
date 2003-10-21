8f9e77353acc337a789327f507e962f653fe0450
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2003-10-21 13:45:49 +0000

    Fix the exchange between the two files
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@260752 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- developer/fileformat/external.php
+++ developer/fileformat/external.php
@@ -4,9 +4,19 @@
   include("koffice.inc");
   include("header.inc");
 ?>
-   <ul>
-     <li><a href="external.php">External Links</a></li>
-   </ul>
+
+  <h2>Documentation</h2>
+    <ul>
+      <li><a href="http://w3c.org">W3C</a> (XHTML, CSS, XPath, XSL, SVG...)</li>
+      <li><a href="http://xml.openoffice.org">OpenOffice's File Format Site</a></li>
+      <!-- TODO: direct link to OO's XML description PDF file -->
+    </ul>
+
+  <h2>Tools</h2>
+    <ul>
+      <li><a href="http://www.alphaworks.ibm.com/tech/xmldiffmerge">XML diffmerge</a></li>
+      <li><a href="http://www.alphaworks.ibm.com/tech/xmltreediff">XML treediff</a></li>
+    </ul>
 
 <?php
    include("footer.inc");
