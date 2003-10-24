c0658eac6029bae85cf6d33eb8798085219202f8
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2003-10-24 10:16:03 +0000

    - Change the text about the licenses - Use the name KDE, not K destop Environment or KDE project - Other small changes
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@261550 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- info/index.php
+++ info/index.php
@@ -6,7 +6,7 @@
 ?>
 
     <p><b>KOffice</b> is an integrated office suite for <a
-    href="http://www.kde.org">KDE</a>, the K Desktop Environment.</p>
+    href="http://www.kde.org">KDE</a>.</p>
 
     <p>KOffice features a full set of applications
     which work together seamlessly to provide the best user experience 
@@ -15,14 +15,13 @@
 
     <p>KOffice components work together. Every program in the KOffice
     suite is a component which adds more functionality to the whole.
-    You can embed any KOffice component in any other KOffice component, 
-    this is realised using the revolutionary KParts object model from the
-    <a href="http://www.kde.org">KDE</a> project. </p>
+    You can embed any KOffice component in any other KOffice component,
+    this is realised using the KParts object model from
+    <a href="http://www.kde.org">KDE</a>. </p>
 
-    <p><b>KOffice</b>, like the whole <a
-    href="http://www.kde.org">KDE</a> project, is a free project
-    which is released under open source licenses, e.g. GPL and
-    Artistic License.</p>
+    <p><b>KOffice</b>, like <a href="http://www.kde.org">KDE</a>, is a free project
+    which is released under GPL-compatible open source licenses, e.g. GPL,
+    LGPL and BSD.</p>
 
     <p>KOffice stays in active development. This means that KOffice
     is continually being improved, based on reports from users,
@@ -31,16 +30,17 @@
     applications. Some components are more complete than others,
     but many KOffice applications are already being used for
     productive professional work. If you want to make KOffice better, or
-    just implement that one little feature you really need, don't 
-    hesitate to jump in and we will happily make your changes part 
-    of the next release for everyone to use and see. 
+    just implement that one little feature you really need, don't
+    hesitate to jump in and we will happily make your changes part
+    of the next release for everyone to use and see.
     </p>
 
     <p>If you want to test the latest development versions of
     KOffice, you can download daily sources and install them. For
     instructions go <a href="/download/source.php">here</a>. Those
     parts of KOffice that are in the current release are also available
-    in pre-packaged binary form and do not need to be compiled from
+    in  <a href="/download/">pre-packaged binary form</a>
+    and do not need to be compiled from
     source, unless you want the very latest versions.</p>
 
     <p>If you have problems compiling KOffice, please look at the
