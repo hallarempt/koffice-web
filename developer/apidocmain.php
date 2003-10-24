c276700d496af8ccfdb81dfb1466962ac2aca30c
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2003-10-24 14:56:54 +0000

    CVS_SILENT: wrong file selected
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@261623 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- developer/apidocmain.php
+++ developer/apidocmain.php
@@ -4,13 +4,15 @@
   include("koffice.inc");
   include("header.inc");
 ?>
+    <!-- NOTE: the directory apidocs and kofficeapidocs.tar.bz2 are not in the CVS;
+      they are generated directly for the Web server. -->
    A programmers API Reference is available. It is generated from the KOffice source code, using Doxygen. It can either be read online here, or read offline by downloading the tarball.</p>
    <h2>Download the tarball</h2>
    <ul>
    <li><a href="kofficeapidocs.tar.bz2">Download the tarball</a>
      <?php
        niceFileSize("./kofficeapidocs.tar.bz2");
-       echo date (" (Y-m-d)", filemtime(__FILE__));
+       echo date (" (Y-m-d)", filemtime("./kofficeapidocs.tar.bz2"));
      ?>
    </li>
    </ul>
