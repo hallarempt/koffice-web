130ca5b2810e1da8fd0770ef54907643585800f5
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2003-12-20 16:22:45 +0000

    Add another screenshot (classic look, picture, document structure) (Also this is a test for Adam7-interlaced PNGs on the web site.)
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@274022 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- kword/screenshots.php
+++ kword/screenshots.php
@@ -7,7 +7,8 @@
 
 <?php
 $gallery = new ImageGallery("KWord Screenshots");
-$gallery->addImage("pics/kword_pt_cnc_sm.png", "pics/kword_pt_cnc.png", "180", "137", "[Screenshot KWord 1.3 Brazilian]", 0 , "KWord 1.3 in Brazilian Portuguese ");
+$gallery->addImage("pics/kword_pt_cnc_sm.png", "pics/kword_pt_cnc.png", "180", "137", "[Screenshot KWord 1.3 Brazilian]", 0 , "KWord 1.3 in Brazilian Portuguese");
+$gallery->addImage("pics/kword_classic_picture_sm.png", "pics/kword_classic_picture.png", "150", "106", "[Screenshot KWord 1.3 Classic Look, with Picture]", 0 , "KWord 1.3 in classic look with picture");
 $gallery->addImage("pics/kword-arabic_sm.png", "pics/kword-arabic.png", "191", "145", "[Screenshot KWord 1.2 Arabic]", 0 , "KWord 1.2 can be used for bi-directional texts like Arabic (05/02)");
 $gallery->startNewRow();
 $gallery->show();
