2935c1f3b6106207913ff6bde8f95f14bfad33b7
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2003-12-27 21:21:48 +0000

    Include newest KSpread snapshot
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@275100 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- kspread/screenshots.php
+++ kspread/screenshots.php
@@ -7,6 +7,8 @@
 
 <?php
 $gallery = new ImageGallery("KSpread Screenshots");
+$gallery->addImage("pics/kspread_simple_sm.png", "pics/kspread_simple.png", "180", "135", "Screenshot KSpread 1.3", 0, "KSpread 1.3 with function dialog (12/2003)");
+$gallery->startNewRow();
 $gallery->addImage("pics/kspread3_sm.png", "pics/kspread3.png", "180", "160", "[Screenshot]", 0, "Simple spread sheet calculations (03/01)");
 $gallery->startNewRow();
 $gallery->addImage("pics/kspread2_sm.png", "pics/kspread2.png", "184", "190", "[Screenshot]", 0, "A table, the border dialog</a> (12/02)");
