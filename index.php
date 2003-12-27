1cc3ba7f8c1df91ebb492fbb961550dd81c38a72
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2003-12-27 21:07:22 +0000

    - fix links (.php instead of .phtml) - do not open a new window when cklicking on Kugar - add "alt" comments to the application logos - trying to increase $site_menu
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@275097 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- index.php
+++ index.php
@@ -1,61 +1,61 @@
 <?php
   $page_title = "KOffice - Integrated Office Suite";
   $site_root = "./";
-  $site_menus = 2;
+  $site_menus = 3;
   include("koffice.inc");
   include("header.inc");
 ?>
 
     <p><b>KOffice</b> is a free, integrated office suite for <a href="http://www.kde.org">KDE</a>, the K Desktop Environment.</p>
 
-    <p><b>KOffice <?php echo $currentversion ?></b> is the current official KOffice release. Please see the <a class="sidebar" href="/releases/<?php echo $currentversion; ?>-release.phtml">release notes</a> for further details.</p>
+    <p><b>KOffice <?php echo $currentversion ?></b> is the current official KOffice release. Please see the <a class="sidebar" href="/releases/<?php echo $currentversion; ?>-release.php">release notes</a> for further details.</p>
     
     <p>There is also an unstable, testing version <?php echo $develversion ?> which you may try out. See the
-    <a class="sidebar" href="/releases/<?php echo $develversion ?>-release.phtml">notes</a> for this release.</p>
+    <a class="sidebar" href="/releases/<?php echo $develversion ?>-release.php">notes</a> for this release.</p>
 
     <p>In the KOffice suite the following parts are being developed:</p>
 
     <table width="90%">
     <tr>
-    <td><a href="kword/"><img src="pics/hi32-app-kword.png" alt="" width="32" height="32" border="0" /></a></td>
+    <td><a href="kword/"><img src="pics/hi32-app-kword.png" alt="KWord logo" width="32" height="32" border="0" /></a></td>
     <td><b><a href="kword/">KWord</a></b> - A frame-based word processor capable of professional standard documents</td>
     </tr>
     <tr>
-    <td><a href="kspread/"><img src="pics/hi32-app-kspread.png" alt="" width="32" height="32" border="0" /></a></td>
+    <td><a href="kspread/"><img src="pics/hi32-app-kspread.png" alt="KSpread logo" width="32" height="32" border="0" /></a></td>
     <td><b><a href="kspread/">KSpread</a></b> - A powerful spreadsheet application.</td>
     </tr>
     <tr>
-    <td><a href="kpresenter/"><img src="pics/hi32-app-kpresenter.png" alt="" width="32" height="32" border="0" /></a></td>
+    <td><a href="kpresenter/"><img src="pics/hi32-app-kpresenter.png" alt="KPresenter logo" width="32" height="32" border="0" /></a></td>
     <td><b><a href="kpresenter/">KPresenter</a></b> - A full-featured presentation program.</td>
     </tr>
     <tr>
-    <td><a href="kivio/"><img src="pics/hi32-app-kivio.png" alt="" width="32" height="32" border="0" /></a></td>
+    <td><a href="kivio/"><img src="pics/hi32-app-kivio.png" alt="Kivio logo" width="32" height="32" border="0" /></a></td>
     <td><b><a href="kivio/">Kivio</a></b> - A <i>Visio&reg;</i>-style flowcharting application.</td>
     </tr>
     <tr>
-    <td><a href="karbon/"><img src="pics/hi32-app-karbon.png" alt="" width="32" height="32" border="0" /></a></td>
+    <td><a href="karbon/"><img src="pics/hi32-app-karbon.png" alt="Karbon14 logo" width="32" height="32" border="0" /></a></td>
     <td><b><a href="karbon/">Karbon14</a></b> - A vector drawing application.</td>
     </tr>
     <tr>
-    <td><a href="krita/"><img src="pics/hi32-app-krita.png" alt="" width="32" height="32" border="0" /></a></td>
+    <td><a href="krita/"><img src="pics/hi32-app-krita.png" alt="Krita logo" width="32" height="32" border="0" /></a></td>
     <td><b><a href="krita/">Krita</a></b> - A pixel-based image manipulation program like <i>The GIMP</i> or <i>Adobe&copy; Photoshop&reg;.</i></td>
     </tr>
     <tr>
-    <td><a href="kugar/"><img src="pics/hi32-app-kugar.png" alt="" width="32" height="32" border="0" /></a></td>
-    <td><b><a href="kugar/" target="_new">Kugar</a></b> - A tool for generating business quality reports.</td>
+    <td><a href="kugar/"><img src="pics/hi32-app-kugar.png" alt="Kugar logo" width="32" height="32" border="0" /></a></td>
+    <td><b><a href="kugar/">Kugar</a></b> - A tool for generating business quality reports.</td>
     </tr>
     <tr>
-    <td><a href="kchart/"><img src="pics/hi32-app-kchart.png" alt="" width="32" height="32" border="0" /></a></td>
+    <td><a href="kchart/"><img src="pics/hi32-app-kchart.png" alt="KChart logo" width="32" height="32" border="0" /></a></td>
     <td><b><a href="kchart">KChart</a></b> - An integrated graph and chart drawing tool.</td>
     </tr>
 
     <tr>
-    <td><a href="kformula/"><img src="pics/hi32-app-kformula.png" alt="" width="32" height="32" border="0" /></a></td>
+    <td><a href="kformula/"><img src="pics/hi32-app-kformula.png" alt="KFormula logo" width="32" height="32" border="0" /></a></td>
     <td><b><a href="kformula">KFormula</a></b> - A powerful formula editor.</td>
     </tr>
 
     <tr>
-    <td><a href="kexi/"><img src="pics/cr32-app-kexi.png" alt="" width="32" height="32" border="0" /></a></td>
+    <td><a href="kexi/"><img src="pics/cr32-app-kexi.png" alt="Kexi logo" width="32" height="32" border="0" /></a></td>
     <td><b><a href="kexi/">Kexi</a></b> - Integrated Database Environment Access.</td>
     </tr>
 
