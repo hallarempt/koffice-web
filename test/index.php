614fe96187d4e2e3dae3a2161773677a6f9bdfd1
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-01-06 15:18:29 +0000

    I18N: try to use gettext on Web pages (TEST)
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@277281 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ test/index.php
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>I18N Test</title>
+ </head>
+ <body>
+  <p>Language: <?php echo $lang; ?><br /></p>
+  <?php
+    setlocale(LC_ALL, $lang);
+    bindtextdomain("index", ".");
+    textdomain("index");
+  ?>
+  <p>Color: <?php echo _("Color"); ?></p>
+  <p>Man: <?php echo _("Man"); ?></p>
+  <p>Woman: <?php echo _("Woman"); ?></p>
+  <p>Computer: <?php echo _("Computer"); ?></p>
+ </body>  
+</html>
