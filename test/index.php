cc76c3413711b7502f2efe7df25491b19dff3f3d
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-01-07 10:39:52 +0000

    Second try: - put the XML declaration into PHP code - get the GET variables indirectly (as a server would probably do not support the direct way.)
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@277520 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- test/index.php
+++ test/index.php
@@ -1,10 +1,11 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?php echo("&lt;?xml version=\"1.0\" encoding=\"UTF-8\"?&gt;"); ?>
 <html>
  <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <title>I18N Test</title>
  </head>
  <body>
+  <?php $lang=$HTTP_GET_VARS["lang"]; ?>
   <p>Language: <?php echo $lang; ?><br /></p>
   <?php
     setlocale(LC_ALL, $lang);
