2343ee612972d0712279bcc41693904b7ae59a20
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-01-24 17:56:56 +0000

    New small pictures (previews), now all of 144 pixel height
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@282289 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- kword/screenshots.php
+++ kword/screenshots.php
@@ -6,12 +6,15 @@
 ?>
 
 <?php
+
+# Use height 144 pixels to have an uniform look
+
 $gallery = new ImageGallery("KWord Screenshots");
-$gallery->addImage("pics/kword_pt_cnc_sm.png", "pics/kword_pt_cnc.png", "180", "137", "[Screenshot KWord 1.3 Brazilian]", 0 , "KWord 1.3 in Brazilian Portuguese");
-$gallery->addImage("pics/kword_classic_picture_sm.png", "pics/kword_classic_picture.png", "150", "106", "[Screenshot KWord 1.3 Classic Look, with Picture]", 0 , "KWord 1.3 in classic look with picture");
+$gallery->addImage("pics/kword_pt_cnc_sm.png", "pics/kword_pt_cnc.png", "189", "144", "[Screenshot KWord 1.3 Brazilian]", 0 , "KWord 1.3 in Brazilian Portuguese");
+$gallery->addImage("pics/kword_classic_picture_sm.png", "pics/kword_classic_picture.png", "204", "144", "[Screenshot KWord 1.3 Classic Look, with Picture]", 0 , "KWord 1.3 in classic look with picture");
 $gallery->startNewRow();
-$gallery->addImage("pics/kword_kde31_it_sm.png", "pics/kword_kde31_it.png", "255", "180", "[Screenshot KWord 1.3 Italian]", 0 , "KWord 1.3 in Italian");
-$gallery->addImage("pics/kword-arabic_sm.png", "pics/kword-arabic.png", "191", "145", "[Screenshot KWord 1.2 Arabic]", 0 , "KWord 1.2 can be used for bi-directional texts like Arabic (05/02)");
+$gallery->addImage("pics/kword_kde31_it_sm.png", "pics/kword_kde31_it.png", "204", "144", "[Screenshot KWord 1.3 Italian]", 0 , "KWord 1.3 in Italian");
+$gallery->addImage("pics/kword-arabic_sm.png", "pics/kword-arabic.png", "189", "144", "[Screenshot KWord 1.2 Arabic]", 0 , "KWord 1.2 can be used for bi-directional texts like Arabic (05/02)");
 $gallery->startNewRow();
 $gallery->show();
 ?>
