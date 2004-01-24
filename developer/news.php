1c72b97d3c0bb620858081b49b8560461d30ce27
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-01-24 19:46:35 +0000

    Add news about preparing 1.3 changelog
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@282318 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- developer/news.php
+++ developer/news.php
@@ -8,8 +8,14 @@
   // Document
   include("./rss2.inc");
   kde_general_news_rss2("./news.rdf", 25, false);
-   
+?>
+
+<a href="http://feedvalidator.org/check?url=http://www.koffice.org/developer/news.rdf">
+ <img src="valid-rss.png" alt="[Valid RSS]" title="Validate my RSS feed" width="88" height="31" />
+</a>
+
+<?php
   // Footer
   include("footer.inc");
- ?>
+?>
 
