17187f00699755c5e65ed8d9cd0ee0714a1fe003
waba <waba@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-01-25 15:44:34 +0000

    Added additional info from beta1, THIS NEEDS UPDATING!!
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@282508 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- announcements/announce-1.3.php
+++ announcements/announce-1.3.php
@@ -8,6 +8,281 @@
 <p>
   KOffice 1.3 is expected soon, check back regularly!
 </p>
+<?php begin_under_construction(); ?>
+<h4>
+  Principal Improvements
+</h4>
+<p align="justify">
+  The principal improvements over KOffice 1.2 are:
+</p>
+    <u>KWord (Word Processor)</u>.
+    <ul type="disc">
+      <li>
+        <div align="justify">
+        support for automatic hyphenation.
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        a new MS Word import filter;
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        support for pictures, tables and footnotes in the
+        RTF export filter (which has been re-written);
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        support for pictures and footnotes in the
+        LaTeX export filter;
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        a new MS Write export filter and a re-written
+        MS Write import filter;
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        support for bookmarks;
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        text sorting;
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        new character attributes, such as word-by-word
+        underlining and relative font sizes; and
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        improved spell checking (selection only; ignore
+        words; etc.).
+        </div>
+      </li>
+    </ul>
+  <p />
+    <u>KPresenter (Presentation Tool)</u>.
+    <ul type="disc">
+      <li>
+        <div align="justify">
+        support for automatic hyphenation.
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        an improved slide transition dialog, featuring a
+        preview of the slide effect;
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        drawing images with alpha channels;
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        customizable printing of slide notes; and
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        improved HTML export as well as usability
+        enhancements to the HTML export wizard.
+        </div>
+      </li>
+    </ul>
+  <p />
+    <u>KSpread (Spreadsheet)</u>.
+    <ul type="disc">
+      <li>
+        <div align="justify">
+        over 100 new formulas (date/time, math, statistical,
+        financial and more);
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        an OpenCalc import and export filter;
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        a new LaTeX export filter;
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        enhancements to the CSV import and export options;
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        new date and time formats;
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        a subtotal dialog and formula; and
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        default cell styles.
+        </div>
+      </li>
+    </ul>
+  <p />
+    <u>Kugar / Kugar Designer (Business Reporting)</u>.
+    <ul type="disc">
+      <li>
+        <div align="justify">
+        redesigned to better integrate into the
+        KOffice environment;
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        enhanced handling of items in the designer
+        (cut / copy / paste; undo / redo; manual
+        z-ordering; etc.);
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        support for printing reports from the
+        command line / scripts; and
+        </div>
+      </li>
+      <li>
+        <div align="justify">
+        a DCOP interface.
+        </div>
+      </li>
+    </ul>
+<p align="justify">
+  A more complete
+  <a href="http://www.koffice.org/announcements/changelog-1.3.php">list
+  of changes</a> is available at the KOffice <a href="http://www.koffice.org/">web site</a>.
+</p>
+<h4>
+  Installing KOffice 1.3 Binary Packages
+</h4>
+<p align="justify">
+  <u>Binary Packages</u>.
+  Some Linux/UNIX OS vendors have kindly provided binary packages of
+  KOffice 1.3 for some versions of their distribution, and in other cases
+  community volunteers have done so.
+  Some of these binary packages are available for free download from KDE's
+  <a href="http://download.kde.org/unstable/koffice-1.2.90/">http</a> or
+  <a href="http://www.kde.org/mirrors/ftp.php">ftp</a> mirrors.
+  Additional binary packages, as well as updates to the packages now
+  available, may become available over the coming weeks.
+</p>
+<p align="justify">
+  Please note that the KDE Project makes these packages available from the
+  KDE web site as a convenience to KDE users.  The KDE Project is not
+  responsible for these packages as they are provided by third
+  parties - typically, but not always, the distributor of the relevant
+  distribution - using tools, compilers, library versions and quality
+  assurance procedures over which KDE exercises no control. If you
+  cannot find a binary package for your OS, or you are displeased with
+  the quality of binary packages available for your system, please read
+  the <a href="http://www.kde.org/download/packagepolicy.php">KDE Binary Package
+  Policy</a> and/or contact your OS vendor.
+</p>
+<p align="justify">
+  <u>Library Requirements / Options</u>.
+  The library requirements for a particular binary package vary with the
+  system on which the package was compiled.  Please bear in mind that
+  some binary packages may require a newer version of Qt and other libraries
+  than was shipped with the system (e.g., LinuxDistro X.Y
+  may have shipped with Qt-3.0.0 but the packages below may require
+  Qt-3.0.3).  For general library requirements for KDE, please see the text at
+  <a href="#source_code-library_requirements">Source Code - Library
+  Requirements</a> below.
+</p>
+<p align="justify">
+  <a name="package_locations"><u>Package Locations</u></a>.
+  At the time of this release, pre-compiled packages are available for:
+</p>
+
+<p>
+<ul>
+  <li><a href="http://download.kde.org/unstable/koffice-1.2.90/SuSE/">SuSE Linux</a></li>
+  <li><a href="http://download.kde.org/unstable/koffice-1.2.90/contrib/Slackware/">Slackware Linux</a></li>
+</ul>
+</p>
+
+<p align="justify">
+Please check the servers periodically for pre-compiled packages for other
+distributions.  More binary packages will become available over the
+coming days and weeks.
+</p>
+<h4>
+  Compiling KOffice 1.3
+</h4>
+<p align="justify">
+  <a name="source_code-library_requirements"></a><u>Library
+  Requirements</u>.
+  KOffice 1.3 requires the following libraries:
+</p>
+<ul>
+  <li>
+    <div align="justify">
+    kdelibs 3.1, which can be downloaded following the
+    <a href="http://www.kde.org/info/3.1.5.php">instructions</a>
+    at the KDE website (for more information on these kdelibs releases, please
+    see the <a href="http://www.kde.org/announcements/announce-3.1.5.php">KDE
+    3.1.5 announcement</a>);
+    </div>
+  </li>
+  <li>
+    <div align="justify">
+    Qt-3.1, which is available in source code from Trolltech as
+    <a href="ftp://ftp.trolltech.com/qt/source/qt-x11-free-3.1.0.tar.gz">qt-x11-free-3.1.0.tar.gz</a>
+    (<a href="ftp://ftp.trolltech.com/qt/source/qt-x11-free-3.1.2.tar.gz">qt-x11-free-3.1.2.tar.gz</a>
+    is recommended);
+    and
+    </div>
+  </li>
+  <li>
+    <div align="justify">
+    for reading help pages and other KOffice documentation,
+    <a href="http://xmlsoft.org/">libxml2</a> &gt;= 2.4.9 and
+    <a href="http://xmlsoft.org/XSLT/">libxslt</a> &gt;= 1.0.7.
+    </div>
+  </li>
+</ul>
+<p align="justify">
+  <u>Compiler Requirements</u>.
+  At a minimum gcc-2.95-* is required.
+</p>
+<p align="justify">
+  <a name="source_code"></a><u>Source Code</u>.
+  The complete source code for KOffice 1.3 is available for free download
+  via one of the KDE
+  <a href="http://download.kde.org/stable/koffice-1.3/src/">http</a>
+  or <a href="http://www.kde.org/ftpmirrors.html">ftp</a> mirrors.
+  <!--Additionally, source rpms are available for the following distributions:-->
+</p>
+<!-- INCLUDE PHP FILE HERE -->
+<p align="justify">
+  <u>Further Information</u>.
+  For further instructions on compiling and installing KOffice, please consult
+  the KOffice <a href="http://www.koffice.org/download/source.php">installation
+  instructions</a>.  For
+  problems with SRPMs, please contact the person listed in the applicable
+  .spec file.
+</p>
+<?php end_under_construction(); ?>
 <h4>
   Corporate KOffice Sponsors
 </h4>
