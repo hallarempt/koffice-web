8f6fe9044de3904836415509062218e287f8f6d9
waba <waba@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-01-25 15:27:33 +0000

    Place holder.
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@282506 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ announcements/announce-1.3.php
@@ -0,0 +1,168 @@
+<?php
+  $page_title ='KOffice 1.3 Announcement';
+  $site_root = '../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+<p>
+  KOffice 1.3 is expected soon, check back regularly!
+</p>
+<h4>
+  Corporate KOffice Sponsors
+</h4>
+<p align="justify">
+  Besides the valuable and excellent efforts by the
+  <a href="http://www.koffice.org/people.php">KOffice developers</a>
+  themselves, significant support for KOffice development has been provided by
+  <a href="http://www.mandrakesoft.com/">MandrakeSoft</a> (which sponsors
+  KOffice developer Laurent Montel),
+  <a href="http://www.thekompany.com/">theKompany.com</a> (which has
+  contributed Kivio and Kugar to KOffice)
+  <a href="http://www.klaralvdalens-datakonsult.se/">Klar&auml;lvdalens
+  Datakonsult AB</a> (which partially sponsors KOffice developer
+  David Faure and has contributed KChart to KOffice) and <a href="http://www.suse.com">SuSE AG</a>
+  which employs Luk&aacute;&scaron; Tinkl, the current KOffice release manager.
+  In addition, a large number of <a href="http://www.kde.org/donations.html">individual
+  sponsors</a> (<a href="http://www.kde.org/support/">donate</a>) have
+  provided significant support for KDE and KOffice.  Thanks!
+</p>
+<h4>
+  About KOffice/KDE
+</h4>
+<p align="justify">
+  KOffice is part of the KDE Project.
+  KDE is an independent project of hundreds of developers, translators,
+  artists and professionals worldwide collaborating over the Internet
+  to create and freely distribute a sophisticated, customizable and stable
+  desktop and office environment employing a flexible, component-based,
+  network-transparent architecture and offering an outstanding development
+  platform.  KDE provides a stable, mature desktop, a full, component-based
+  office suite (<a href="http://www.koffice.org/">KOffice</a>), a large
+  set of networking and administration tools and utilities, and an
+  efficient, intuitive development environment featuring the 
+  <a href="http://www.kde.org/awards/">excellent</a> IDE
+  <a href="http://www.kdevelop.org/">KDevelop</a>.  KDE is working proof
+  that the Open Source "Bazaar-style" software development model can yield
+  first-rate technologies on par with and superior to even the most complex
+  commercial software.
+</p>
+<p align="justify">
+  Please visit the KDE family of web sites for the
+  <a href="http://www.kde.org/info/faq.php">KDE FAQ</a>,
+  <a href="http://www.kde.org/screenshots/kde310shots.php">screenshots</a>,
+  <a href="http://www.koffice.org/">KOffice information</a> and
+  <a href="http://developer.kde.org/documentation/">developer
+  information</a>.
+  Much more <a href="http://www.kde.org/whatiskde/">information</a>
+  about KDE is available from KDE's
+  <a href="http://www.kde.org/family/">family of web sites</a>.
+</p>
+<hr>
+<p align="justify">
+  <font size="2">
+  <em>Release Coordinator</em>:  Thanks to Luk&aacute;&scaron;
+  Tinkl for his excellent services as release coordinator.
+  </font>
+</p>
+<p align="justify">
+  <font size="2">
+  <em>Trademarks Notices.</em>
+
+  KDE, K Desktop Environment and KOffice are trademarks of KDE e.V.
+  Incorporated.
+<!--
+  Adobe Illustrator and PostScript are registered trademarks of Adobe Systems
+
+  dBASE is a registered trademark of dBASE Inc.
+
+  Corel, Quattro Pro and WordPerfect are registered trademarks of Corel
+  Corporation or Corel Corporation Limited.
+-->
+  Linux is a registered trademark of Linus Torvalds.
+
+  Trolltech and Qt are trademarks of Trolltech AS.
+
+  UNIX is a registered trademark of The Open Group.
+
+  All other trademarks and copyrights referred to in this announcement are
+  the property of their respective owners.
+  </font>
+</p>
+<hr>
+<h4>Press Contacts</h2>
+<table cellpadding="10"><tr valign="top">
+<td>
+
+<b>Africa</b><br />
+Uwe Thiem<br />
+P.P.Box 30955<br />
+Windhoek<br />
+Namibia<br />
+Phone: +264 - 61 - 24 92 49<br />
+<a href="mailto:info-africa@kde.org">info-africa@kde.org</a><br />
+</td>
+
+<td>
+<b>Asia</b><br />
+Sirtaj S. Kang <br />
+C-324 Defence Colony <br />
+New Delhi <br />
+India 110024 <br />
+Phone: +91-981807-8372 <br />
+<a href="mailto:info-asia@kde.org">info-asia@kde.org</a>
+</td>
+
+</tr>
+<tr valign="top">
+
+<td>
+<b>Europe</b><br />
+Matthias Kalle Dalheimer<br />
+Rysktorp<br />
+S-683 92 Hagfors<br />
+Sweden<br />
+Phone: +46-563-540023<br />
+Fax: +46-563-540028<br />
+<a href="mailto:info-europe@kde.org">info-europe@kde.org</a>
+</td>
+
+<td>
+<b>North America</b><br />
+George Staikos <br />
+889 Bay St. #205 <br />
+Toronto, ON, M5S 3K5 <br />
+Canada<br />
+Phone: (416)-925-4030 <br />
+<a href="mailto:info-northamerica@kde.org">info-northamerica@kde.org</a><br />
+</td>
+
+</tr>
+
+<tr>
+<td>
+<b>Oceania</b><br />
+Hamish Rodda<br />
+11 Eucalyptus Road<br />
+Eltham VIC 3095<br />
+Australia<br />
+Phone: (+61)402 346684<br />
+<a href="mailto:info-oceania@kde.org">info-oceania@kde.org</a><br />
+</td>
+
+<td>
+<b>South America</b><br />
+Helio Chissini de Castro<br />
+R. Jos&eacute; de Alencar 120, apto 1906<br />
+Curitiba, PR 80050-240<br />
+Brazil<br />
+Phone: +55(41)262-0782 / +55(41)360-2670<br />
+<a href="mailto:info-southamerica@kde.org">info-southamerica@kde.org</a><br />
+</td>
+
+
+</tr></table>
+
+<?php
+  include("footer.inc");
+?>
