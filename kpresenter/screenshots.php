185f1452b6136bec6964fcfc7d7a56fd47950366
lukas <lukas@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-01-27 15:35:42 +0000

    fix the screenshots previews
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@283007 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- kpresenter/screenshots.php
+++ kpresenter/screenshots.php
@@ -13,13 +13,13 @@ $gallery->addImage("pics/kpresenter8_sm.png", "pics/kpresenter8.png", "190", "15
 $gallery->addImage("pics/kpresenter7_sm.png", "pics/kpresenter7.png", "199", "166", "[Screenshot]", 0 , "Balanced and unbalanced gradients");
 $gallery->startNewRow();
 $gallery->addImage("pics/kpresenter6_sm.png", "pics/kpresenter6.png", "160", "178", "[Screenshot]", 0 , "Assign effects and structure dialogs");
-$gallery->addImage("pics/kpresenter9_sm.png", "pics/kpresenter5.png", "203", "174", "[Screenshot]", 0 , "Headers and footers");
+$gallery->addImage("pics/kpresenter5_sm.png", "pics/kpresenter5.png", "203", "174", "[Screenshot]", 0 , "Headers and footers");
 $gallery->startNewRow();
-$gallery->addImage("pics/kpresenter9_sm.png", "pics/kpresenter4.png", "202", "167", "[Screenshot]", 0 , "New effects");
-$gallery->addImage("pics/kpresenter9_sm.png", "pics/kpresenter3.png", "191", "173", "[Screenshot]", 0 , "An embedded chart and rotated text");
+$gallery->addImage("pics/kpresenter4_sm.png", "pics/kpresenter4.png", "202", "167", "[Screenshot]", 0 , "New effects");
+$gallery->addImage("pics/kpresenter3_sm.png", "pics/kpresenter3.png", "191", "173", "[Screenshot]", 0 , "An embedded chart and rotated text");
 $gallery->startNewRow();
-$gallery->addImage("pics/kpresenter9_sm.png", "pics/kpresenter2.png", "208", "175", "[Screenshot]", 0 , "Presentation structure viewer");
-$gallery->addImage("pics/kpresenter9_sm.png", "pics/kpresenter1.png", "189", "169", "[Screenshot]", 0 , "Basic shot");
+$gallery->addImage("pics/kpresenter2_sm.png", "pics/kpresenter2.png", "208", "175", "[Screenshot]", 0 , "Presentation structure viewer");
+$gallery->addImage("pics/kpresenter1_sm.png", "pics/kpresenter1.png", "189", "169", "[Screenshot]", 0 , "Basic shot");
 $gallery->show();
 ?>
 
