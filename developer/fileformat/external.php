d7a6377cd8567fc6257ab670fe654fe68c4cce06
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-02-10 14:21:02 +0000

    Add 2 links: - DTD tutorial - a XML survey
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@286792 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- developer/fileformat/external.php
+++ developer/fileformat/external.php
@@ -5,18 +5,34 @@
   include("header.inc");
 ?>
 
-  <h2>Documentation</h2>
+  <h2>Specifications</h2>
+  <p>
     <ul>
       <li><a href="http://w3c.org">W3C</a> (XHTML, CSS, XPath, XSL, SVG...)</li>
       <li><a href="http://xml.openoffice.org">OpenOffice's File Format Site</a></li>
       <!-- TODO: direct link to OO's XML description PDF file -->
     </ul>
+  </p>
+
+  <h2>Tutorials</h2>
+  <p>
+    <ul>
+      <li><a href="http://www.zvon.org/xxl/DTDTutorial/General/book.html">DTD Tutorial</a></li>
+    </ul>
+  </p>
+
+  <h2>Overviews</h2>
+  <p>
+    <ul>
+      <li><a href="http://www-106.ibm.com/developerworks/xml/library/x-stand1.html">A survey of XML standards</a></li>
 
   <h2>Tools</h2>
+  <p>
     <ul>
       <li><a href="http://www.alphaworks.ibm.com/tech/xmldiffmerge">XML diffmerge</a></li>
       <li><a href="http://www.alphaworks.ibm.com/tech/xmltreediff">XML treediff</a></li>
     </ul>
+  </p>
 
 <?php
    include("footer.inc");
