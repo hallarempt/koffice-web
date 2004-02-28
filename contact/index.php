aeb5349ff4e9947dbe25124c0a35cf17d58aacf4
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-02-28 22:26:39 +0000

    Replace the mailing list information by a slightly modified version of the corresponding entry of the KOffice FAQ
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@292390 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- contact/index.php
+++ contact/index.php
@@ -5,24 +5,33 @@
   include("header.inc"); 
 ?> 
  
-      <h2><a name="bugs">Bug Reporting</a></h2> 
+<h2><a name="bugs">Bug Reporting</a></h2> 
        
-      <p> 
-      If you find a bug in KOffice, please go to the "Help" menu in the application, and choose "Report bug...". 
-      Alternatively, please go to the <a href="http://bugs.kde.org">KDE bugs database</a>. 
-      </p> 
+    <p> 
+        If you find a bug in KOffice, please go to the "Help" menu in the application, and choose "Report bug...". 
+        Alternatively, please go to the <a href="http://bugs.kde.org">KDE Bugs database</a>. 
+    </p> 
        
-      <h2><a name="lists">Mailing Lists</a></h2> 
-      <p> 
-      To subscribe to KOffice's Mailing lists, simply head over to the web interfaces for the 
-      <a href="http://mail.kde.org/mailman/listinfo/koffice">koffice</a> and 
-      <a href="http://mail.kde.org/mailman/listinfo/koffice-devel">koffice-devel</a> 
-      lists respectively. 
-      </p>
-      <p> 
-      There are also archives of the KOffice mailing list available. 
-      The list archives are <a href="http://lists.kde.org/?l=koffice&r=1&w=2">koffice</a> and 
-      <a href="http://lists.kde.org/?l=koffice-devel&r=1&w=2">koffice-devel</a>. koffice-devel is for developers only. 
-      </p> 
+<h2><a name="lists">Mailing Lists</a></h2> 
+     
+    <p>KOffice has two general mailing lists:</p>
+    <p>
+        <ul>
+            <li><a href=\"http://www.kde.org/mailinglists/index.php#koffice\">koffice mailing list</a></li>
+            <li><a href=\"http://www.kde.org/mailinglists/index.php#koffice-devel\">koffice-devel mailing list</a></li>
+        </ul>
+    </p>
+    <p>The koffice mailing list is mainly for the users of KOffice, the koffice-devel is mainly for the developers.
+    Please note that the write access of the koffice-devel mailing list is restricted.</p>
+    <p>As KOffice is part of KDE it uses some of KDE's mailing lists, for example
+        <a href=\"http://www.kde.org/mailinglists/index.php#kde-i18n-doc\">the kde-i18n-doc mailing list for the translators</a>.
+    </p>
+    <p>
+        Please note that for historical reasons,
+        <a href=\"http://www.kde.org/mailinglists/index.php#krita\">Krita</a> and
+        <a href=\"http://www.kde.org/mailinglists/index.php#kplato\">KPlato</a>
+        have their own mailing lists.
+    </p>
+    <p><a href=\"http://lists.kde.org\">Archives of the mailing lists are available.</a></p>
  
 <?php include("footer.inc"); ?> 
