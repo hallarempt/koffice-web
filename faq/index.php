b47817e673f41a938d3c32cb01403ee6ca2ff40d
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-02-28 18:17:15 +0000

    Link to the general KDE mailing list info page
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@292307 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- faq/index.php
+++ faq/index.php
@@ -41,20 +41,20 @@ please report them by using <a href=\"http://bugs.kde.org\">KDE Bugs</a>."
 
 // CHECK: Krita & KPlato: why the email address?
 $faq->addQuestion("Which mailing lists are used for KOffice?",
-"KOffice has two own mailing lists:</p>
+"KOffice has two general mailing lists:</p>
 <ul>
-<li><a href=\"mailto:koffice@kde,org\">koffice mailing list</a></li>
-<li><a href=\"mailto:koffice-devel@kde.org\">koffice-devel mailing list</a></li>
+<li><a href=\"http://www.kde.org/mailinglists/index.php#koffice\">koffice mailing list</a></li>
+<li><a href=\"http://www.kde.org/mailinglists/index.php#koffice-devel\">koffice-devel mailing list</a></li>
 </ul>
 <p>The koffice mailing list is mainly for the users of KOffice, the koffice-devel is mainly for the developers.
 Please note that the write access of the koffice-devel mailing list is restricted.</p>
-<p>As KOffice is part of KDE it uses some of KDE's mailing lists, for example for the translators.</p>
-<p>Information on the mailing lists can be obtained <a href=\"http://www.kde.org/mailinglists\">here</a>.
-The archives of mailing lists is <a href=\"http://lists.kde.org\">here.</a></p>
+<p>As KOffice is part of KDE it uses some of KDE's mailing lists, for example
+<a href=\"http://www.kde.org/mailinglists/index.php#kde-i18n-doc\">the kde-i18n-doc mailing list for the translators</a>.</p>
 <p>Please note that for historical reasons,
-<a href=\"mailto:kimageshop@kde.org\">Krita</a> and
-<a href=\"mailto:kplato@kde.org\">KPlato</a>
- have their own mailing lists."
+<a href=\"http://www.kde.org/mailinglists/index.php#krita\">Krita</a> and
+<a href=\"http://www.kde.org/mailinglists/index.php#kplato\">KPlato</a>
+have their own mailing lists.</p>
+<p><a href=\"http://lists.kde.org\">Archives of mailing lists are available.</a>"
 );
 
 // CHECK: Kexi, Kugar, KPlato
