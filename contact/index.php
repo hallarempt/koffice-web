b91b6015e22275bf27e89dff3405ec96dc1f8ec7
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-02-28 22:58:14 +0000

    Fix link
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@292400 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- contact/index.php
+++ contact/index.php
@@ -17,21 +17,21 @@
     <p>KOffice has two general mailing lists:</p>
     <p>
         <ul>
-            <li><a href=\"http://www.kde.org/mailinglists/index.php#koffice\">koffice mailing list</a></li>
-            <li><a href=\"http://www.kde.org/mailinglists/index.php#koffice-devel\">koffice-devel mailing list</a></li>
+            <li><a href="http://www.kde.org/mailinglists/index.php#koffice">koffice mailing list</a></li>
+            <li><a href="http://www.kde.org/mailinglists/index.php#koffice-devel">koffice-devel mailing list</a></li>
         </ul>
     </p>
     <p>The koffice mailing list is mainly for the users of KOffice, the koffice-devel is mainly for the developers.
     Please note that the write access of the koffice-devel mailing list is restricted.</p>
     <p>As KOffice is part of KDE it uses some of KDE's mailing lists, for example
-        <a href=\"http://www.kde.org/mailinglists/index.php#kde-i18n-doc\">the kde-i18n-doc mailing list for the translators</a>.
+        <a href="http://www.kde.org/mailinglists/index.php#kde-i18n-doc">the kde-i18n-doc mailing list for the translators</a>.
     </p>
     <p>
         Please note that for historical reasons,
-        <a href=\"http://www.kde.org/mailinglists/index.php#krita\">Krita</a> and
-        <a href=\"http://www.kde.org/mailinglists/index.php#kplato\">KPlato</a>
+        <a href="http://www.kde.org/mailinglists/index.php#krita">Krita</a> and
+        <a href="http://www.kde.org/mailinglists/index.php#kplato">KPlato</a>
         have their own mailing lists.
     </p>
-    <p><a href=\"http://lists.kde.org\">Archives of the mailing lists are available.</a></p>
+    <p><a href="http://lists.kde.org">Archives of the mailing lists are available.</a></p>
  
 <?php include("footer.inc"); ?> 
