82e2acdc26b9e11c1b43dbe6f708e14aefd09202
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-03-05 23:45:06 +0000

    Add the "Constructive Critics" page
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@293847 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ developer/constructive/index.php
@@ -0,0 +1,19 @@
+<?php
+  $page_title="Constructive Critics For KOffice";
+  $site_root='../../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+<p>This page lists interesting reports about KOffice that people have written.</p>
+
+<h2>Emails</h2>
+
+<ul>
+    <li><a href="http://lists.kde.org/?l=koffice-devel&m=103867692818163&w=2">Usecases of KOffice</a></li>
+    <li><a href="http://lists.kde.org/?l=koffice-devel&m=107835129123866&w=2">KWord vs Framemaker review</a></li>
+</ul>
+
+<?php
+   include("footer.inc");
+?>
