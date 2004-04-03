fd314cddcdf410e8ce5feb2a60dfaf1de5955372
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-04-03 15:32:38 +0000

    - Concentrate the call for help on only one page - Bring the real important points at the top - Allow also the koffice mailing list, e.g. if a user does not feel to be   a "real" programmer/developer. (However we wan such kind of people too.)
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@301004 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- getinvolved/index.php
+++ getinvolved/index.php
@@ -10,27 +10,32 @@
     to KOffice you could:</p>
 
     <ul>
-      <li><a href="doc-help.php">write documentation</a></li>
+      <li>help programming. This means development work on the main
+      applications, new applications, <a
+      href="/developer/filters/index.php">filters (desperately
+      needed!)</a>, plugins and datatools, more functions for
+      KSpread...</li>
+
+      <li>fix small bugs if you find them and send a patch.
+      See also the <a href="/developer/tasks">Task List</a>.</li>
+      
+      <li>write documentation. In this case, please
+      <a href="mailto:lauri@kde.org">contact Lauri Watts, the chief document writer of KDE</a>,
+      to discuss with her where to work.
+      (<a href="http://i18n.kde.org/doc/index-script.php">See also the documentation writing homepage</a>).
+      </li>
 
       <li><a href="http://i18n.kde.org">translate KOffice</a></li>
 
       <li><a href="http://artist.kde.org">draw graphics</a></li>
 
+      <!-- What is the status of this? -->
       <li><a href="http://users.skynet.be/kborrey/klipart/klipart.html">contribute artwork</a></li>
-
-      <li>help programming. This means development work on the main
-      applications, new applications, <a
-      href="/developer/filters/index.php">filters (desperately
-      needed!)</a>, plugins and datatools, more functions for
-      KSpread, ...</li>
-
-      <li>fix small bugs if you find them and send a patch.
-      See also the <a href="/developer/tasks">Task List</a>.</li>
       
       <li>help maintaining koffice.org. See the <a href="/developer/website/tasks.php">Web Task List</a>.
 
       <li>test KOffice extensively and send descriptive bug reports
-      via the "Help, Report Bug" mechanism in the programs.</li>
+      <a href="http://bugs.kde.org">via the "Help, Report Bug" mechanism</a> in the programs.</li>
 
       <li><a href="/support">donate money or hardware</a>
       Yes, this is needed in free software development, too,
@@ -39,9 +44,10 @@
     </ul>
 
     <p>If you want to help the KOffice developers (or even become
-    one) please <a href="http://www.kde.org/mailinglists/index.php#koffice-devel">subscribe to the
-    koffice-devel mailing list</a> so you know what is needed and we avoid
-    duplicating work. Please do not be angry if you do not get
+    one) please
+    <a href="http://www.kde.org/mailinglists/index.php#koffice-devel">subscribe to the koffice-devel mailing list</a>
+    (or use <a href="http://www.kde.org/mailinglists/index.php#koffice">the KOffice mailing list</a>)
+    so you know what is needed and we avoid duplicating work. Please do not be angry if you do not get
     instant replies - everybody has got a lot to do.</p>
 
 <?php
