25dda40d199ce62123e9f48b662b4f14533d1412
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-05-06 22:43:53 +0000

    XHTML 1.0 ( </li> is not optional )
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@309232 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- announcements/changelog-1.3.1.php
+++ announcements/changelog-1.3.1.php
@@ -13,76 +13,76 @@
 <h3><a name="koffice_libs">KOffice Libraries</a></h3>
 KOfficeCore:
 <ul>
-<li>Changed .desktop file setup so that changes to the K menu structure,
+<li>Changed .desktop file setup so that changes to the K menu structure,</li>
   to file-program associations, or other distribution customizations cannot
-  prevent KOffice applications from starting.
+  prevent KOffice applications from starting.</li>
 <li>Fixed parsing of number+unit, due to change in Qt 3.3. Fixes problems
-  when opening OOWriter documents in particular.
-<li>Don't show the filter dialog when generating thumbnails
-<li>Improved error handling to catch "partition full" problems.
+  when opening OOWriter documents in particular.</li>
+<li>Don't show the filter dialog when generating thumbnails</li>
+<li>Improved error handling to catch "partition full" problems.</li>
 </ul>
 
 KOfficeUI:
 <ul>
-<li>Fix overwriting of templates (#73537)
-<li>Fix loading icons when creating templates
-<li>Allow template icons up to 64x64 instead of 60x60 and scale better if too big
+<li>Fix overwriting of templates (#73537)</li>
+<li>Fix loading icons when creating templates</li>
+<li>Allow template icons up to 64x64 instead of 60x60 and scale better if too big</li>
 </ul>
 
 <h4>KoText library features (shared between KWord and KPresenter):</h4>
 <ul>
 <li>Date variables for printing-date, last-modified, and creation-date were
-  not loaded correctly.
-<li>Background spellchecking works again
-<li>Fixed crash during background spellchecking (refcounting of formats)
-<li>Fixed race condition during background spellchecking (leading to misplaced red underlines)
+  not loaded correctly.</li>
+<li>Background spellchecking works again</li>
+<li>Fixed crash during background spellchecking (refcounting of formats)</li>
+<li>Fixed race condition during background spellchecking (leading to misplaced red underlines)</li>
 <li>Repaint paragraph after bg spell checking, in all cases (errors or not),
-  otherwise newly-added-to-the-dictionary words were still red-underlined (#56506).
-<li>Fixed hyphenation checkbox not correctly set in font dialog
-<li>Fixed wrong hyphenation in many cases, due to wrong word sent to hyphenation engine
+  otherwise newly-added-to-the-dictionary words were still red-underlined (#56506).</li>
+<li>Fixed hyphenation checkbox not correctly set in font dialog</li>
+<li>Fixed wrong hyphenation in many cases, due to wrong word sent to hyphenation engine</li>
 <li>Font dialog usability: turn radiobuttons superscript/subscript into
-  checkboxes to make it possible to deactivate both.
+  checkboxes to make it possible to deactivate both.</li>
 <li>Usability in style dialog: when creating a new style, select it as the
-  'next style' for itself (#45868).
-<li>Allow decimal values in "multiple" (proportional) linespacing.
-<li>Fixed wrong left margin on multiline paragraphs with a counter.
+  'next style' for itself (#45868).</li>
+<li>Allow decimal values in "multiple" (proportional) linespacing.</li>
+<li>Fixed wrong left margin on multiline paragraphs with a counter.</li>
 </ul>
 
 <h3><a name="kword">KWord</a></h3>
 <ul>
-<li>Fixed "add bookmark" for the case where there's no selection
-<li>Keep bookmark valid after deleting its paragraph
-<li>Usability improvements in "select bookmark" dialog
-<li>Fixed drawing of borders for copied frames (e.g. header/footers)
-<li>Fixed "text doesn't immediately run around copied frame on new page" (#73819)
-<li>Fixed crash when activating text view mode and there's no text frameset (#64952)
-<li>Allow to save custom shortcuts for 'insert variable' actions
-<li>Fixed crash when undoing a paste action
-<li>Fixed crash related to tables and deleting frames
-<li>Fixed crash when pasting plain text over a selection (#78561)
-<li>Fixed wrong conversion of values in config dialog when switching units (#65972)
+<li>Fixed "add bookmark" for the case where there's no selection</li>
+<li>Keep bookmark valid after deleting its paragraph</li>
+<li>Usability improvements in "select bookmark" dialog</li>
+<li>Fixed drawing of borders for copied frames (e.g. header/footers)</li>
+<li>Fixed "text doesn't immediately run around copied frame on new page" (#73819)</li>
+<li>Fixed crash when activating text view mode and there's no text frameset (#64952)</li>
+<li>Allow to save custom shortcuts for 'insert variable' actions</li>
+<li>Fixed crash when undoing a paste action</li>
+<li>Fixed crash related to tables and deleting frames</li>
+<li>Fixed crash when pasting plain text over a selection (#78561)</li>
+<li>Fixed wrong conversion of values in config dialog when switching units (#65972)</li>
 </ul>
 
 
 <h3><a name="kpresenter">KPresenter</a></h3>
 <ul>
-<li>Fixed possible crash in presentation mode if not all slides are shown
-<li>Fixed wrong clipping of fullscreen presentation (#73943)
-<li>Fixed show the duration for the correct page if not all slides are shown
-<li>Fixed crash in infinite loop presentation mode when first slide is not shown
-<li>Fixed speed of automatic presentation (it was reversed) (#69260)
-<li>Fixed wrong positioning of text when using "Vertical Alignment" (#72224)
-<li>Fixed redrawing of lines in drawing mode in case of a repaint event
-<li>Save HTML Slideshow export settings does not add extension to filenames (#76287)
+<li>Fixed possible crash in presentation mode if not all slides are shown</li>
+<li>Fixed wrong clipping of fullscreen presentation (#73943)</li>
+<li>Fixed show the duration for the correct page if not all slides are shown</li>
+<li>Fixed crash in infinite loop presentation mode when first slide is not shown</li>
+<li>Fixed speed of automatic presentation (it was reversed) (#69260)</li>
+<li>Fixed wrong positioning of text when using "Vertical Alignment" (#72224)</li>
+<li>Fixed redrawing of lines in drawing mode in case of a repaint event</li>
+<li>Save HTML Slideshow export settings does not add extension to filenames (#76287)</li>
 <li>If a page is shown more than once during a presentation, add the time for
-  the duration instead of showing only the last duration.
+  the duration instead of showing only the last duration.</li>
 </ul>
 
 
 <h3><a name="kspread">KSpread</a></h3>
 <ul>
-<li>undo not working after deleting multiple cells (#77844)
-<li>fixed crash when opening a file over the network (#45822)
+<li>undo not working after deleting multiple cells (#77844)</li>
+<li>fixed crash when opening a file over the network (#45822)</li>
 </ul>
 
 <!--
@@ -95,9 +95,9 @@ KOfficeUI:
 <h3><a name="kivio">Kivio</a></h3>
 <ul>
 <li>Add missing code to make the 'Crop picture to edges' option
-  in the page export dialog work.
-<li>Fixed painting bug when moving stencils containing text.
-<li>Add boundaries to the zoom to prevent lockups and crashes.
+  in the page export dialog work.</li>
+<li>Fixed painting bug when moving stencils containing text.</li>
+<li>Add boundaries to the zoom to prevent lockups and crashes.</li>
 </ul>
 
 <!--
@@ -115,34 +115,34 @@ KOfficeUI:
 
 <em>KWord's RTF import filter:</em>
 <ul>
-<li>make it compile on gcc 3.4
-<li>import dotted underline correctly (instead of double underline)
-<li>fallback to previously used encoding if the asked encoding is not known
-<li>fix linespacing
+<li>make it compile on gcc 3.4</li>
+<li>import dotted underline correctly (instead of double underline)</li>
+<li>fallback to previously used encoding if the asked encoding is not known</li>
+<li>fix linespacing</li>
 </ul>
 
 <em>KWord's RTF export filter:</em>
 <ul>
-<li>fix some paragraph formattings (margins, linespacing)
-<li>escape correctly the document information
+<li>fix some paragraph formattings (margins, linespacing)</li>
+<li>escape correctly the document information</li>
 </ul>
 
 <em>KWord's OOWriter import filter:</em>
 <ul>
-<li>added support for paragraph bullets/counters, footnotes/endnotes, links, table of contents and bookmarks
-<li>fixed shadow color and headers/footers
+<li>added support for paragraph bullets/counters, footnotes/endnotes, links, table of contents and bookmarks</li>
+<li>fixed shadow color and headers/footers</li>
 </ul>
 
 <em>KWord's OOWriter export filter:</em>
 <ul>
-<li>added support for tables and many field variables
-<li>fixes for underlining and linespacing
+<li>added support for tables and many field variables</li>
+<li>fixes for underlining and linespacing</li>
 </ul>
 
 <em>KWord's HTML export filter:</em>
 <ul>
-<li>fix some paragraph formattings in the enhanced mode (margins, linespacing)
-<li>fix background colour in enhanced mode
+<li>fix some paragraph formattings in the enhanced mode (margins, linespacing)</li>
+<li>fix background colour in enhanced mode</li>
 <li>fixed pictures
 </ul>
 
