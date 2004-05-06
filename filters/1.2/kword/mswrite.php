955db7333a460ff680249532adf922aecd7e1c14
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-05-06 21:59:49 +0000

    Update the MS Write information with data from the status.html file CCMAIL:dang@kde.org
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@309220 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- filters/1.2/kword/mswrite.php
+++ filters/1.2/kword/mswrite.php
@@ -5,10 +5,21 @@
   include("header.inc");
 ?>
 
-<p>Microsoft Write files usually end with extention .WRI.  It is the format used by Microsoft Write,
-a very simple word processor which first appears in Microsoft Windows 3.1. KWord can
-read Microsoft Write documents fairly well.</p><p>KWord is not able to save
-the document to Microsoft Write format.</p>
+<p>Imports almost everything except OLE from MS Write (3.0 &amp; 3.1) documents:</p>
+<ul>
+    <li>- Character formatting (fonts, bold, italic, underline, superscript, subscript, &quot;(page)&quot; numbering)</li>
+    <li>- Optional/Soft Hyphenation</li>
+    <li>- Paragraph formatting (alignment, linespacing, indentation, tabulation)</li>
+    <li>- Paragraphs with multiple newlines</li>
+    <li>- Pagebreaking (at the start or end of the line and on the next line)</li>
+    <li>- Images (Standard WMFs, monochrome BMPs, position from left margin)</li>
+    <li>- Character Set Conversion</li>
+
+    <li>- Headers & Footers (including whether or not they are printed on first page)</li>
+    <li>- Page formatting (margins, positioning of headers & footers, page numbers starting from any number)</li>
+</ul>
+
+<p>KWord is not able to save the document to Microsoft Write format.</p>
 
 <?php
    include("footer.inc");
