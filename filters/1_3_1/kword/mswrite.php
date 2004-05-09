845a9c1067aa31100b5709bebad09f01c2025466
dang <dang@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-05-09 10:49:18 +0000

    - KWord 1.3 _can_ save to mswrite.  This means that I can work on my 386 laptop   and Win 3.x users can share documents with KWord (even docs with embedded 24bpp BMPs!) :) - Use &amp; CCMAIL: goutte@kde.org,lauri@kde.org
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@309713 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- filters/1_3_1/kword/mswrite.php
+++ filters/1_3_1/kword/mswrite.php
@@ -15,11 +15,18 @@
     <li>Images (Standard WMFs, monochrome BMPs, position from left margin)</li>
     <li>Character Set Conversion</li>
 
-    <li>Headers & Footers (including whether or not they are printed on first page)</li>
-    <li>Page formatting (margins, positioning of headers & footers, page numbers starting from any number)</li>
+    <li>Headers &amp; Footers (including whether or not they are printed on first page)</li>
+    <li>Page formatting (margins, positioning of headers &amp; footers, page numbers starting from any number)</li>
 </ul>
 
-<p>KWord is not able to save the document to Microsoft Write format.</p>
+<p>Exports text, images and a useful amount of formatting to MS Write 3.0 documents:</p>
+<ul>
+     <li>This import/export filter pair is almost &quot;symmetric&quot; i.e. export (import (f)) = f</li>
+     <li>It can export everything that the import filter can import except Page Layout</li>
+     <li>Images of various types (e.g. BMP, JPEG, WMF, PNG) are supported</li>
+     <li>Tables are simply dumped out as paragraphs</li>
+     <li>Basic list support</li>
+</ul>
 
 <?php
    include("footer.inc");
