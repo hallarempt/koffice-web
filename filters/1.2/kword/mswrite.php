fd2aeb788d664be27b995bbfd8b583c0ab279db1
dang <dang@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-05-09 10:27:18 +0000

    don't double <li>
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@309706 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- filters/1.2/kword/mswrite.php
+++ filters/1.2/kword/mswrite.php
@@ -7,16 +7,16 @@
 
 <p>Imports almost everything except OLE from MS Write (3.0 &amp; 3.1) documents:</p>
 <ul>
-    <li>- Character formatting (fonts, bold, italic, underline, superscript, subscript, &quot;(page)&quot; numbering)</li>
-    <li>- Optional/Soft Hyphenation</li>
-    <li>- Paragraph formatting (alignment, linespacing, indentation, tabulation)</li>
-    <li>- Paragraphs with multiple newlines</li>
-    <li>- Pagebreaking (at the start or end of the line and on the next line)</li>
-    <li>- Images (Standard WMFs, monochrome BMPs, position from left margin)</li>
-    <li>- Character Set Conversion</li>
+    <li>Character formatting (fonts, bold, italic, underline, superscript, subscript, &quot;(page)&quot; numbering)</li>
+    <li>Optional/Soft Hyphenation</li>
+    <li>Paragraph formatting (alignment, linespacing, indentation, tabulation)</li>
+    <li>Paragraphs with multiple newlines</li>
+    <li>Pagebreaking (at the start or end of the line and on the next line)</li>
+    <li>Images (Standard WMFs, monochrome BMPs, position from left margin)</li>
+    <li>Character Set Conversion</li>
 
-    <li>- Headers & Footers (including whether or not they are printed on first page)</li>
-    <li>- Page formatting (margins, positioning of headers & footers, page numbers starting from any number)</li>
+    <li>Headers & Footers (including whether or not they are printed on first page)</li>
+    <li>Page formatting (margins, positioning of headers & footers, page numbers starting from any number)</li>
 </ul>
 
 <p>KWord is not able to save the document to Microsoft Write format.</p>
