31439233500b412bd65e674b4abad83dec9fcba3
rempt <rempt@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-05-29 12:32:10 +0000

    Added a really ancient screenshot of Krita.
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@315763 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- krita/screenshots.php
+++ krita/screenshots.php
@@ -8,8 +8,10 @@
 <?php
 $gallery = new ImageGallery("Krita Screenshots");
 $gallery->addImage("pics/may2004_sm.png", "pics/may2004.png", "204", "200", "[Screenshot]", 0 , "Krita (May 2004)");
-$gallery->addImage("pics/kis_sm.png", "pics/kis.jpg", "204", "200", "[Screenshot]", 0 , "Krita (November 2003)");
 $gallery->addImage("pics/konqi_krita_sm.png", "pics/konqi_krita.png", "204", "200", "[Screenshot]", 0 , "Krita (December 2003)");
+$gallery->addImage("pics/kis_sm.png", "pics/kis.jpg", "204", "200", "[Screenshot]", 0 , "Krita (November 2003)");
+$gallery->addImage("pics/kimageschop_sm.png", "pics/kimageshop.png", "204", "200", "[Screenshot]", 0 , "Krita (circa 2000)");
+
 $gallery->show();
 ?>
 
