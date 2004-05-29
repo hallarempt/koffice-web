1637e4dce1f27a5e784cb871d508be8c88af8a4e
rempt <rempt@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-05-29 14:48:59 +0000

    Typo.
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@315817 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- krita/screenshots.php
+++ krita/screenshots.php
@@ -10,7 +10,7 @@ $gallery = new ImageGallery("Krita Screenshots");
 $gallery->addImage("pics/may2004_sm.png", "pics/may2004.png", "204", "200", "[Screenshot]", 0 , "Krita (May 2004)");
 $gallery->addImage("pics/konqi_krita_sm.png", "pics/konqi_krita.png", "204", "200", "[Screenshot]", 0 , "Krita (December 2003)");
 $gallery->addImage("pics/kis_sm.png", "pics/kis.jpg", "204", "200", "[Screenshot]", 0 , "Krita (November 2003)");
-$gallery->addImage("pics/kimageschopjc_sm.png", "pics/kimageshopjc.png", "204", "200", "[Screenshot]", 0 , "Krita (circa 2000)");
+$gallery->addImage("pics/kimageshopjc_sm.png", "pics/kimageshopjc.png", "204", "200", "[Screenshot]", 0 , "Krita (circa 2000)");
 
 $gallery->show();
 ?>
