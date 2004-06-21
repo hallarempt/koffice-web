d2daf4d9910a5bc08312df8e6c2c2f6133a24f2e
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-06-21 15:24:17 +0000

    Prepare the changelog for KOffice 1.3.2 (currently without useful content)
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@322502 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ announcements/changelog-1.3.2.php
@@ -0,0 +1,90 @@
+<?php
+  $page_title ='Future KOffice 1.3.2 Changelog'; ### TODO
+  $site_root = '../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+<p style="border-color:red;color:red;text-align:center">
+    This document is <em>under construction</em>.<br />
+    KOffice 1.3.2 has <strong>not</strong> being released yet!
+</p>
+
+<p>
+  The following summarizes the major changes and bugfixes to the KOffice Suite since the
+  release of KOffice 1.3.1.
+</p>
+
+<h3><a name="koffice_libs">KOffice Libraries</a></h3>
+<p>KOfficeCore:</p>
+<ul>
+<li>### TODO</li>
+</ul>
+
+<p>KOfficeUI:</p>
+<ul>
+<li>### TODO</li>
+</ul>
+
+<h4>KoText library features (shared between KWord and KPresenter):</h4>
+<ul>
+<li>### TODO</li>
+</ul>
+
+<h3><a name="kword">KWord</a></h3>
+<ul>
+<li>### TODO</li>
+</ul>
+
+
+<h3><a name="kpresenter">KPresenter</a></h3>
+<ul>
+<li>### TODO</li>
+</ul>
+
+
+<h3><a name="kspread">KSpread</a></h3>
+<ul>
+<li>### TODO</li>
+</ul>
+
+<!--
+<h3><a name="kugar">Kugar</a></h3>
+<ul>
+
+</ul>
+-->
+
+<h3><a name="kivio">Kivio</a></h3>
+<ul>
+<li>### TODO</li>
+</ul>
+
+<!--
+<h3><a name="kformula">KFormula</a></h3>
+<ul>
+</ul>
+
+
+<h3><a name="karbon">Karbon</a></h3>
+<ul>
+</ul>
+-->
+
+<h3><a name="filters">Filters</a></h3>
+
+### TODO
+
+
+<h3>Previous changelogs</h3>
+<a href="changelog-1.3.1.php">1.3.1</a> |
+<a href="changelog-1.3.php">1.3</a> |
+<a href="changelog-1.3rc1.php">1.3rc1</a> |
+<a href="changelog-1.3beta4.php">1.3beta4</a> |
+<a href="changelog-1.3beta3.php">1.3beta3</a> |
+<a href="changelog-1.3beta2.php">1.3beta2</a> |
+<a href="changelog-1.3beta1.php">1.3beta1</a>
+
+<?php
+  include("footer.inc");
+?>
