3ad6988c4686584ac5200fc34c2594db970dffce
ginn <ginn@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-06-23 13:41:07 +0000

    Add draft of announcement page changelogs do not match announcements (there are several more changelogs than announced versions) what should be done about that? (I was going to list the changelog as a linnk for each announcement, but that wouldn't cover all of them) CCMAIL: nicolasg@snafu.de
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@323098 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ announcements/index.php
@@ -0,0 +1,94 @@
+<?php
+  $page_title = "KOffice Announcements";
+  $site_root = "../";
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+<p>
+To receive new announcements by e-mail subscribe to the
+KDE Announcements <a href="https://mail.kde.org/mailman/listinfo/kde-announce">mailinglist</a> !
+</p>
+
+<strong>27 January 2004</strong>
+ - <a href="announce-1.3.php">KOffice 1.3 Announcement</a>
+<br />"<em>Improved interoperability Highlights Stable KOffice Release for Linux and Other UNIXes</em>"
+<p />
+
+<strong>23 April 2003</strong>
+ - <a href="announce-1.3-beta1.php">KDE Ships Beta Test Release of Major Enhancements to Free Integrated Office Suite</a>
+<br />"<em>Hyphenation, New Database Client, Scriptability and Filters Highlight KOffice
+  Beta Release for Linux and Other UNIXes</em>"
+<p />
+
+<strong>12 December 2002</strong>
+ - <a href="announce-1.2.1.php">KDE Ships Stability Release of Free Integrated Office Suite</a>
+<br />"<em>Improved Performance / Stability Highlight KOffice Release for Linux and Other UNIXes</em"
+<p />
+
+<strong>10 September 2002</strong>
+ - <a href="announce-1.2.php">KDE Ships Free Integrated Office Suite</a>
+<br />"<em>Advanced Thesaurus, Scriptability, Component Embedding,
+  PDF Export, Filters, and Unicode / BiDi Support
+  Highlight Stable KOffice Release for Linux and Other
+  UNIXes</em>"
+<p />
+
+<strong>09 August 2002</strong>
+ - <a href="announce-1.2-rc1.php">KDE Ships Release Candidate of Free Integrated Office Suite</a>
+<br />"<em>Konqueror Read-Only Embedding, WordPerfect Export Filter and Improved
+  Stability Highlight KOffice Release Candidate for Linux and Other
+  UNIXes</em>"
+<p />
+
+<strong>26 June 2002</strong>
+ - <a href="announce-1.2-beta2.php">KDE Ships Second Beta Test Release of Free Integrated Office Suite</a>
+<br />"<em>KWord Footnotes, KSpread Database Connectivity
+  Highlight KOffice Beta Release for Linux and Other UNIXes</em>"
+<p />
+
+<strong>25 April 2002</strong>
+ - <a href="announce-1.2-beta1.php">KDE Ships Beta Test Release of Free Integrated Office Suite</a>
+<br />"<em>Improved Layout, Scriptability and Filters Highlight KOffice
+  Beta Release for Linux and Other UNIXes</em>"
+<p />
+
+<strong>17 December 2001</strong>
+ - <a href="announce-1.1.1.php">KDE Ships Stability Release of Free Integrated Office Suite</a>
+<br />"<em>Improved Printing, Performance and Stability Highlight KOffice
+ Release for Linux and Other UNIXes</em>"
+<p />
+
+<strong>28 August 2001</strong>
+ - <a href="announce-1.1.php">KDE's Free, Integrated Office Suite Available for Linux/Unix</a>
+<br />"<em>Leading Linux Desktop Ships Free Stable Release of KOffice</em>"
+<p />
+
+<strong>30 July 2001</strong>
+ - <a href="announce-1.1-rc1.php">KOffice Suite Release Candidate Available for Linux/Unix</a>
+<br />"<em>KDE, the Leading Linux Desktop, Ships First Release Candidate of
+    KOffice Suite</em>"
+<p />
+
+<strong>25 June 2001</strong>
+ - <a href="announce-1.1-beta3.php">KOffice Suite Beta Released for Linux</a>
+<br />"<em>KDE, the Leading Linux Desktop, Ships Beta of KOffice
+    Suite</em>"
+<p />
+
+<strong>21 May 2001</strong>
+ - <a href="announce-1.1-beta2.php">KOffice Suite Beta Released for Linux</a>
+<br />"<em>KDE, the Leading Linux Desktop, Ships Beta of KOffice
+    Suite</em>"
+<p />
+
+<strong>24 April 2001</strong>
+ - <a href="announce-1.1-beta1.php">KOffice Suite Beta Released for Linux</a>
+<br />"<em>KDE, the Leading Linux Desktop, Ships Beta of KOffice
+    Suite</em>"
+<p />
+
+
+<?php 
+  include("footer.inc"); 
+?>
