765f2942d925d3b23caea737986c70967a71b655
ginn <ginn@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-06-23 17:54:06 +0000

    Add an index of releases (in reverse chronological order, of course).
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@323157 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ releases/index.php
@@ -0,0 +1,36 @@
+<?php
+  $page_title = "KOffice Releases";
+  $site_root = "../";
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+<p>
+To receive new announcements by e-mail subscribe to the
+KDE Announcements <a href="https://mail.kde.org/mailman/listinfo/kde-announce">mailinglist</a> !
+</p>
+
+<p /><a href="1.3.2-release.php">KOffice 1.3.2 Release</a>
+<p /><a href="1.3.1-release.php">KOffice 1.3.1 Release</a>
+<p /><a href="1.3-release.php">KOffice 1.3 Release</a>
+<p /><a href="1.3rc2-release.php">KOffice 1.3 RC 2 Release</a>
+<p /><a href="1.3rc1-release.php">KOffice 1.3 RC 1 Release</a>
+<p /><a href="1.3beta4-release.php">KOffice 1.3 Beta 4 Release</a>
+<p /><a href="1.3beta3-release.php">KOffice 1.3 Beta 3 Release</a>
+<p /><a href="1.3beta2-release.php">KOffice 1.3 Beta 2 Release</a>
+<p /><a href="1.3beta1-release.php">KOffice 1.3 Beta 1 Release</a>
+<p /><a href="1.2.1-release.php">KOffice 1.2.1 Release</a>
+<p /><a href="1.2-release.php">KOffice 1.2 Release</a>
+<p /><a href="1.2rc1-release.php">KOffice 1.2 RC 1 Release</a>
+<p /><a href="1.2beta2-release.php">KOffice 1.2 Beta 2 Release</a>
+<p /><a href="1.2beta1-release.php">KOffice 1.2 Beta 1 Release</a>
+<p /><a href="1.1.1-release.php">KOffice 1.1.1 Release</a>
+<p /><a href="1.1-release.php">KOffice 1.1 Release</a>
+<p /><a href="1.1beta3-release.php">KOffice 1.1 Beta 3 Release</a>
+<p /><a href="1.1beta2-release.php">KOffice 1.1 Beta 2 Release</a>
+<p /><a href="1.1rc1-release.php">KOffice 1.1 RC1 Release</a>
+
+
+<?php 
+  include("footer.inc"); 
+?>
