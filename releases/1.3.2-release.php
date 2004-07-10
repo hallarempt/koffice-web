b80826fff71613bee7f9c74a72be4caadc6f6641
dang <dang@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-07-10 07:05:09 +0000

    +)
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@327415 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- releases/1.3.2-release.php
+++ releases/1.3.2-release.php
@@ -24,7 +24,7 @@
       <p>
         <em>Note:</em> the source language packages unpack as directories
         whose names start with "kde-i18n" instead of "koffice-i18n"
-        (as KOffice 1.3.1 did.
+        (as KOffice 1.3.1 did).
       </p>
       
       <h2>MD5 sums</h2>
