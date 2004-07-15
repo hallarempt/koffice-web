b8572903decda0e38749c43e66e003e0a2b8128e
mlaurent <mlaurent@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-07-15 10:23:09 +0000

    Update
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@329539 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- announcements/changelog-1.3.3.php
+++ announcements/changelog-1.3.3.php
@@ -100,7 +100,7 @@
 <h4>KSpread's OOCalc export filter:</h4>
 <ul>
 <li>Fix save manifest.xml into META-INF/</li>
-<li>Implement save settings into settings.xml</li>
+<li>Implement save settings into settings.xml (active table, cursor position)</li>
 </ul>
 
 <h4>Karbon's EPS filter:</h4>
