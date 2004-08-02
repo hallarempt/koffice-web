6696580824e0a416740e285abfe64039b62dfa1f
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-08-02 14:06:01 +0000

    Clen up the link list: - split the tables, to allow easier removal or adding of links - hide dead links by PHP - remove cell colors
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@335151 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- developer/filters/useful.php
+++ developer/filters/useful.php
@@ -11,28 +11,30 @@
 
     <h2>File Formats</h2>
 
+    <h3>General</h3>
+    
     <table border="0" cellspacing="0" cellpadding="1" bgcolor="#000000" >
-    <tr>
-        <td>
-        <table border="0" cellspacing="1" cellpadding="3" bgcolor="#EEEEFF">
-        <tr bgcolor="#AAAAFF">
-        <td rowspan="1" valign="top"><b>General</b></td>
+      <tr>
         <td><a href="http://www.wotsit.org">Wotsit</a></td>
         <td>Information on various file formats</td>
-        </tr>
+      </tr>
+    </table>
 
-        <tr bgcolor="#CCCCFF">
+    <h3>MS Office</h3>
+    <table border="0" cellspacing="0" cellpadding="1" bgcolor="#000000" >
+    
+        <tr>
         <td rowspan="9" valign="top"><b>MS Office</b></td>
         <td><a href="http://msdn.microsoft.com">msdn</a></td>
         <td>The MS Developer's Network Library</td>
         </tr>
 
-        <tr bgcolor="#CCCCFF">
+        <tr>
         <td><a href="http://snake.cs.tu-berlin.de:8081/~schwartz/pmh/index.html">LAOLA</a></td>
         <td>The famous LAOLA homepage (OLE)</td>
         </tr>
 
-        <tr bgcolor="#CCCCFF">
+        <tr>
         <td><a href="http://www.btinternet.com/~shaheedhaque/">Word 97</a></td>
         <td>Information to Word Filters (by Shaheed Haque)</td>
         </tr>
@@ -40,7 +42,7 @@
 <?php
   /* FIXME Broken URL
 
-        <tr bgcolor="#CCCCFF">
+        <tr>
         <td><a href="http://www.xlhtml.org">Excel95, ppt</a></td>
         <td><table border="0">
                 <tr>
@@ -59,43 +61,45 @@
     */
 ?>
 
-        <tr bgcolor="#CCCCFF">
+        <tr>
         <td><a href="http://www.mlib.unibel.by/pub/pub/qqq/office/MSWordView.html">Word 8</a></td>
         <td>MSWord 8 converter for unix: MSWordView</td>
         </tr>
 
-        <tr bgcolor="#CCCCFF">
+        <tr>
         <td><a href="http://www.wvware.com/wvInfo.html">File formats</a></td>
         <td>MS Office File Formats Information and Converters </td>
         </tr>
 
-        <tr bgcolor="#CCCCFF">
+        <tr>
         <td><a href="http://arturo.directmail.org/filtersweb/">Filtersweb</a></td>
         <td>libole2(GNOME), xls2xml, cole (r+w ole2 structured storage files)</td>
         </tr>
 
-        <tr bgcolor="#CCCCFF">
+        <tr>
         <td><a href="http://sourceforge.net/projects/wvware">wv-library</a></td>
         <td>The wv-library (by Caolan McNamara (new link))</td>
         </tr>
 
-        <tr bgcolor="#CCCCFF">
+        <tr>
         <td><a href="http://skynet.csn.ul.ie/~caolan">wv-library</a></td>
         <td>The wv-library (by Caolan McNamara (old link))</td>
         </tr>
 
-        <tr bgcolor="#CCCCFF">
+        <tr>
         <td><a href="http://sc.openoffice.org/excelfileformat.pdf">MS Excel</a></td>
         <td>OpenOffice commentary on the Excel file format</td>
         </tr>
+    </table>
 
-        <tr bgcolor="#AAAAFF">
-        <td rowspan="5" valign="top"><b>Star Office</b></td>
+    <h3>Star/Open Office</h3>
+    <table border="0" cellspacing="0" cellpadding="1" bgcolor="#000000" >
+        <tr>
         <td><a href="http://xml.openoffice.org/">XML</a></td>
-        <td>The future-format for StarOffice (open office): XML</td>
+        <td>File format of Open Office</td>
         </tr>
 
-        <tr bgcolor="#AAAAFF">
+        <tr>
         <td><a href="http://www.openoffice.org/source/browse/sw/sw/source/filter/">
             Filter</a></td>
         <td>StarOffice filters (incl. Starword 6.x) via webcvs: <br />
@@ -104,7 +108,7 @@
         </td>
         </tr>
 
-        <tr bgcolor="#AAAAFF">
+        <tr>
         <td><a href="http://www.openoffice.org/source/browse/sw/sw/source/core/sw3io/">
             Version 3-5.x</a></td>
         <td>StarOffice old formats (version 3->5.x) via webcvs: <br />
@@ -112,67 +116,71 @@
         </td>
         </tr>
 
-        <tr bgcolor="#AAAAFF">
+        <tr>
         <td><a href="http://www.abisource.com/lxr/source/abiword-plugins/wp/impexp/sdw/docs/StarWrite_File_Format.html">
             SDW file format</a></td>
         <td>Description of the SDW file format, by an Abiword developer, who sent us this link
         </td>
         </tr>
 
-        <tr bgcolor="#AAAAFF">
+        <tr>
         <td><a href="http://www.marcozanon.com/download/swfile.html">
             Starwriter 5.x</a></td>
         <td>Another description of the Starwriter 5.x file format
         </td>
         </tr>
+    </table>
 
-        <tr bgcolor="#CCCCFF">
-        <td rowspan="2" valign="top"><b>Adobe</b></td>
+    <h3>Adobe</h3>
+    <table border="0" cellspacing="0" cellpadding="1" bgcolor="#000000" >
+        <tr>
         <td><a href="http://partners.adobe.com/asn/tech/pdf/specifications.jsp">PDF</a></td>
         <td>PDF specification</td>
         </tr>
 
-        <tr bgcolor="#CCCCFF">
+        <tr>
         <td><a href="http://www.adobe.com/support/downloads/865e.htm">
             MIF &amp; MML</a></td>
-        <td> MIF &amp; MML format documentation. &nbsp; (MML= Maker Markup Language;&nbsp;  MIF=Marker Interchange Format.)</td>
+        <td>MIF &amp; MML format documentation. (MML=Maker Markup Language; MIF=Marker Interchange Format.)</td>
         </tr>
+    </table>
 
-        <tr bgcolor="#CCCCFF">
-        <td rowspan="1" valign="top"><b>Corel</b></td>
+    <h3>Corel</h3>
+    <table border="0" cellspacing="0" cellpadding="1" bgcolor="#000000" >
+        <tr>
         <td><a href="http://www.corel.com/partners_developers/ds/wpsdks.htm">
             Version 7.0</a></td>
         <td>Fileformat description for WordPerfect, Presentations, Quattro Pro... (Version 7.0) </td>
         </tr>
+    </table>
 
 
-        <tr bgcolor="#AAAAFF">
-        <td rowspan="2" valign="top"><b>Abiword</b></td>
+    <h3>AbiSource</h3>
+    <table border="0" cellspacing="0" cellpadding="1" bgcolor="#000000" >
+        <tr>
         <td><a href="http://www.abisource.com/awml.dtd">
             DTD</a></td>
-        <td>Fileformat description: DTD</td>
+        <td>Fileformat description of AbiWord: DTD</td>
         </tr>
 
-        <tr bgcolor="#AAAAFF">
+        <tr>
         <td><a href="http://www.abisource.com/lxr/source/abi/docs/AbiWord_DocumentFormat.abw">
             abw</a></td>
         <td>Fileformat description: AbiWord_DocumentFormat.abw </td>
         </tr>
+    </table>
 
-        <!--
-        <tr bgcolor="#CCCCFF">
-        <td rowspan="1" valign="top"><b>Lotus</b></td>
+<?php /*
+	
+    <h3>Lotus</h3>
+    <table border="0" cellspacing="0" cellpadding="1" bgcolor="#000000" >
+        <tr>
         <td><a href="ftp://www.thekompany.com/pub/KOffice/filters/">
             WordPro</a></td>
         <td>API of WordPro (ftp)</td>
         </tr>
-        -->
-
-        </table>
-        </td>
-    </tr>
     </table>
-
+*/ ?>
 
     <h2>Home Pages</h2>
     
@@ -206,10 +214,5 @@
             (illustrations)
         </li>
     </ul>
-    <br /><br />
-
-    <p>If you find another interesting one,
-    <a href="mailto:koffice@kde.org">please let us know</a>,
-    so that we can add it here.</p>
 
 <?php include("footer.inc"); ?>
