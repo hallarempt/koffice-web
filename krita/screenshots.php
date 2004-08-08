a34f84f7674d07943eca10f04d39b62831028595
binner <binner@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-08-08 06:28:31 +0000

    CVS_SILENT typo fix
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@336791 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- krita/screenshots.php
+++ krita/screenshots.php
@@ -7,7 +7,7 @@
 
 <?php
 $gallery = new ImageGallery("Krita Screenshots");
-$gallery->addImage("pics/krita-june_sm.png", "pics/krita-june.png", "257", "200", "[Screenshot]", 0 , "Krita (May 2004)");
+$gallery->addImage("pics/krita-june_sm.png", "pics/krita-june.png", "257", "200", "[Screenshot]", 0 , "Krita (June 2004)");
 $gallery->startNewRow(); 
 $gallery->addImage("pics/may2004_sm.png", "pics/may2004.png", "204", "200", "[Screenshot]", 0 , "Krita (May 2004)");
 $gallery->addImage("pics/konqi_krita_sm.png", "pics/konqi_krita.png", "204", "200", "[Screenshot]", 0 , "Krita (December 2003)");
