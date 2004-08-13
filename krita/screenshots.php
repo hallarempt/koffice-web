ca7fa8023a8b9971940dd681d6a876ad54733b1f
rempt <rempt@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-08-13 13:20:46 +0000

    Added one more screenshots, showing off the new selections feature.
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@337709 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- krita/screenshots.php
+++ krita/screenshots.php
@@ -7,6 +7,7 @@
 
 <?php
 $gallery = new ImageGallery("Krita Screenshots");
+$gallery->addImage("pics/krita_selection_sm.png", "pics/kritas_selection.png", "252", "200", "[Screenshot]", 0 , "Krita (August 2004)");
 $gallery->addImage("pics/krita-june_sm.png", "pics/krita-june.png", "257", "200", "[Screenshot]", 0 , "Krita (June 2004)");
 $gallery->startNewRow(); 
 $gallery->addImage("pics/may2004_sm.png", "pics/may2004.png", "204", "200", "[Screenshot]", 0 , "Krita (May 2004)");
