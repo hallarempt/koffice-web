84f866a66cff646bd67b56ac1242fb1fa7e5ef8b
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-08-17 21:49:45 +0000

    As the KOffice's API in only on koffice.org, so try to have a version that works also on other sites (for example kde.org)
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@338719 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- developer/apidocmain.php
+++ developer/apidocmain.php
@@ -4,21 +4,45 @@
   include("koffice.inc");
   include("header.inc");
 ?>
-    <!-- NOTE: the directory apidocs and kofficeapidocs.tar.bz2 are not in the CVS;
-      they are generated directly for the Web server. -->
+
+<?php
+    /* NOTE: the directory apidocs and kofficeapidocs.tar.bz2 are not in the CVS;
+      they are generated directly for the Web server koffice.org. */
+
+    $flag = file_exists( "./kofficeapidocs.tar.bz2" );
+?>
    <p>A programmers API Reference is available. It is generated from the KOffice source code, using Doxygen. It can either be read online here, or read offline by downloading the tarball.</p>
    <h2>Download the tarball</h2>
    <ul>
-   <li><a href="kofficeapidocs.tar.bz2">Download the tarball</a>
-     <?php
-       niceFileSize("./kofficeapidocs.tar.bz2");
-       echo gmdate (" (Y-m-d H:i)", filemtime("./kofficeapidocs.tar.bz2"));
-     ?>
-   </li>
+     <li>
+<?php
+    if ( %flag )
+    { 
+        echo "<a href=\"kofficeapidocs.tar.bz2\">Download the tarball</a>";
+        niceFileSize("./kofficeapidocs.tar.bz2");
+        echo gmdate (" (Y-m-d H:i)", filemtime("./kofficeapidocs.tar.bz2"));
+    }
+    else
+    {
+        echo "<a href=\"http://www.koffice.org/developer/kofficeapidocs.tar.bz2\">Download the tarball</a>";
+    }
+?>
+     </li>
    </ul>
    <h2>Read online</h2>
     <ul>
-    <li><a href="apidocs">Index</a></li>
+      <li>
+<?php
+    if ( %flag )
+    { 
+        echo "<a href=\"apidocs\">Index</a>";
+    }
+    else
+    {
+        echo "<a href=\"http://www.koffice.org/developer/apidocs\">Index</a>";
+    }
+?>
+      </li>
     </ul>
 
 <?php
