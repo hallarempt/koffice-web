8a76ba46a4a1a59e79b4aa43973be17b178c0a38
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-09-16 21:22:32 +0000

    - do not use absolute paths for KOffice web pages   (as it does not work with kde.org/areas/koffice and   despite that some HTML user agents do not seem to understand .. in URLs ) - change the description of the changelogs (as noticed by Thomas Zander) - completely remove the comment about the former problem of KOffice 1.3.2
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@347058 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- releases/1.3.3-release.php
+++ releases/1.3.3-release.php
@@ -15,27 +15,19 @@
 
       <h2>Changelog</h2>
       <ul>
-        <li><a href="/announcements/changelog-1.3.3.php">Changes done after 1.3.2 (1.3.3 -- 1.3.2)</a></li>
-        <li><a href="/announcements/changelog-1.3.2.php">Changes done after 1.3.1 (1.3.2 -- 1.3.1)</a></li>
-        <li><a href="/announcements/changelog-1.3.1.php">Changes done after 1.3 (1.3.1 -- 1.3)</a></li>
-        <li><a href="/announcements/changelog-1.3.php">Complete 1.3 changelog</a></li>
+        <li><a href="../announcements/changelog-1.3.3.php">Changes done after 1.3.2 (1.3.2 to 1.3.3)</a></li>
+        <li><a href="../announcements/changelog-1.3.2.php">Changes done after 1.3.1 (1.3.1 to 1.3.2)</a></li>
+        <li><a href="../announcements/changelog-1.3.1.php">Changes done after 1.3 (1.3 to 1.3.1)</a></li>
+        <li><a href="../announcements/changelog-1.3.php">Complete 1.3 changelog</a></li>
       </ul>
 
       <h2>Download</h2>
       <p>Both source code and binary packages are available.</p>
       <ul>
-      <li><a href="/download/source.php">Source code</a></li>
-      <li><a href="/download/">Binary packages</a></li>
+      <li><a href="../download/source.php">Source code</a></li>
+      <li><a href="../download/">Binary packages</a></li>
       </ul>
 
-<?php /*
-      <p>
-        <em>Note:</em> the source language packages unpack as directories
-        whose names start with "kde-i18n" instead of "koffice-i18n"
-        (as KOffice 1.3.1 did).
-      </p>
-      */ ?>
-      
       <h2>MD5 sums</h2>
       <p>
        The MD5 sums for the tarballs are:
@@ -49,7 +41,7 @@
       </p>
       
       <p><em>Note:</em>the PGP key for signing is the same as for KOffice 1.3.2,
-        <a href="/download/signatures.php#goutte">please see the signing key overview.</a>
+        <a href="../download/signatures.php#goutte">please see the signing key overview.</a>
       </p>
 
 <?php include("footer.inc"); ?>
