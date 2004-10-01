f39fa6b99430801ea8fab9f048c94dad8e1ebc99
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-10-01 19:41:39 +0000

    Add a column with a short description of the packages (Useful for people taht do not know the language codes.)
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@350956 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- releases/md5sum2table.pl
+++ releases/md5sum2table.pl
@@ -4,15 +4,127 @@
 $VERSION="1.3.3";
 $STATUS="stable";
 
+# team names (from kde-i18n/teamnames)
+use constant LANGUAGES =>
+{
+    "af" => "Afrikaans",
+    "ar" => "Arabic",
+    "az" => "Azerbaijani",
+    "be" => "Belarusian",
+    "bg" => "Bulgarian",
+    "bn" => "Bengali",
+    "bo" => "Tibetan",
+    "br" => "Breton",
+    "bs" => "Bosnian",
+    "ca" => "Catalan",
+    "cs" => "Czech",
+    "cy" => "Welsh",
+    "da" => "Danish",
+    "de" => "German",
+    "el" => "Greek",
+    "en_GB" => "British English",
+    "eo" => "Esperanto",
+    "es" => "Spanish",
+    "et" => "Estonian",
+    "eu" => "Basque",
+    "fa" => "Farsi",
+    "fi" => "Finnish",
+    "fo" => "Faroese",
+    "fr" => "French",
+    "fy" => "Frisian",
+    "ga" => "Irish Gaelic",
+    "gl" => "Galician",
+    "he" => "Hebrew",
+    "hi" => "Hindi",
+    "hr" => "Croatian",
+    "hsb" => "Upper Sorbian",
+    "hu" => "Hungarian",
+    "id" => "Indonesian",
+    "is" => "Icelandic",
+    "it" => "Italian",
+    "ja" => "Japanese",
+    "ko" => "Korean",
+    "ku" => "Kurdish",
+    "lo" => "Lao",
+    "lt" => "Lithuanian",
+    "lv" => "Latvian",
+    "mi" => "Maori",
+    "mk" => "Macedonian",
+    "mn" => "Mongolian",
+    "ms" => "Malay",
+    "mt" => "Maltese",
+    "nl" => "Dutch",
+    "nb" => "Norwegian Bokm&aring;l",
+    "nso" => "Northern Sotho",
+    "nn" => "Norwegian Nynorsk",
+    "oc" => "Occitan",
+    "pl" => "Polish",
+    "pt" => "Portuguese",
+    "pt_BR" => "Brazilian Portuguese",
+    "ro" => "Romanian",
+    "ru" => "Russian",
+    "se" => "Northern Sami",
+    "sk" => "Slovak",
+    "sl" => "Slovenian",
+    "sq" => "Albanian",
+    "sr" => "Serbian",
+    "ss" => "Swati",
+    "sv" => "Swedish",
+    "ta" => "Tamil",
+    "tg" => "Tajik",
+    "th" => "Thai",
+    "tr" => "Turkish",
+    "uk" => "Ukrainian",
+    "ven" => "Venda",
+    "vi" => "Vietnamese",
+    "wa" => "Walloon",
+    "xh" => "Xhosa",
+    "zh_CN" => "Chinese Simplified",
+    "zh_TW" => "Chinese Traditional",
+    "zu" => "Zulu"
+};
+
 print '<table border="0" cellpadding="4" cellspacing="0">'."\n";
-print '<tr valign="top"><th align="left">Location</th><th align="left">MD5&nbsp;Sum</th></tr>'."\n";
+print '<tr valign="top"><th align="left">Location</th><th align="left">Description</th><th align="left">MD5&nbsp;Sum</th></tr>'."\n";
 
 while ( <STDIN> )
 {
     /^([0-9a-z]+) +(.+)/ ;
-    print '<tr valign="top"><td><a href="http://download.kde.org/' . $STATUS . '/koffice-' . $VERSION . '/src/' . $2 . '">'."\n";
-    print $2 . '</a></td>'."\n";
-    print '<td><tt>' . $1 . '</tt></td></tr>'."\n";
+    $package = $2;
+    $md5sum = $1;
+
+    if ( $package =~ /^koffice-i18n/ )
+    {
+        $package =~ /^koffice-i18n-([0-9a-zA-Z_]+)-/ ;
+        if ( $1 cmp "" )
+        {
+            ### TODO: transform to language name
+            $description = LANGUAGES->{$1};
+            if ( ! $description cmp "" )
+            {
+                $description = $1;
+                warn "Unknown language: $1";
+            }
+        }
+        else
+        {
+            $description = "All languages";
+        }
+    }
+    else
+    {
+        $description = "General";
+    }
+
+    if ( $package =~ /xdelta$/ )
+    {
+        $description = $description . " (as XDelta)";
+    }
+    
+    print '<tr valign="top"><td><a href="http://download.kde.org/' . $STATUS . '/koffice-' . $VERSION . '/src/' . $package . '">'."\n";
+    print $package . '</a></td>'."\n";
+    print "<td>$description</td>\n";
+    print '<td><tt>' . $md5sum . '</tt></td></tr>'."\n";
 }
 print '</table>'."\n";
 
