472dd627d74595d677f33f2e216f04be8b9a57e3
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-10-20 10:00:23 +0000

    Add a changelog for KOffice 1.3.5 as bug #72827 is fixed
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@356167 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ announcements/changelog-1.3.5.php
@@ -0,0 +1,147 @@
+<?php
+  $page_title ='KOffice 1.3.5 Changelog';
+  $site_root = '../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+<p style="border:solid red;color:red;text-align:center">
+    This document is <em>under construction</em>.<br />
+    KOffice 1.3.5 has <strong>not</strong> been released yet!
+</p>
+
+
+<p>
+  The following summarizes the major changes and bugfixes to the KOffice Suite since the
+  release of KOffice 1.3.4.
+</p>
+
+<?php /*
+<h3><a name="i18n">Languages</a></h3>
+<ul>
+</ul>
+*/ ?>
+
+<h3><a name="koffice_libs">KOffice Libraries</a></h3>
+<h4>KOfficeCore</h4>
+<ul>
+</ul>
+
+<h4>KOfficeUI</h4>
+<ul>
+</ul>
+
+<h4>KoText library features (shared between KWord and KPresenter)</h4>
+<ul>
+</ul>
+
+<h3><a name="allapps">All Applications</a></h3>
+<ul>
+</ul>
+
+<h3><a name="kword">KWord</a></h3>
+<ul>
+  <li>
+    Fix image inserting
+    (<a href="http://bugs.kde.org/show_bug.cgi?id=72827">#72827</a>)
+  </li>
+</ul>
+
+
+<h3><a name="kpresenter">KPresenter</a></h3>
+<ul>
+</ul>
+
+
+<h3><a name="kspread">KSpread</a></h3>
+<ul>
+</ul>
+
+<h3><a name="kchart">KChart</a></h3>
+<ul>
+</ul>
+
+<?php /*
+<h3><a name="kugar">Kugar</a></h3>
+<ul>
+</ul>
+
+<h3><a name="kivio">Kivio</a></h3>
+<ul>
+</ul>
+
+<h3><a name="kformula">KFormula</a></h3>
+<ul>
+</ul>
+
+<h3><a name="karbon">Karbon</a></h3>
+<ul>
+</ul>
+
+*/ ?>
+
+<h3><a name="filters">Filters</a></h3>
+
+<h4>KWord's OpenOffice.org Writer Export Filter</h4>
+<ul>
+</ul>
+
+<h4>KWord's RTF Import Filter</h4>
+<ul>
+</ul>
+
+<?php /*
+<h4>KPresenter's OpenOffice.org Impress Import Filter</h4>
+<ul>
+</ul>
+*/ ?>
+
+<h4>KPresenter's OpenOffice.org Impress Export Filter</h4>
+<ul>
+</ul>
+
+<?php /*
+<h4>KSpread's OpenOffice.org Calc Import Filter</h4>
+<ul>
+</ul>
+
+<h4>KSpread's OpenOffice.org Calc Export Filter</h4>
+<ul>
+</ul>
+
+<h4>KSpread's GNUmeric Export Filter</h4>
+<ul>
+</ul>
+
+<h4>KSpread's MS Excel Import Filter</h4>
+<ul>
+</ul>
+
+<h4>KSpread's CSV Export Filter</h4>
+<ul>
+</ul>
+*/ ?>
+
+<h4>KFormula's MathML Import Filter</h4>
+<ul>
+</ul>
+
+<h4>KFormula's MathML Export Filter</h4>
+<ul>
+</ul>
+
+<h3>Previous changelogs</h3>
+<a href="changelog-1.3.4.php">1.3.4</a> |
+<a href="changelog-1.3.3.php">1.3.3</a> |
+<a href="changelog-1.3.2.php">1.3.2</a> |
+<a href="changelog-1.3.1.php">1.3.1</a> |
+<a href="changelog-1.3.php">1.3</a> |
+<a href="changelog-1.3rc1.php">1.3rc1</a> |
+<a href="changelog-1.3beta4.php">1.3beta4</a> |
+<a href="changelog-1.3beta3.php">1.3beta3</a> |
+<a href="changelog-1.3beta2.php">1.3beta2</a> |
+<a href="changelog-1.3beta1.php">1.3beta1</a>
+
+<?php
+  include("footer.inc");
+?>
