ebef4f9f4b15bfda51185cc485dfb4605e6aab90
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-10-26 18:03:45 +0000

    Make a list out of the KOffice versions (Also XHTML 1.0 Appendix C (avoid <p /> )
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@357991 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- releases/index.php
+++ releases/index.php
@@ -10,28 +10,29 @@ To receive new announcements by e-mail subscribe to the
 KDE Announcements <a href="https://mail.kde.org/mailman/listinfo/kde-announce">mailinglist</a> !
 </p>
 
-<p /><a href="1.3.4-release.php">KOffice 1.3.4 Release</a>
-<p /><a href="1.3.3-release.php">KOffice 1.3.3 Release</a>
-<p /><a href="1.3.2-release.php">KOffice 1.3.2 Release</a>
-<p /><a href="1.3.1-release.php">KOffice 1.3.1 Release</a>
-<p /><a href="1.3-release.php">KOffice 1.3 Release</a>
-<p /><a href="1.3rc2-release.php">KOffice 1.3 RC 2 Release</a>
-<p /><a href="1.3rc1-release.php">KOffice 1.3 RC 1 Release</a>
-<p /><a href="1.3beta4-release.php">KOffice 1.3 Beta 4 Release</a>
-<p /><a href="1.3beta3-release.php">KOffice 1.3 Beta 3 Release</a>
-<p /><a href="1.3beta2-release.php">KOffice 1.3 Beta 2 Release</a>
-<p /><a href="1.3beta1-release.php">KOffice 1.3 Beta 1 Release</a>
-<p /><a href="1.2.1-release.php">KOffice 1.2.1 Release</a>
-<p /><a href="1.2-release.php">KOffice 1.2 Release</a>
-<p /><a href="1.2rc1-release.php">KOffice 1.2 RC 1 Release</a>
-<p /><a href="1.2beta2-release.php">KOffice 1.2 Beta 2 Release</a>
-<p /><a href="1.2beta1-release.php">KOffice 1.2 Beta 1 Release</a>
-<p /><a href="1.1.1-release.php">KOffice 1.1.1 Release</a>
-<p /><a href="1.1-release.php">KOffice 1.1 Release</a>
-<p /><a href="1.1beta3-release.php">KOffice 1.1 Beta 3 Release</a>
-<p /><a href="1.1beta2-release.php">KOffice 1.1 Beta 2 Release</a>
-<p /><a href="1.1rc1-release.php">KOffice 1.1 RC1 Release</a>
-
+<ul>
+  <li><a href="1.3.4-release.php">KOffice 1.3.4 Release</a></li>
+  <li><a href="1.3.3-release.php">KOffice 1.3.3 Release</a></li>
+  <li><a href="1.3.2-release.php">KOffice 1.3.2 Release</a></li>
+  <li><a href="1.3.1-release.php">KOffice 1.3.1 Release</a></li>
+  <li><a href="1.3-release.php">KOffice 1.3 Release</a></li>
+  <li><a href="1.3rc2-release.php">KOffice 1.3 RC 2 Release</a></li>
+  <li><a href="1.3rc1-release.php">KOffice 1.3 RC 1 Release</a></li>
+  <li><a href="1.3beta4-release.php">KOffice 1.3 Beta 4 Release</a></li>
+  <li><a href="1.3beta3-release.php">KOffice 1.3 Beta 3 Release</a></li>
+  <li><a href="1.3beta2-release.php">KOffice 1.3 Beta 2 Release</a></li>
+  <li><a href="1.3beta1-release.php">KOffice 1.3 Beta 1 Release</a></li>
+  <li><a href="1.2.1-release.php">KOffice 1.2.1 Release</a></li>
+  <li><a href="1.2-release.php">KOffice 1.2 Release</a></li>
+  <li><a href="1.2rc1-release.php">KOffice 1.2 RC 1 Release</a></li>
+  <li><a href="1.2beta2-release.php">KOffice 1.2 Beta 2 Release</a></li>
+  <li><a href="1.2beta1-release.php">KOffice 1.2 Beta 1 Release</a></li>
+  <li><a href="1.1.1-release.php">KOffice 1.1.1 Release</a></li>
+  <li><a href="1.1-release.php">KOffice 1.1 Release</a></li>
+  <li><a href="1.1beta3-release.php">KOffice 1.1 Beta 3 Release</a></li>
+  <li><a href="1.1beta2-release.php">KOffice 1.1 Beta 2 Release</a></li>
+  <li><a href="1.1rc1-release.php">KOffice 1.1 RC1 Release</a></li>
+</ul>
 
 <?php 
   include("footer.inc"); 
