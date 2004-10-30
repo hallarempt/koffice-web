ff0a0d19daca5956f64e83f8859b7c0b86e6abbf
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2004-10-30 17:12:49 +0000

    Strong xpdf fox for KOffice 1.3.4
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@359039 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- releases/1.3.4-release.php
+++ releases/1.3.4-release.php
@@ -13,6 +13,37 @@
 	  <li>to be able to compile KOffice on KDE 3.1.5 and Qt 3.1.2 again</li>
       </ul>
 
+<div style="border:thick solid red">
+
+    <h2>Security Errata</h2>
+
+    <p>KOffice 1.3.4 has an integer overflow vulnerability fix in KWord's PDF import filter
+    which is weak against compiler optimization.</p>
+    
+    <p>
+        <a href="http://download.kde.org/stable/koffice-1.3.4/src/patch/xpdf_koffice_1_3_4_security_integer_overflow.diff">
+            A patch for the source package is available.
+        </a>
+    </p>
+
+    <p>The patch applies to the directory koffice/filters/kword/pdf/xpdf/xpdf .</p>
+
+    <p>
+        To patch, do: (please adjust paths) <br />
+        <code>
+            cd koffice/filters/kword/pdf/xpdf/xpdf<br />
+            patch -p0 &lt; xpdf_koffice_1_3_4_security_integer_overflow.diff
+        </code>
+    </p>
+
+    <p>For background information about the vulnerability,
+        <a href="http://kde.org/info/security/advisory-20041021-1.txt">
+            please see the corresponding security advisiory for KDE.
+        </a>
+    </p>
+
+</div>
+
 
       <h2>Changelog</h2>
       <ul>
