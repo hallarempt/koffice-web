ba9a98893fa8c94a6b872699df31ef75285ad343
dfaure <dfaure@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-03-16 09:57:53 +0000

    404s to this url too
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@398036 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ killustrator/index.php
@@ -0,0 +1,13 @@
+<?php
+  $page_title='Kontour';
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+      <p>Kontour (formerly known as KIllustrator) has been discontinued.
+      Another application takes its place within KOffice:
+      <a href="../karbon">Karbon 14</a>
+      </p>
+
+<?php include("footer.inc"); ?>
