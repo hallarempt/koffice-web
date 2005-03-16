150150e75a00fcae9921b8c8ffd7eacec5cf5072
dfaure <dfaure@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-03-16 10:02:37 +0000

    Fixed /krayon 404 with a link to krita
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@398037 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ krayon/index.php
@@ -0,0 +1,10 @@
+<?php
+  $page_title='Krayon';
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+      <p>Krayon is now known as <a href="../krita">Krita</a></p>
+
+<?php include("footer.inc"); ?>
