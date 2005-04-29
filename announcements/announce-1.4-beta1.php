009f9076c7dbbd8db1cfcf4c2f9e081deda04676
dfaure <dfaure@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-04-29 10:49:34 +0000

    updated contacts
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@408579 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- announcements/announce-1.4-beta1.php
+++ announcements/announce-1.4-beta1.php
@@ -438,53 +438,77 @@ coming days and weeks.
   </font>
 </p>
 <hr />
-<table border="0" cellpadding="8" cellspacing="0">
-  <tr>
-    <th colspan="2" align="left">
-      Press Contacts:
-    </th>
-  </tr>
-  <tr valign="top">
-    <td align="right" nowrap="nowrap">
-      United&nbsp;States:
-    </td>
-    <td nowrap="nowrap">
-      Andreas Pour<br />
-      KDE League, Inc.<br />
-      pour AT kdeleague DOT org<br />
-      (1) 917 312 3122
-    </td>
-  </tr>
-  <tr valign="top">
-    <td align="right" nowrap="nowrap">
-      Europe (French and English):
-    </td>
-    <td nowrap="nowrap">
-      David Faure<br />
-      faure@kde.org<br />
-      (33) 4 3250 1445
-    </td>
-  </tr>
-  <tr valign="top">
-    <td align="right" nowrap="nowrap">
-      Europe (German and English):
-    </td>
-    <td nowrap="nowrap">
-      Ralf Nolden<br />
-      nolden@kde.org<br />
-      (49) 2421 502758
-    </td>
-  </tr>
-  <tr valign="top">
-    <td align="right" nowrap="nowrap">
-      Europe (English):
-    </td>
-    <td nowrap="nowrap">
-      Jono Bacon<br />
-      jono@kde.org
-    </td>
-  </tr>
-</table>
+<h2>Press Contacts</h2>
+<table cellpadding="10"><tr valign="top">
+<td>
+
+<b>Africa</b><br />
+Uwe Thiem<br />
+P.P.Box 30955<br />
+Windhoek<br />
+Namibia<br />
+Phone: +264 - 61 - 24 92 49<br />
+<a href="&#109;a&#105;l&#116;o:&#105;&#110;fo-&#0097;&#0102;r&#105;&#99;a&#x40;k&#100;e.&#111;&#x72;g">info-africa kde.org</a><br />
+</td>
+
+<td>
+<b>Asia</b><br />
+Sirtaj S. Kang <br />
+C-324 Defence Colony <br />
+New Delhi <br />
+India 110024 <br />
+Phone: +91-981807-8372 <br />
+<a href="ma&#0105;&#108;to&#00058;inf&#00111;-&#97;&#115;&#x69;a&#x40;kde.or&#x67;">info-asia kde.org</a>
+</td>
+
+</tr>
+<tr valign="top">
+
+<td>
+<b>Europe</b><br />
+Matthias Kalle Dalheimer<br />
+Rysktorp<br />
+S-683 92 Hagfors<br />
+Sweden<br />
+Phone: +46-563-540023<br />
+Fax: +46-563-540028<br />
+<a href="m&#x61;il&#0116;o&#x3a;i&#x6e;fo-&#00101;&#00117;rope&#64;k&#x64;&#x65;&#00046;o&#x72;&#00103;">info-europe kde.org</a>
+</td>
+
+<td>
+<b>North America</b><br />
+George Staikos <br />
+889 Bay St. #205 <br />
+Toronto, ON, M5S 3K5 <br />
+Canada<br />
+Phone: (416)-925-4030 <br />
+<a href="&#109;ai&#x6c;&#x74;&#x6f;&#0058;i&#x6e;&#0102;o&#0045;no&#0114;t&#104;&#0097;m&#x65;&#x72;i&#x63;&#x61;&#x40;k&#x64;e&#46;&#0111;&#x72;&#x67;">info-northamerica kde.org</a><br />
+</td>
+
+</tr>
+
+<tr>
+<td>
+<b>Oceania</b><br />
+Hamish Rodda<br />
+11 Eucalyptus Road<br />
+Eltham VIC 3095<br />
+Australia<br />
+Phone: (+61)402 346684<br />
+<a href="&#109;&#x61;&#x69;&#x6c;&#x74;o:&#105;&#x6e;fo&#45;&#x6f;c&#101;&#x61;&#110;ia&#064;kde&#00046;org">info-oceania kde.org</a><br />
+</td>
+
+<td>
+<b>South America</b><br />
+Helio Chissini de Castro<br />
+R. Jos&eacute; de Alencar 120, apto 1906<br />
+Curitiba, PR 80050-240<br />
+Brazil<br />
+Phone: +55(41)262-0782 / +55(41)360-2670<br />
+<a href="ma&#105;&#x6c;&#116;&#x6f;&#x3a;&#0105;&#110;&#102;&#x6f;-&#00115;&#111;ut&#104;&#97;&#x6d;e&#0114;&#x69;ca&#0064;&#107;d&#x65;.&#111;r&#x67;">info-southamerica kde.org</a><br />
+</td>
+
+</tr></table>
 
 <?php
   include("footer.inc");
