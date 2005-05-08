4e8066158eae865d4062f980065120af5be9e285
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-05-08 22:44:58 +0000

    Remove the "RSS Valid" logo from the page, as the news feed has not validated for month.
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@411333 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- developer/news.php
+++ developer/news.php
@@ -10,12 +10,6 @@
   kde_general_news_rss2("./news.rdf", 25, false);
 ?>
 
-<p>
-<a href="http://feedvalidator.org/check?url=http://kde.org/areas/koffice/developer/news.rdf">
- <img src="../pics/valid-rss.png" alt="[Valid RSS]" title="Validate my RSS feed" width="88" height="31" />
-</a>
-</p>
-
 <?php
   // Footer
   include("footer.inc");
