1de0c3ef04f629941e655c04731245812033e901
goutte <goutte@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-05-14 17:06:42 +0000

    One way to redirect to Karbon is enough. (If I have well understood, W3C recommends server re-directions for improved Web Accessibility.)
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@413812 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- 404.php
+++ 404.php
@@ -11,6 +11,7 @@ $handler->add("/signatures.phtml", "/download/signatures.php");
 $handler->add("/support.phtml", "/support");
 $handler->add("/supporters.phtml", "/support/supporters.php");
 $handler->add("/filters/status.php", "/developer/filters/status.php");
+$handler->add("/killustrator/index.php", "/karbon/index.php");
 $handler->add("/kontour/index.php", "/karbon/index.php");
 $handler->add("/filters/status.phtml", "/filters/1.2");
 $handler->add("/developer/news.rdf", "/news_rss2.php");
