10043bff749fb54fe132ca4f6743b9ccded75284
mueller <mueller@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-06-03 09:20:10 +0000

    obscore at david's request
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@421498 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- people.php
+++ people.php
@@ -13,7 +13,7 @@
       <p>
       If you have a question or if you want to discuss about KOffice,
       please write an email to the
-      <a href="mailto:koffice@kde.org">KOffice mailing list!</a>
+      <a href="&#0109;&#x61;il&#116;o&#0058;koff&#105;&#0099;e&#x40;&#00107;d&#101;&#46;&#111;rg">KOffice mailing list!</a>
       <a href="http://lists.kde.org">(Archives.)</a>
       If you have any compiling problem, please have a look at the
       <a href="/faq/">FAQs</a> first.
@@ -28,144 +28,144 @@
       <h2>Current maintainers by project</h2>
       
       <p>
-      <b>KWord: <a href="mailto:zander@kde.org">Thomas Zander</a>,&nbsp; 
-      <a href="mailto:srhaque@iee.org">Shaheed Haque</a>, &nbsp;<a href="mailto:faure@kde.org">David Faure</a>,&nbsp;
-      <a href="mailto:montel@kde.org">Laurent Montel</a></b><br />
-      <b>KSpread &amp; KChart: <a href="mailto:montel@kde.org">Laurent Montel</a></b><br />
-      <b>KPresenter: <a href="mailto:lukas@kde.org">Luk&aacute;&scaron; Tinkl</a></b><br />
-      <b>Krita: <a href="mailto:boud@valdyas.org">Boudewijn Rempt</a></b><br />
-      <b>KOffice Library: <a href="mailto:faure@kde.org">David Faure</a>,&nbsp;
-       <a href="mailto:trobin@kde.org">Werner Trobin</a></b><br />      
-      <b>KFormula: <a href="mailto:rizzi@kde.org">Andrea Rizzi</a>,&nbsp;<a
-	  href="mailto:ulrich.kuettler@mailbox.tu-dresden.de">Ulrich
+      <b>KWord: <a href="&#x6d;ail&#116;&#0111;&#58;&#122;and&#x65;&#114;&#x40;k&#100;&#x65;.or&#x67;">Thomas Zander</a>,&nbsp; 
+      <a href="&#109;&#97;il&#x74;&#x6f;:&#00038;#1&#49;5&#x3b;&#38;&#00035;&#0049;1&#52;&#x3b;&#104;&#x26;#97;&#x26;#&#120;&#55;1&#x3b;&&#35;&#x30;&#x31;&#x31;&#00055;&#x3b;&#0038;&#35;01&#048;&#0049;;&#&#00120;4&#x30;;&&#x23;&#x78;6&#x39;;&#1&#x30;1&#059;&#038;&#35;&#120;6&#x35;&#x3b;&#38;&#35;&#x34;6&#00059;&#111;&#x26;#1&#x31;&#x34;;&#38;&#35;x&#x36;7&#59;">Shaheed Haque</a>, &nbsp;<a href="&#x6d;&#097;&#x69;l&#0116;&#111;:&#00102;a&#117;&#x72;e&#x40;kde&#x2e;&#x6f;&#114;g">David Faure</a>,&nbsp;
+      <a href="m&#00097;ilt&#x6f;&#058;m&#111;n&#116;el&#064;kde&#46;o&#114;&#x67;">Laurent Montel</a></b><br />
+      <b>KSpread &amp; KChart: <a href="&#109;ail&#0116;&#x6f;&#x3a;&#109;&#111;n&#116;&#101;&#x6c;&#64;kde.org">Laurent Montel</a></b><br />
+      <b>KPresenter: <a href="m&#97;&#x69;lto&#0058;&#108;uk&#97;s&#x40;k&#00100;&#x65;&#46;&#x6f;r&#0103;">Luk&aacute;&scaron; Tinkl</a></b><br />
+      <b>Krita: <a href="m&#0097;il&#116;o:b&#111;&#x75;&#100;&#64;va&#108;dy&#x61;s&#x2e;o&#114;g">Boudewijn Rempt</a></b><br />
+      <b>KOffice Library: <a href="m&#97;il&#x74;o:fa&#x75;re&#x40;kd&#x65;.&#0111;rg">David Faure</a>,&nbsp;
+       <a href="&#109;&#97;&#105;lto:&#116;&#x72;&#0111;bin&#x40;&#107;d&#101;&#046;org">Werner Trobin</a></b><br />      
+      <b>KFormula: <a href="&#x6d;&#x61;i&#00108;&#x74;o:&#x72;i&#x7a;&#x7a;i&#x40;k&#100;e.&#111;r&#103;">Andrea Rizzi</a>,&nbsp;<a
+	  href="m&#97;&#x69;lto:&#117;lri&#x63;h&#46;&#107;u&#x65;tt&#0108;e&#00114;&#00064;&#x6d;&#0097;&#105;&#108;&#x62;&#x6f;x.&#x74;u&#45;&#x64;&#114;&#x65;sde&#00110;&#00046;de">Ulrich
 	  Kuettler</a></b><br />
-      <b>Kivio: <a href="mailto:psn@linux.se">Peter Simonsson</a></b><br />
-	  <b>CVS Repository, Configure-Scripts and Makefiles: <a href="mailto:coolo@kde.org">Stephan Kulow</a></b><br />
+      <b>Kivio: <a href="m&#97;il&#x74;o&#58;&#0112;s&#0110;&#064;l&#105;n&#0117;x&#0046;&#115;&#00101;">Peter Simonsson</a></b><br />
+	  <b>CVS Repository, Configure-Scripts and Makefiles: <a href="&#0109;&#x61;ilt&#111;:c&#111;&#111;&#108;&#111;&#64;k&#x64;e.o&#00114;&#103;">Stephan Kulow</a></b><br />
       </p>
       
       <h2>All developers (including original authors and project founders)</h2>
       <p>
-      <b><a href="mailto:weis@kde.org">Torben Weis</a></b>
+      <b><a href="&#109;&#x61;&#x69;&#108;&#x74;&#x6f;&#58;w&#0101;is&#x40;k&#x64;&#101;&#x2e;o&#114;&#103;">Torben Weis</a></b>
       (KSpread, KWord, KOffice Library, KOScript, founder of the KOffice project)<br />
 
-      <b><a href="mailto:reggie@kde.org">Reginald Stadlbauer</a></b>
+      <b><a href="&#109;&#x61;i&#x6c;&#x74;&#111;:&#x72;&#0101;&#103;&#103;&#x69;e&#x40;&#0107;de.o&#114;&#x67;">Reginald Stadlbauer</a></b>
       (KPresenter, KWord, KOffice Library)<br />
 
-      <b><a href="mailto:faure@kde.org">David Faure</a></b>
+      <b><a href="mailt&#00111;:&#00102;&#0097;&#x75;&#x72;&#101;&#x40;&#00107;de.&#111;r&#00103;">David Faure</a></b>
       (KWord, KOffice Library, KSpread, CSV Filters)<br />
 
-      <b><a href="mailto:hausmann@kde.org">Simon Hausmann</a></b>
+      <b><a href="&#x6d;a&#x69;&#0108;t&#111;:&#104;&#00097;u&#115;&#x6d;an&#0110;&#0064;kd&#x65;.&#x6f;r&#103;">Simon Hausmann</a></b>
       (KOffice Library, various hacks and fixes)<br />
 
-      <b><a href="mailto:kalle@kde.org">Kalle Dalheimer</a></b>
+      <b><a href="ma&#105;l&#00116;&#111;:kall&#x65;&#0064;kd&#101;.org">Kalle Dalheimer</a></b>
       (KChart)<br />
 
-      <b><a href="mailto:coolo@kde.org">Stephan Kulow</a></b>
+      <b><a href="&#109;a&#x69;l&#116;o:coo&#0108;o&#64;&#x6b;de&#46;o&#0114;g">Stephan Kulow</a></b>
       (KFormula, Build system)<br />
 
-      <b><a href="mailto:Bilibao@ouverture.it">Andrea Rizzi</a></b>
+      <b><a href="&#109;a&#105;&#x6c;&#116;o:&#66;&#00105;l&#x69;b&#x61;&#0111;&#x40;&#111;u&#0118;&#101;r&#x74;ure.it">Andrea Rizzi</a></b>
       (KFormula)<br />
 
-      <b><a href="mailto:kus@iti.cs.uni-magdeburg.de">Kai-Uwe Sattler</a></b>
+      <b><a href="&#x6d;a&#x69;lto&#x3a;kus&#x40;i&#00116;i&#x2e;&#99;&#x73;.&#x75;&#110;&#x69;-&#x6d;&#x61;gde&#098;urg&#46;d&#x65;">Kai-Uwe Sattler</a></b>
       (Kontour)<br />
 
-      <b><a href="mailto:flemming@kde.org">Robert Flemming</a></b>
+      <b><a href="ma&#105;&#x6c;t&#111;:&#102;&#108;e&#x6d;&#109;ing&#064;&#107;&#100;e&#x2e;org">Robert Flemming</a></b>
       (Icons/Graphic)<br />
 
-      <b><a href="mailto:jost@schenck.de">Jost Schenck</a></b>
+      <b><a href="&#109;ai&#0108;t&#x6f;&#58;j&#x6f;&#x73;&#x74;&#0064;sc&#x68;&#101;&#x6e;&#00099;k&#46;&#00100;&#x65;">Jost Schenck</a></b>
       (Icons/Graphics)<br />
 
-      <b><a href="mailto:uhl@in.tum.de">Martin Uhl</a></b>
+      <b><a href="&#0109;a&#x69;l&#x74;&#111;&#0058;uh&#108;&#64;i&#x6e;&#46;t&#x75;m.d&#101;">Martin Uhl</a></b>
       (Icons/Graphics)<br />
 
-      <b><a href="mailto:koch@kde.org">Michael Koch</a></b>
+      <b><a href="m&#00097;ilt&#111;:&#107;o&#99;&#104;&#x40;&#x6b;&#100;e&#046;o&#x72;g">Michael Koch</a></b>
       (KImage and Krita)<br />
 
-      <b><a href="mailto:me@kde.org">Matthias Elter</a></b>
+      <b><a href="ma&#x69;&#108;&#x74;&#x6f;:&#109;e&#x40;&#107;&#x64;e&#x2e;&#111;rg">Matthias Elter</a></b>
       (Krita)<br />
 
-      <b><a href="mailto:physajr@phys.canterbury.ac.nz">Andrew Richards</a></b>
+      <b><a href="&#x6d;&#0097;&#0105;&#108;&#x74;&#00111;:&#x70;&#x68;ys&#x61;jr&#x40;p&#x68;y&#x73;.ca&#x6e;&#116;&#101;rb&#x75;r&#x79;&#x2e;&#97;c.nz">Andrew Richards</a></b>
       (Krita)<br />
 
-      <b><a href="mailto:carpdjih@sp.zrz.tu-berlin.de">Carsten Pfeiffer</a></b>
+      <b><a href="m&#x61;&#105;l&#x74;&#x6f;&#x3a;&#x63;a&#0114;&#0112;&#x64;&#106;&#x69;&#x68;&#64;sp&#00046;zrz&#x2e;t&#117;&#045;ber&#x6c;in&#x2e;&#x64;e">Carsten Pfeiffer</a></b>
       (Krita)<br />
 
-      <b><a href="mailto:percy@eris23.de">Percy Leonhardt</a></b>
+      <b><a href="m&#0097;i&#x6c;t&#111;:&#x70;&#x65;&#114;&#x63;&#x79;&#064;e&#x72;&#105;&#115;23.&#100;&#x65;">Percy Leonhardt</a></b>
       (MS Excel(tm) import filter for KSpread)<br />
 
-      <b><a href="mailto:trobin@kde.org">Werner Trobin</a></b>
+      <b><a href="&#109;ai&#108;&#x74;o&#x3a;&#x74;&#114;&#0111;&#98;&#00105;n&#x40;kde.or&#x67;">Werner Trobin</a></b>
       (Import/Export Filters, KOffice Library, graphite)<br />
 
-      <b><a href="mailto:montel@kde.org">Laurent Montel</a></b>
+      <b><a href="m&#00097;ilt&#111;:&#x6d;&#x6f;n&#00116;&#101;l&#x40;&#x6b;de&#x2e;or&#00103;">Laurent Montel</a></b>
       (KSpread, KChart, KPresenter, KWord)<br />
 
-      <b><a href="mailto:srhaque@iee.org">Shaheed Haque</a></b>
+      <b><a href="&#109;&#0097;il&#116;&#111;&#58;s&#114;&#0104;aq&#0117;e&#0064;i&#00101;&#0101;.&#111;&#x72;&#x67;">Shaheed Haque</a></b>
       (MS Word(tm) Import Filter, KWord)<br />
 
-      <b><a href="mailto:zander@kde.org">Thomas Zander</a></b>
+      <b><a href="m&#00097;&#x69;&#0108;to&#x3a;za&#0110;&#0100;&#x65;&#x72;&#64;kde&#46;o&#0114;g">Thomas Zander</a></b>
       (KWord)<br />
 
-      <b><a href="mailto:jcaliff@compuzone.net">John Califf</a></b>
+      <b><a href="&#109;ai&#108;t&#111;:&#00106;ca&#00108;&#x69;&#00102;f&#64;c&#00111;mp&#117;&#122;&#x6f;n&#x65;.net">John Califf</a></b>
       (Krita, KWord GUI enhancements)<br />
 
-      <b><a href="mailto:rm@linux.ru.net">Igor Janssen</a></b>
+      <b><a href="m&#x61;&#0105;l&#116;o:&#0114;m&#00064;linu&#0120;.&#114;u.&#110;&#0101;t">Igor Janssen</a></b>
       (Kontour)<br />
 
-      <b><a href="mailto:fujioka@kde.org">Toshitaka Fujioka</a></b>
+      <b><a href="&#109;a&#105;&#x6c;t&#x6f;&#x3a;&#102;&#x75;&#106;&#105;o&#x6b;a&#00064;kd&#101;.&#x6f;&#x72;&#00103;">Toshitaka Fujioka</a></b>
       (KPresenter)<br />
 
-      <b><a href="mailto:sven@kde.org">Sven L&uuml;ppken</a></b>
+      <b><a href="ma&#x69;lt&#x6f;&#58;&#115;ve&#110;&#00064;&#107;&#x64;&#101;&#x2e;&#x6f;r&#00103;">Sven L&uuml;ppken</a></b>
       (KWord)<br />
 
-      <b><a href="mailto:ariya@kde.org">Ariya Hidayat</a></b>
+      <b><a href="&#x6d;&#97;i&#x6c;&#0116;o:&#97;&#114;&#00105;ya&#x40;&#x6b;de.&#0111;&#x72;g">Ariya Hidayat</a></b>
       (Filters, KSpread)<br />
 
-      <b><a href="mailto:goutte@kde.org">Nicolas Goutte</a></b>
+      <b><a href="m&#97;&#105;&#x6c;to:g&#111;utte&#x40;&#107;&#0100;e.&#x6f;rg">Nicolas Goutte</a></b>
       (various KWord filters, picture support of the KOffice library)<br />
 
-      <b><a href="mailto:jowenn@kde.org">Joseph Wenninger</a></b>
+      <b><a href="m&#x61;i&#x6c;to&#58;&#x6a;o&#x77;&#101;n&#110;&#x40;k&#00100;&#x65;.o&#0114;&#103;">Joseph Wenninger</a></b>
        (KWord Mail merge)<br />
 
-      <b><a href="mailto:rjacolin@ifrance.com">Robert Jacolin</a></b>
+      <b><a href="m&#00097;&#105;&#108;&#0116;&#111;&#58;&#114;jac&#x6f;&#108;&#0105;&#110;&#0064;i&#x66;r&#97;nce.&#99;&#x6f;&#00109;">Robert Jacolin</a></b>
       (Latex filters, XSLT filter)<br />
 
-      <b><a href="mailto:nandres@web.de">Norbert Andres</a></b>
+      <b><a href="m&#x61;&#0105;&#x6c;to:n&#97;&#x6e;&#x64;re&#115;&#64;&#119;e&#x62;&#x2e;&#100;&#00101;">Norbert Andres</a></b>
       (KSpread)<br />
 
-      <b><a href="mailto:dailey@vt.edu">John Dailey</a></b>
+      <b><a href="m&#x61;ilt&#0111;&#x3a;&#x64;ai&#108;e&#0121;&#64;vt&#0046;&#0101;d&#117;">John Dailey</a></b>
       (KSpread)<br />
 
-      <b><a href="mailto:dang at kde dot org">Clarence Dang</a></b>
+      <b><a href="&#109;&#97;&#0105;&#x6c;to:d&#x61;&#110;&#103;&#064;&#107;d&#x65;.&#111;rg">Clarence Dang</a></b>
       (MS Write filters for KWord, various hacks, GUI enhancements)<br />
 
-      <b><a href="mailto:philipp.mueller@gmx.de">Philipp Mueller</a></b>
+      <b><a href="m&#097;i&#108;to&#x3a;&#0112;h&#105;&#x6c;&#x69;&#x70;p&#x2e;mue&#108;l&#101;&#114;&#x40;&#103;m&#x78;.&#x64;&#00101;">Philipp Mueller</a></b>
       (KSpread)<br />
 
-      <b><a href="mailto:daniel.naber@t-online.de">Daniel Naber</a></b>
+      <b><a href="&#0109;&#x61;&#00105;l&#00116;o&#00058;d&#x61;n&#x69;&#00101;l.n&#97;&#00098;&#x65;&#x72;&#64;t-&#111;nli&#110;&#x65;.&#100;e">Daniel Naber</a></b>
       (KThesaurus)<br />
     
-      <b><a href="mailto:phillipezolt@hotmail.com">Phillip Ezolt</a></b>
+      <b><a href="mai&#x6c;t&#0111;&#0058;&#x70;&#x68;i&#x6c;l&#0105;p&#x65;zo&#0108;t&#00064;h&#x6f;tm&#97;i&#108;.c&#0111;m">Phillip Ezolt</a></b>
       (KSpread import and export filter for gnumeric)<br />
 
-      <b><a href="mailto:eva@kde.org">Eva Brucherseifer</a></b>
+      <b><a href="&#00109;&#x61;&#105;&#00108;to&#x3a;&#0101;&#118;a&#64;k&#x64;&#101;.org">Eva Brucherseifer</a></b>
       (KSpread export filter for html) <br />
 
-      <b><a href="mailto:GrahShrt@netscape.net">Graham Short</a></b>
+      <b><a href="&#x6d;&#97;&#0105;&#108;to&#x3a;Gra&#0104;&#x53;&#104;rt&#64;&#0110;e&#x74;scape&#x2e;&#x6e;et">Graham Short</a></b>
       (KSpread import filter for Quattro Pro)<br />
 
-      <b><a href="mailto:ennobartels@t-online.de">Enno Bartels</a></b>
+      <b><a href="m&#x61;&#x69;&#00108;&#116;&#111;:&#00101;&#110;&#00110;o&#98;a&#00114;tel&#115;&#x40;&#x74;-&#x6f;n&#0108;ine&#x2e;&#00100;&#x65;">Enno Bartels</a></b>
       (KWord, Kontour, KSpread import filter for applixword, -spreadsheet, -graphics)<br />
             
-      <b><a href="mailto:lukas@kde.org">Luk&aacute;&scaron; Tinkl</a></b>
+      <b><a href="&#x6d;&#x61;il&#116;&#x6f;&#x3a;lu&#107;&#097;&#x73;&#x40;k&#x64;&#x65;&#x2e;&#00111;&#114;g">Luk&aacute;&scaron; Tinkl</a></b>
       (KPresenter, KSpread, filters)<br />
 
-      <b><a href="mailto:psn@linux.se">Peter Simonsson</a></b>
+      <b><a href="ma&#0105;lt&#x6f;:p&#115;&#110;&#064;li&#x6e;u&#120;&#x2e;&#x73;e">Peter Simonsson</a></b>
       (Kivio, Kexi)<br />
       
-      <b><a href="mailto:js @ iidea . pl">Jaroslaw Staniek</a></b>
+      <b><a href="mailt&#x6f;&#x3a;j&#115; &#x40;&#00032;i&#0105;dea &#x2e; p&#x6c;">Jaroslaw Staniek</a></b>
       (Kexi)<br />
 
-      <b><a href="mailto:f_edemar@linux.se">Fredrik Edemar</a></b>
+      <b><a href="&#109;&#x61;&#x69;&#x6c;&#116;&#00111;:&#102;&#x5f;&#101;demar&#x40;l&#x69;&#110;ux.&#115;e">Fredrik Edemar</a></b>
       (KWord, KPresenter, KSpread, KChart)<br />
 </p>
 
