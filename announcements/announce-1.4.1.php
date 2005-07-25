209fa2738b03fb3c10abba770f2bb4975f31e956
raphael <raphael@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-07-25 07:46:54 +0000

    Initial version of the KOffice 1.4.1 announcement, basically complete. Maybe some more linking and link updating in the About KOffice and KDE section.
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@438503 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ announcements/announce-1.4.1.php
@@ -0,0 +1,210 @@
+<?php
+  $page_title ='KOffice 1.4.1 Announcement';
+  $site_root = '../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+<p align="justify">
+  DATELINE JUNE 25, 2005<br />
+  DRAFT VERSION, DO NOT USE UNTIL ANNOUNCED!
+</p>
+<h2>
+  The KDE Project Ships KOffice 1.4.1 - a Free Integrated Office Suite
+</h2>
+<p align="justify">
+<strong>
+Improved Overall Stability, Better OASIS OpenDocument Support,
+Krita gains Performance and the KOffice Workspace gets a new Icon.
+</strong>
+</p>
+<p align="justify">July 25, 2005 (The INTERNET).
+The <a href="http://www.kde.org/">KDE Project</a> today announced the
+immediate release of KOffice 1.4.1 for Linux and Unix operating systems.
+KOffice is an integrated set of free office applications based on the
+<a href="http://www.kde.org/awards/">award winning</a> KDE desktop.
+KOffice is a light-weight yet complete and feature rich office solution
+and provides a variety of filters to interoperate with other popular
+office suites such as OpenOffice.org and Microsoft Office. Among
+various bugfixes and improvements, the KOffice 1.4.1 release further
+improves support for the
+<a href="http://en.wikipedia.org/wiki/OpenDocument">OASIS OpenDocument</a>
+file format.
+</p>
+
+<h3>KOffice 1.4.1 consists of the following applications:</h3>
+<ul>
+<li><a href="../kword/">KWord</a> - A frame-based word processor</li>
+<li><a href="../kspread/">KSpread</a> - An advanced spreadsheet application</li>
+<li><a href="../kpresenter/">KPresenter</a> - A full-featured presentation program</li>
+<li><a href="../kivio/">Kivio</a> - A Visio®-style flowcharting application</li>
+<li><a href="../krita/">Krita</a> - A pixel based image manipulation application</li>
+<li><a href="../kexi/">Kexi</a> - An integrated data management application</li>
+<li><a href="../karbon/">Karbon14</a> - A vector drawing application</li>
+<li><a href="../kugar/">Kugar</a> - A tool for generating business quality reports</li>
+<li><a href="../kchart/">KChart</a> - An integrated graph and chart drawing tool</li>
+<li><a href="../kformula/">KFormula</a> - A powerful formula editor</li>
+</ul>
+<p>
+This full set of components and excellent desktop integration makes KOffice
+one of the most complete office suites available.
+</p>
+<h3>
+Major Improvements in the KOffice 1.4.1 release are:
+</h3>
+<ul>Improved OASIS OpenDocument Support, especially in KWord and KPresenter</ul>
+<ul>Crash fixes for Kexi, Krita, KWord and KSpread</ul>
+<ul>Krita Performance Improvements</ul>
+<ul>Various other bugfixes</ul>
+<li>
+</ul>
+<p>
+Please take a look at the 
+<a href="http://www.koffice.org/announcements/changelog-1.4.1.php">list of
+changes</a> to see all the improvements made to KOffice since the
+<a href="http://www.koffice.org/announcements/announce-1.4.php">1.4 release</a>.
+</p>
+
+<h3>Getting KOffice 1.4.1</h3>
+<p>
+Download information can be found on the KOffice website.
+</p>
+
+<h4>
+  About KOffice and KDE
+</h4>
+<p align="justify">
+  <a href="http://www.koffice.org">KOffice</a> is part of the KDE Project.
+  <a href="http://www.kde.org">KDE</a> is an <a href="http://www.kde.org/awards/">award-winning</a>,
+  independent <a href="http://www.kde.org/people/">project of hundreds</a> of developers, translators,
+  artists and other professionals worldwide collaborating over the Internet
+  to create and freely distribute a sophisticated, customizable and stable
+  desktop and office environment employing a flexible, component-based,
+  network-transparent architecture and offering an outstanding development
+  platform.<p>
+
+<p align="justify">
+  KDE provides a stable, mature desktop including a state-of-the-art browser
+  (<a href="http://konqueror.kde.org/">Konqueror</a>), a personal information
+  management suite (<a href="http://kontact.org/">Kontact</a>), a full
+  office suite (<a href="http://www.koffice.org/">KOffice</a>), a large
+  set of networking application and utilities, and an
+  efficient, intuitive development environment featuring the excellent IDE
+  <a href="http://www.kdevelop.org/">KDevelop</a>.</p>
+
+<p align="justify">
+  KDE is working proof
+  that the Open Source "Bazaar-style" software development model can yield
+  first-rate technologies on par with and superior to even the most complex
+  commercial software.
+</p>
+<p align="justify">
+  <font size="2">
+  <em>Release Coordinator</em>:  Thanks to David Faure
+  for his excellent services as release coordinator.
+  </font>
+</p>
+<hr />
+<p align="justify">
+  <font size="2">
+  <em>Press Release</em>:
+  Written by <a href="mailto:raphael.langerhorst AT kdemail DOT net">Raphael
+  Langerhorst</a>.
+<!--  [Translated into _____________ by ______________ .] -->
+  </font>
+</p>
+<p align="justify">
+  <font size="2">
+  <em>Trademarks Notices.</em>
+
+  KDE, K Desktop Environment and KOffice are trademarks of KDE e.V.
+  Incorporated.
+
+  WordPerfect is a registered trademarks of Corel Corporation or Corel Corporation Limited.
+
+  Linux is a registered trademark of Linus Torvalds.
+
+  Trolltech and Qt are trademarks of Trolltech AS.
+
+  UNIX is a registered trademark of The Open Group.
+
+  All other trademarks and copyrights referred to in this announcement are
+  the property of their respective owners.
+  </font>
+</p>
+<hr />
+<h2>Press Contacts</h2>
+<table cellpadding="10"><tr valign="top">
+<td>
+
+<b>Africa</b><br />
+Uwe Thiem<br />
+P.P.Box 30955<br />
+Windhoek<br />
+Namibia<br />
+Phone: +264 - 61 - 24 92 49<br />
+<a href="&#109;a&#105;l&#116;o:&#105;&#110;fo-&#0097;&#0102;r&#105;&#99;a&#x40;k&#100;e.&#111;&#x72;g">info-africa kde.org</a><br />
+</td>
+
+<td>
+<b>Asia</b><br />
+Sirtaj S. Kang <br />
+C-324 Defence Colony <br />
+New Delhi <br />
+India 110024 <br />
+Phone: +91-981807-8372 <br />
+<a href="ma&#0105;&#108;to&#00058;inf&#00111;-&#97;&#115;&#x69;a&#x40;kde.or&#x67;">info-asia kde.org</a>
+</td>
+
+</tr>
+<tr valign="top">
+
+<td>
+<b>Europe</b><br />
+Matthias Kalle Dalheimer<br />
+Rysktorp<br />
+S-683 92 Hagfors<br />
+Sweden<br />
+Phone: +46-563-540023<br />
+Fax: +46-563-540028<br />
+<a href="m&#x61;il&#0116;o&#x3a;i&#x6e;fo-&#00101;&#00117;rope&#64;k&#x64;&#x65;&#00046;o&#x72;&#00103;">info-europe kde.org</a>
+</td>
+
+<td>
+<b>North America</b><br />
+George Staikos <br />
+889 Bay St. #205 <br />
+Toronto, ON, M5S 3K5 <br />
+Canada<br />
+Phone: (416)-925-4030 <br />
+<a href="&#109;ai&#x6c;&#x74;&#x6f;&#0058;i&#x6e;&#0102;o&#0045;no&#0114;t&#104;&#0097;m&#x65;&#x72;i&#x63;&#x61;&#x40;k&#x64;e&#46;&#0111;&#x72;&#x67;">info-northamerica kde.org</a><br />
+</td>
+
+</tr>
+
+<tr>
+<td>
+<b>Oceania</b><br />
+Hamish Rodda<br />
+11 Eucalyptus Road<br />
+Eltham VIC 3095<br />
+Australia<br />
+Phone: (+61)402 346684<br />
+<a href="&#109;&#x61;&#x69;&#x6c;&#x74;o:&#105;&#x6e;fo&#45;&#x6f;c&#101;&#x61;&#110;ia&#064;kde&#00046;org">info-oceania kde.org</a><br />
+</td>
+
+<td>
+<b>South America</b><br />
+Helio Chissini de Castro<br />
+R. Jos&eacute; de Alencar 120, apto 1906<br />
+Curitiba, PR 80050-240<br />
+Brazil<br />
+Phone: +55(41)262-0782 / +55(41)360-2670<br />
+<a href="ma&#105;&#x6c;&#116;&#x6f;&#x3a;&#0105;&#110;&#102;&#x6f;-&#00115;&#111;ut&#104;&#97;&#x6d;e&#0114;&#x69;ca&#0064;&#107;d&#x65;.&#111;r&#x67;">info-southamerica kde.org</a><br />
+</td>
+
+</tr></table>
+
+<?php
+  include("footer.inc");
+?>
