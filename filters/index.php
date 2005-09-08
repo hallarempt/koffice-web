2cecc97e2111366db5d7a2b080b0054d109434c5
martin <martin@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-09-08 18:59:44 +0000

    Make a link to the KOffice 1.4 filters information on the main filters page.  Explain what filters are.
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@458654 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- filters/index.php
+++ filters/index.php
@@ -6,24 +6,45 @@
 ?>
 
     <p>Welcome to the KOffice Filter Information Page.</p>
-    <p>Various filters are available for most of the KOffice applications.</p>
+
+    <h2>About Filters</h2>
+    
+    <p>Filters are used to convert documents from one <em>file format</em> into
+       another.  In KOffice, they serve two purposes:</p>
+    <ul>
+      <li>to convert KOffice documents into formats that can be read by other 
+          computer programs;</li>
+      <li>to convert documents created in other programs into KOffice
+          documents.</li>
+    </ul>
+
+    <p>For example, if you have documents that were created in a different office
+       package, filters may be allow you to load those documents into a KOffice
+       application.  It is not normally necessary to worry about filters, because
+       they are used automatically when opening or saving a document
+       in different formats.</p>
+
+    <p>Various filters are available for most of the KOffice applications.
+       For more information on which file formats KOffice supports, see the 
+       filter status for recent versions of KOffice below.</p>
    
-    <h2>Stable versions</h2>
+    <h2>Current (stable) versions</h2>
+    
+     <ul>
+      <li><a href="1.4">Filter status of KOffice 1.4</a></li>
+     </ul>
+    
+    <h2>Old versions</h2>
     
      <ul>
       <li><a href="1.2">Filter status of KOffice 1.3</a></li>
       <li><a href="1_3_1">Filter status of KOffice 1.3.1</a></li>
      </ul>
     
-    <h2>Stable branches</h2>
-    
-     <ul>
-      <li><a href="1_3_1"><!-- No special status for the branch currently -->Filter status of KOffice 1.3.x</a></li>
-     </ul>
-
     <h2>Developer Information</h2>
-    <p><a href="../developer/filters">The developer information about the
-    filters were moved.</a></p>
+    <p>We're always looking for developers to help improve the KOffice
+       filters.  There is <a href="../developer/filters">more information</a>
+       for interested developers.</p>
    
 <?php
     include("footer.inc");
