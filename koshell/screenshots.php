d4b453e420e0f20558007980837ed858ead9cd55
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-09-17 19:58:03 +0000

    add screenshot page
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@461497 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ koshell/screenshots.php
@@ -0,0 +1,23 @@
+<?php
+  $title="KOffice Workspace Screenshots";
+  $page_title = 'KOffice Workspace - Screenshots';
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+<?php
+$gallery = new ImageGallery("KOffice Workspace Screenshots");
+$gallery->addImage("pics/koshell1_sm.png", "pics/koshell1.png", "350", "325", "[Screenshot]", 0 , "KOffice Workspace at start");
+$gallery->addImage("pics/koshell2_sm.png", "pics/koshell2.png", "350", "253", "[Screenshot]", 0 , "Karbon14 in KOffice Workspace");
+$gallery->show();
+?>
+
+ <br />
+ <hr width="30%" align="center" />
+ <p>
+ Last update: <?php echo date ("Y-m-d", filemtime(__FILE__)); ?>
+ </p>
+<?php include("footer.inc"); ?>
+
+
