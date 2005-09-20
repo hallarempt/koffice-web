fb53186c3f3444be558b6724ab52839bd12b155b
martin <martin@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-09-20 21:51:33 +0000

    Give KPlato it's icon and new page structure.
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@462377 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ kplato/more_info.php
@@ -0,0 +1,13 @@
+<?php
+  $page_title="About KPlato";
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+<?php
+  include("kplato.inc");
+  $appinfo->show();
+?>
+
+<?php include("footer.inc"); ?>
