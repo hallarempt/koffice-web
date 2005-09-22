07cc228d660fdcbc0b74e76311adba93337dab84
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-09-22 18:46:57 +0000

    Karbon14 pages update
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@463021 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- karbon/index.php
+++ karbon/index.php
@@ -6,23 +6,24 @@
 ?>
 
 <?php
-  $appinfo = new AppInfo("Karbon14");
-  $appinfo->setIcon( "pics/hi48-app-karbon.png", "48", "48");
-  $appinfo->setVersion($karbonversion);
-  $appinfo->setLicense("gpl");
-  $appinfo->setCopyright("1998", "the Karbon Team");
-  $appinfo->addAuthor("Rob Buis", "buis&#x40;&#x6b;&#100;e.or&#103;");
-  $appinfo->addAuthor("Lenny Kudling", ""); // Email removed at developer's request
-  $appinfo->addContributor("Rob Buis", "b&#117;i&#x73;&#064;k&#100;&#101;&#0046;org");
-  $appinfo->addContributor("Lenny Kudling", ""); // Email removed at developer's request
-  $appinfo->addContributor("Benoit Vautrin", "benoit.&#118;a&#117;t&#114;&#x69;n&#00064;free&#46;f&#114;");
-  $appinfo->addContributor("Tomislav Lukman", "tomislav.l&#117;kma&#0110;&#064;c&#107;&#x2e;h&#x69;&#110;e&#x74;.h&#0114;");
-  $appinfo->show();
+   include("karbon.inc");
+   $appinfo->showIconAndCopyright();
 ?>
 
-<h2>Description</h2>
+<p>Karbon14 is the scalable vector drawing application in KDE.</p>
 
-<p>Karbon14 is the vector drawing application in KDE.</p>
+        <center>
+        <?php
+        $gallery = new ImageGallery("Karbon - Screenshot");
+        $gallery->addImage("pics/karbon_sm.png", "pics/karbon.png", 330, 232,  "[Screenshot]", "", "Karbon14 importing a svg file");
+        $gallery->show();
+        ?>
+        </center>
 
+ <br />
+ <hr width="30%" align="center" />
+ <p>
+ Last update: <?php echo date ("Y-m-d", filemtime(__FILE__)); ?>
+ </p>
 <?php include("footer.inc"); ?>
 
---
+++ karbon/more_info.php
@@ -0,0 +1,13 @@
+<?php
+  $page_title="About Karbon14";
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+<?php
+  include("karbon.inc");
+  $appinfo->show();
+?>
+
+<?php include("footer.inc"); ?>
