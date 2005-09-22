7639d26064bdef1b0e09654503e40bf57e35c0b6
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-09-22 14:52:05 +0000

    improve
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@462965 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- kpresenter/index.php
+++ kpresenter/index.php
@@ -6,25 +6,10 @@
 ?>
 
 <?php
-  $appinfo = new AppInfo("KPresenter");
-  $appinfo->setIcon( "pics/hi48-app-kpresenter.png", "48", "48");
-  $appinfo->setVersion($kpresenterversion);
-  $appinfo->setLicense("lgpl");
-  $appinfo->setCopyright("1998", "the KPresenter Team");
-  $appinfo->addAuthor("Reginald Stadlbauer", "r&#101;g&#0103;&#x69;e&#x40;kde.o&#00114;&#x67;");
-  $appinfo->addContributor("David Faure", "fa&#117;&#00114;e&#64;kd&#0101;.&#x6f;rg");
-  $appinfo->addContributor("Laurent Montel", "&#109;&#00111;ntel&#x40;&#107;&#00100;&#00101;.or&#x67;");
-  $appinfo->addContributor("Wernet Trobin", "&#116;robi&#x6e;&#064;&#107;&#x64;&#0101;.o&#114;g");
-  $appinfo->addContributor("Toshitaka Fujioka", "fuj&#105;ok&#0097;&#0064;kde.or&#103;");
-  $appinfo->addContributor("Luk&aacute;&scaron; Tinkl", "l&#x75;&#107;a&#00115;&#064;kd&#101;&#x2e;org");
-  $appinfo->addContributor("Thorsten Zachmann", "t.zac&#104;m&#097;n&#110;&#x40;&#x7a;a&#103;&#x67;e.de");
-  $appinfo->addContributor("Ariya Hidayat", "a&#114;iya&#64;kd&#101;.&#111;&#x72;&#x67;");
-  $appinfo->addContributor("Percy Leonhardt", "p&#x65;r&#x63;&#121;&#64;er&#105;&#x73;23.de");
-  $appinfo->show();
+  include("kpresenter.inc");
+  $appinfo->showIconAndCopyright();
 ?>
 
-
-<h2>Description</h2>
     <p>KPresenter is a presentation application. Its features include:</p>
 
     <ul>
@@ -46,6 +31,14 @@
     <li>using XML as the document format;</li>
     <li>a Presentations Structure Viewer</li>
     </ul>
+
+        <center>
+        <?php
+        $gallery = new ImageGallery("KPresenter- Screenshot");
+        $gallery->addImage("pics/kpresenter_sm.png", "pics/kpresenter.png", 300, 203,  "[Screenshot]", "", "KPresenter");
+        $gallery->show();
+        ?>
+        </center>
 
     <p>You can find some of the presentations that KPresenter has been used for: <a href="http://www.kde.org/kdeslides/">http://www.kde.org/kdeslides/</a>.</p>
 
---
+++ kpresenter/more_info.php
@@ -0,0 +1,13 @@
+<?php
+  $page_title="About KPresenter";
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+<?php
+  include("kpresenter.inc");
+  $appinfo->show();
+?>
+
+<?php include("footer.inc"); ?>
