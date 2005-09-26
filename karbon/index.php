767b6522769164d15ecafcbf63f8ffdddc6dec90
martin <martin@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-09-26 19:25:15 +0000

    Add a description for Karbon14. List Rob Buis as maintainer.
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@464274 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- karbon/index.php
+++ karbon/index.php
@@ -23,6 +23,32 @@
     $gallery->show();
   ?>
 </center>
+<p>Karbon is a <em>vector-based</em> drawing application for KOffice: it allows
+you to create complex drawings without losing image quality when you zoom in
+on, or resize, the drawing. Ideas can be quickly and easily transformed into
+high quality drawings with Karbon.</p>
+
+<p>What makes a <em>vector-based</em> drawing application like Karbon different
+to a <em>pixel-based</em> drawing application like
+<a href="../krita/">Krita</a>?
+
+<p>In a vector drawing - such as the drawing shown above - your drawing is
+stored as geometric shapes such as lines and curves.  When the drawing is
+resized, the image scales smoothly.</p>
+
+<p>However, in a pixel-based drawing, your drawing is stored as many dots
+(pixels) in rows and columns.  When you resize a pixel-based image, the dots
+will be enlarged. This means that the quality of the image will go down.
+Pixel-based drawings often have larger file sizes compared to simple vector
+drawings.</p>
+
+<p>For example: Karbon is useful for creating and editing clip-art which
+often needs to be resized to fit in a document or on a poster, whereas Krita is
+more suited to painting pictures, or photo manipulation.</p>
+
+<p>To get started with Krita quickly, you might want to try opening
+and modifying some of the images from the
+<a href="http://www.openclipart.org/">Open Clip Art Library</a>.</p>
 
  <br />
  <hr width="30%" align="center" />
