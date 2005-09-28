c24c095e62d81de8d9f65683a83a5d42739e112f
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-09-28 00:51:55 +0000

    refresh screenshot
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@464643 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- kchart/screenshots.php
+++ kchart/screenshots.php
@@ -7,8 +7,8 @@
 
 <?php
 $gallery = new ImageGallery("KChart Screenshots");
-$gallery->addImage("pics/kdiagramm_sm.png", "pics/kdiagramm.png", "190", "139", "[Screenshot]", 0 , "Some pie charts");
 $gallery->addImage("pics/bar3D_2005_09_270.png", "pics/bar3D_2005_09.png", "270", "207", "[Screenshot]", 0 , "A basic 3D bar chart");
+$gallery->addImage("pics/polar_2005_09_270.png", "pics/polar_2005_09.png", "270", "201", "[Screenshot]", 0 , "A polar chart");
 $gallery->show();
 ?>
 
