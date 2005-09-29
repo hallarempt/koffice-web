a601833903d6298a18207a3cdd2ac74ac031feb0
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-09-29 12:17:23 +0000

    improve information about additional templates
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@465319 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- addons/index.php
+++ addons/index.php
@@ -7,7 +7,9 @@
 
 <p>This sections currently contains various addons for KOffice which are not included in the main distribution.</p>
 
-<h2><a name="templates">Templates</a></h2>
+<h2><a name="templates">KWord Templates</a></h2>
+
+<p>This package contains 7 KWord templates: InterofficeMemo.kwt, PayrollMasterRecordForm.kwt, Resume.kwt, SimpleLayout.kwt, Memo.kwt, Questionnaire.kwt and  ShortLetter.kwt.</p>
 
 <p><strong>Download:</strong> <a href="templates.tar.gz">Download template archive (46KB)</a></p>
 
@@ -26,6 +28,12 @@ cp -a Additional `kde-config --localprefix`share/apps/kword/templates
 </pre>
 
 <p>In subsequent versions of KOffice we are going to provide more templates with the main program, and provide an easier method for installation of additional templates.</p>
+
+<h2>kde-files.org templates</h2>
+
+<p>You can find additional templates for KOffice applications at <a href="http://kde-files.org/index.php?xcontentmode=610x611x612x613x614x615">kde-files.org</a>. You download these templates and out them in your home directory for example.</p>
+
+<p>You are encouraged to share your own templates and upload them on kde-files.org. Many thanks in advance!</p>
 
 <h2><a name="thesauri">Thesauri</a></h2>
 
@@ -51,6 +59,11 @@ compatible with KOffice is available for download at the following site:</p>
     <li>The Open Clip Art Libary at <a href="http://www.openclipart.org">www.openclipart.org</a></li>
 </ul>
 
+ <br />
+ <hr width="30%" align="center" />
+ <p>
+ Last update: <?php echo date ("Y-m-d", filemtime(__FILE__)); ?>
+ </p>
 <?php
   include("footer.inc");
 ?>
