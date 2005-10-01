4f0750e1b185b7bc4e372a4187b7460802a7daeb
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-10-01 20:12:06 +0000

    include Alan improvements, sorry for having been so long!
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@466170 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- karbon/index.php
+++ karbon/index.php
@@ -23,13 +23,15 @@
     $gallery->show();
   ?>
 </center>
-<p>Karbon is a <em>vector-based</em> drawing application for KOffice: it allows
-you to create complex drawings without losing image quality when you zoom in
-on, or resize, the drawing. Ideas can be quickly and easily transformed into
-high quality drawings with Karbon.</p>
+<p>Karbon is a <em>vector-based</em> drawing application for KOffice. It allows
+artists to create complex drawings without losing image quality when zooming in
+on, or resizing  the drawing. You can use Karbon to add finishing
+touches to diagrams created using <a href="../kivio/">Kivio</a> or charts created using <a href="../kchart/">KChart</a>.
+Graphic design ideas can be quickly and easily transformed into high
+quality illustrations with Karbon.</p>
 
 <p>What makes a <em>vector-based</em> drawing application like Karbon different
-to a <em>pixel-based</em> drawing application like
+from a <em>pixel-based</em> drawing application like
 <a href="../krita/">Krita</a>?
 
 <p>In a vector drawing - such as the drawing shown above - your drawing is
@@ -38,13 +40,15 @@ resized, the image scales smoothly.</p>
 
 <p>However, in a pixel-based drawing, your drawing is stored as many dots
 (pixels) in rows and columns.  When you resize a pixel-based image, the dots
-will be enlarged. This means that the quality of the image will go down.
+will be enlarged. This can result in jagged edges known as pixelation and aliasing and
+thus the quality of the image will go down.
 Pixel-based drawings often have larger file sizes compared to simple vector
 drawings.</p>
 
-<p>For example: Karbon is useful for creating and editing clip-art which
-often needs to be resized to fit in a document or on a poster, whereas Krita is
-more suited to painting pictures, or photo manipulation.</p>
+<p>For example: Karbon is useful for creating cartoons drawings or editing
+clip art which often needs to be resized to fit in a document or on a
+poster.  <a href="../krita">Krita</a> is more suited to painting pictures, or editing
+photographs</p>
 
 <p>To get started with Karbon quickly, you might want to try opening
 and modifying some of the images from the
