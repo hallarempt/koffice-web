b826f18fb6949ad888d8d15788e9e018a2c11cc1
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-10-02 20:30:41 +0000

    add screenshot on main page
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@466504 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- kplato/index.php
+++ kplato/index.php
@@ -9,16 +9,29 @@
   $appinfo->showIconAndCopyright();
 ?>
 
-      <p><b>Description:</b><br />
+      <p>
          KPlato is a project management application, allowing for the
          planning and scheduling of projects.  It is in the very early
          stages of development.
       </p>
+
+<center>
+  <?php
+    $gallery = new ImageGallery("KPlato- Screenshot");
+    $gallery->addImage("pics/plain_2005_09_330.png",
+                       "pics/plain_2005_09.png",
+                       330, 247,
+                       "[Screenshot]", "",
+                       "KPlato with the plain template");
+    $gallery->show();
+  ?>
+</center>
 
       <p><b>Mailing List:</b><br />
          The KPlato mailing list is at <a href="&#109;&#97;i&#108;&#x74;o&#x3a;&#0107;p&#0108;ato&#x40;kd&#x65;.&#x6f;r&#0103;">&#x6b;&#x70;l&#x61;t&#111;&#64;k&#00100;e&#x2e;&#0111;rg</a><br />
          Subscription information can be found <a href="http://mail.kde.org/mailman/listinfo/kplato">here</a>.<br />
          Online archives at: <a href="http://lists.kde.org/?l=kplato&amp;r=1&amp;w=2">lists.kde.org</a>
       </p>
+
 
 <?php include("footer.inc"); ?>
