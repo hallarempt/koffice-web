089fce1d2c749e82c7d56c8e9b4c5952387e96a1
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-10-02 13:33:41 +0000

    improve
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@466377 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- kexi/index.php
+++ kexi/index.php
@@ -10,13 +10,20 @@
   $appinfo->showIconAndCopyright();
 ?>
 
+<div id="quicklinks">
+[
+  <a href="#project">The project</a> |
+  <a href="#version">Current version</a>
+]
+</div>
+
 <p>
 Kexi is an integrated data management application. It can be used for creating database schemas, inserting data, performing queries, and processing data. Forms can be created to provide a custom interface to your data. All database objects - tables, queries and forms - are stored in the database, making it easy to share data and design.
 </p>
 
 <center>
   <?php
-    $gallery = new ImageGallery("Karbon - Screenshot");
+    $gallery = new ImageGallery("Kexi - Screenshot");
     $gallery->addImage("pics/sampleDatabase_2005_09_330.png",
                        "pics/sampleDatabase_2005_09.png",
                        330, 237,
@@ -27,14 +34,14 @@ Kexi is an integrated data management application. It can be used for creating d
 </center>
 
 <p>
-As a real member of the KDE and KOffice projects, Kexi integrates fluently into both. It is designed to be fully usable also without KDE on Linux/Unix and MS Windows platforms (Mac OS X version planned).
+As a real member of the KDE and KOffice projects, Kexi integrates fluently into both. It is also designed to be fully usable without KDE on Linux/Unix and MS Windows platforms (Mac OS X version planned).
 </p>
 
 <p>
 Kexi is considered as a long awaited Open Source competitor for MS Access, Filemaker and Oracle Forms. Its development is motivated by the lack of Rapid Application Development (RAD) tools for database systems that are sufficiently powerful, inexpensive, open standards driven and portable across many operating systems and hardware platforms. 
 </p>
 
-<h2>The Project</h2>
+<h2><a name="project">The Project</a></h2>
 
 <p>
 Kexi Project, started in 2002, is developed by <a href="http://www.kexi-project.org/people.html">Kexi Team</a> - an international group of independent developers, since February 2003 with assistance and support from the <a href="http://www.openoffice.com.pl">OpenOffice Polska</a> company.
@@ -45,7 +52,7 @@ The major communication channel in Kexi development is <a href="irc://irc.freeno
 </p>
 
 
-<h2>Current version</h2>
+<h2><a name="version">Current version</a></h2>
 <p>
 There are two kinds of Kexi release. 
 </p>
