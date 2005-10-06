df59711c87f5b803a19bf4d8b96805921869dcc2
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-10-06 15:28:41 +0000

    add a templates page to inform the user about sharing his own templates
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@467850 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ kpresenter/templates.php
@@ -0,0 +1,46 @@
+<?php
+  $page_title="KPresenter Templates";
+  $site_root='../';
+  include("koffice.inc"); 
+  include("header.inc");
+?> 
+<div id="quicklinks">
+[
+  <a href="#get">Get new templates</a> |
+  <a href="#share">Share your templates</a>
+]
+</div>
+
+<h2><a name="get">Get more KPresenter templates</a></h2> 
+      <p>
+      KPresenter ships with some templates but you can get some more.
+      </p>
+      <p>You can download templates on the <a href="http://kde-files.org/index.php?xcontentmode=612">kde-files.org KPresenter webpage</a>. </p>
+
+
+<p><strong>Installation:</strong></p>
+
+<p>For a system wide installation (the templates will be available to all users on the system):</p>
+
+<pre>
+cp template_name $KDEDIR/share/apps/kpresenter/templates
+</pre>
+
+<p>For a single user installation:</p>
+
+<pre>mkdir -p `kde-config --localprefix`share/apps/kpresenter/templates
+cp template_name.kpr `kde-config --localprefix`share/apps/kpresenter/templates
+</pre>
+
+<h2><a name="share">Share your own KPresenter templates</a></h2>
+<p>You are encouraged to share your own templates and upload them on <a href="http://kde-files.org/index.php?xcontentmode=612">kde-files.org, in the KPresenter section</a>. Many thanks in advance!</p>
+<p>Create your own templates in KPresenter using the <tt>File</tt> -&gt; <tt>Template manager</tt> menu. Read more information in the online handbook, in the <a href="http://docs.kde.org/development/en/koffice/kpresenter/creating-templates.html">Creating KPresenter Templates</a> section.</p>
+
+<br />
+ <br />
+ <hr width="30%" align="center" />
+ <p>
+ Last update: <?php echo date ("Y-m-d", filemtime(__FILE__)); ?>
+ </p>
+<?php include("footer.inc"); ?>
+
