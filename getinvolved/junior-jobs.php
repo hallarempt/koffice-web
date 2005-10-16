ca18721d71a3eb163d0f51646e7b82d10cbf8cd3
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-10-16 20:37:36 +0000

    start a Junior Jobs page
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@471216 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ getinvolved/junior-jobs.php
@@ -0,0 +1,36 @@
+<?php
+  $page_title="Junior Jobs 2005";
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+    <p>Junior Jobs - October &amp; November targets</p>
+
+<p>
+<table cellspacing="0" cellpadding="3" border="1">
+  <tr>
+    <th></th>
+    <th>Task Description</th>
+    <th>Contact</th>
+    <th>Date Target</th>
+    <th>Name of Applican</th>
+  </tr>
+  <tr>
+    <th></th>
+    <th>Kpresenter QWhatsThis help in Configure KPrsenter dialog</th>
+    <th>Anne-Marie Mahfouf</th>
+    <th>30th November 2005</th>
+    <th>-</th>
+  </tr>
+</table>
+
+<br />
+ <br />
+ <hr width="30%" align="center" />
+ <p>
+ Last update: <?php echo date ("Y-m-d", filemtime(__FILE__)); ?>
+ </p>
+<?php
+  include("footer.inc");
+?>
