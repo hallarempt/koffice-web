da4af0704394632588b188c962c19659e88c0417
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-10-26 17:34:22 +0000

    add Karbon JJs
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@474543 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- getinvolved/junior-jobs.php
+++ getinvolved/junior-jobs.php
@@ -25,7 +25,7 @@
     <td><a href="http://bugs.kde.org/show_bug.cgi?id=112933">http://bugs.kde.org/show_bug.cgi?id=112933</a></td>
     <td><a href="&#x6d;ail&#116;&#0111;&#58;&#122;and&#x65;&#114;&#x40;k&#100;&#x65;.or&#x67;">Thomas Zander</a></td>
     <td>10th December 2005</td>
-    <td>-</td>
+    <td><a href="mailto:maju7@o2.pl">maju</a></td>
   </tr>
   <tr>
     <td>Add QWhatsThis help to the chart configuration dialog in <b>KChart</b></td>
@@ -43,6 +43,30 @@
   <tr>
     <td><b>Kexi</b>: Fix all "unused parameter" warnings, by commenting out parameter names in function definitions; for example, change <tt>void MyClass::method( int unusedArg ) {//...}</tt><br /> to<br /> <tt>void MyClass::method( int unusedArg ) {Q_UNUSED( unusedArg ); //...}</tt></td>
     <td><a href="mailto:js@iidea.pl">Jarosław Staniek</a></td>
+    <td>10th December 2005</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td><b>Karbon</b>: <a href="http://bugs.kde.org/show_bug.cgi?id=109189">http://bugs.kde.org/show_bug.cgi?id=109189</a></tt></td>
+    <td><a href="mailto:buis@kde.org">Rob Buis</a></td>
+    <td>10th December 2005</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td><b>Karbon</b>:  adding tootips <a href="http://bugs.kde.org/show_bug.cgi?id=112680">http://bugs.kde.org/show_bug.cgi?id=112680</a></tt></td>
+    <td><a href="mailto:buis@kde.org">Rob Buis</a></td>
+    <td>10th December 2005</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td><b>Karbon</b>:  add image export to Karbon SVG filter <a href="http://bugs.kde.org/show_bug.cgi?id=113014">http://bugs.kde.org/show_bug.cgi?id=113014</a></tt></td>
+    <td><a href="mailto:buis@kde.org">Rob Buis</a></td>
+    <td>10th December 2005</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td><b>Karbon</b>:  fix units <a href="http://bugs.kde.org/show_bug.cgi?id=115010">http://bugs.kde.org/show_bug.cgi?id=115010</a></tt></td>
+    <td><a href="mailto:tbscope@gmail.com">Tim Beaulen</a></td>
     <td>10th December 2005</td>
     <td>-</td>
   </tr>
