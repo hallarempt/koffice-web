a7dc6e6e65a35ba9b6d73019cd596693bddb66c7
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-10-26 12:21:49 +0000

    improve
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@474447 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- getinvolved/junior-jobs.php
+++ getinvolved/junior-jobs.php
@@ -16,7 +16,7 @@
     <th>Name of Applicant</th>
   </tr>
   <tr>
-    <td>Add Kpresenter QWhatsThis help in Configure KPresenter dialog</td>
+    <td>Add <b>Kpresenter</b> QWhatsThis help in Configure KPresenter dialog</td>
     <td><a href="mailto:annemarie.mahfouf@free.fr">Anne-Marie Mahfouf</a></td>
     <td>10th December 2005</td>
     <td>-</td>
@@ -28,20 +28,20 @@
     <td>-</td>
   </tr>
   <tr>
-    <td>Add QWhatsThis help to the chart configuration dialog in KChart</td>
+    <td>Add QWhatsThis help to the chart configuration dialog in <b>KChart</b></td>
     <td><a href="mailto:raphael.langerhorst@kdemail.net">Raphael Langerhorst</a> or
 	<a href="mailto:inge@lysator.liu.se">Inge Wallin</a></td>
     <td>10th December 2005</td>
     <td>-</td>
   </tr>
   <tr>
-    <td>Add QWhatsThis help to the cell format configuration dialog in KSpread</td>
+    <td>Add QWhatsThis help to the cell format configuration dialog in <b>KSpread</b></td>
     <td><a href="mailto:raphael.langerhorst@kdemail.net">Raphael Langerhorst</a></td>
     <td>10th December 2005</td>
     <td>-</td>
   </tr>
   <tr>
-    <td>Kexi: Fix all "unused parameter" warnings, by commenting out parameter names in function definitions; for example, change void MyClass::method( int unusedArg ) {//...} to void MyClass::method( int unusedArg ) {
+    <td><b>Kexi</b>: Fix all "unused parameter" warnings, by commenting out parameter names in function definitions; for example, change void MyClass::method( int unusedArg ) {//...} to void MyClass::method( int unusedArg ) {
         Q_UNUSED( unusedArg ); //...}</td>
     <td><a href="mailto:js@iidea.pl">Jarosław Staniek</a></td>
     <td>10th December 2005</td>
