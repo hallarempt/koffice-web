0ed1d89ee9379532e962e92e018c5d8f0c232969
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-10-27 21:24:33 +0000

    group per app and add 2 more
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@475003 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- getinvolved/junior-jobs.php
+++ getinvolved/junior-jobs.php
@@ -16,35 +16,10 @@
     <th>Name of Applicant</th>
   </tr>
   <tr>
-    <td>Add <b>Kpresenter</b> QWhatsThis help in Configure KPresenter dialog</td>
-    <td><a href="mailto:annemarie.mahfouf@free.fr">Anne-Marie Mahfouf</a></td>
-    <td>10th December 2005</td>
-    <td>-</td>
-  </tr>
-  <tr>
     <td><a href="http://bugs.kde.org/show_bug.cgi?id=112933">http://bugs.kde.org/show_bug.cgi?id=112933</a></td>
     <td><a href="&#x6d;ail&#116;&#0111;&#58;&#122;and&#x65;&#114;&#x40;k&#100;&#x65;.or&#x67;">Thomas Zander</a></td>
     <td>10th December 2005</td>
     <td><a href="mailto:maju7@o2.pl">maju</a></td>
-  </tr>
-  <tr>
-    <td>Add QWhatsThis help to the chart configuration dialog in <b>KChart</b></td>
-    <td><a href="mailto:raphael.langerhorst@kdemail.net">Raphael Langerhorst</a> or
-	<a href="mailto:inge@lysator.liu.se">Inge Wallin</a></td>
-    <td>10th December 2005</td>
-    <td>-</td>
-  </tr>
-  <tr>
-    <td>Add QWhatsThis help to the cell format configuration dialog in <b>KSpread</b></td>
-    <td><a href="mailto:raphael.langerhorst@kdemail.net">Raphael Langerhorst</a></td>
-    <td>10th December 2005</td>
-    <td>-</td>
-  </tr>
-  <tr>
-    <td><b>Kexi</b>: Fix all "unused parameter" warnings, by commenting out parameter names in function definitions; for example, change <tt>void MyClass::method( int unusedArg ) {//...}</tt><br /> to<br /> <tt>void MyClass::method( int unusedArg ) {Q_UNUSED( unusedArg ); //...}</tt></td>
-    <td><a href="mailto:js@iidea.pl">Jarosław Staniek</a></td>
-    <td>10th December 2005</td>
-    <td>-</td>
   </tr>
   <tr>
     <td><b>Karbon</b>: <a href="http://bugs.kde.org/show_bug.cgi?id=109189">http://bugs.kde.org/show_bug.cgi?id=109189</a></tt></td>
@@ -73,6 +48,43 @@
   <tr>
     <td><b>Karbon</b>:  Usability: The color and opacity bars in the color chooser palette are backwards. <a href="http://bugs.kde.org/show_bug.cgi?id=115213">http://bugs.kde.org/show_bug.cgi?id=115213</a></tt></td>
     <td><a href="mailto:tbscope@gmail.com">Tim Beaulen</a></td>
+    <td>10th December 2005</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td><b>KChart</b>: Add QWhatsThis help to the chart configuration dialog.</td>
+    <td><a href="mailto:raphael.langerhorst@kdemail.net">Raphael Langerhorst</a> or
+	<a href="mailto:inge@lysator.liu.se">Inge Wallin</a></td>
+    <td>10th December 2005</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td><b>Kexi</b>: Fix all "unused parameter" warnings, by commenting out parameter names in function definitions; for example, change <tt>void MyClass::method( int unusedArg ) {//...}</tt><br /> to<br /> <tt>void MyClass::method( int unusedArg ) {Q_UNUSED( unusedArg ); //...}</tt></td>
+    <td><a href="mailto:js@iidea.pl">Jarosław Staniek</a></td>
+    <td>10th December 2005</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td>Add <b>Kpresenter</b> QWhatsThis help in Configure KPresenter dialog</td>
+    <td><a href="mailto:annemarie.mahfouf@free.fr">Anne-Marie Mahfouf</a></td>
+    <td>10th December 2005</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td><b>KPresenter</b>:  Usability: Most basic resizing operation for pictures are not possible. <a href="http://bugs.kde.org/show_bug.cgi?id=112310">http://bugs.kde.org/show_bug.cgi?id=112310</a></tt></td>
+    <td><a href="mailto:montel@kde.org">Laurent Montel</a></td>
+    <td>10th December 2005</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td><b>KPresenter</b>:  Usability: Make "Keep Ratio" for pictures easiert to reach. <a href="http://bugs.kde.org/show_bug.cgi?id=112306">http://bugs.kde.org/show_bug.cgi?id=112306</a></tt></td>
+    <td><a href="mailto:montel@kde.org">Laurent Montel</a></td>
+    <td>10th December 2005</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td><b>KSpread</b>: Add QWhatsThis help to the cell format configuration dialog.</td>
+    <td><a href="mailto:raphael.langerhorst@kdemail.net">Raphael Langerhorst</a></td>
     <td>10th December 2005</td>
     <td>-</td>
   </tr>
