fa2d4097079b81e6e7974dab0c9bfa2c7b6abb82
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-10-29 19:24:54 +0000

    improve
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@475576 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- download/unstable.php
+++ download/unstable.php
@@ -6,10 +6,11 @@
 ?>
 <p>KOffice unstable is the next coming KOffice 1.5 version. You might want to get it in order to test it or to develop on it or to write docs. This is for advanced users.
 </p>
-<p>You need qt >3.3.0 + headers (devel packages if you use qt from your distribution), kdelibs >= 3.4.0 + headers (devel packages if you use kdelibs from your distribution), arts >=1.4
+<p>You need qt >3.3.0 + headers (devel or dev packages if you use qt from your distribution), kdelibs >= 3.4.0 + headers (devel or dev packages if you use kdelibs from your distribution), arts >=1.4
 </p>
 <p><tt>svn</tt> is in the subversion package from your distribution.</p>
 <p>Type <tt>configure --help</tt> if you want to use configure options.</p>
+<p>After you finish <tt>./configure</tt>, look at the output and install all missing packages (+ devel or dev)  mentioned and re-run  <tt>./configure</tt>.</p>
 
 <pre>
     $ svn co svn://anonsvn.kde.org/home/kde/trunk/koffice
