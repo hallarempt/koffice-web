5e907007fdd6582bd5fc89930d227f086f25e253
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-11-10 21:06:56 +0000

    improve
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@479534 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- getinvolved/howto-write-doc.php
+++ getinvolved/howto-write-doc.php
@@ -1,26 +1,19 @@
 <?php
-  $page_title="Docs Junior Jobs 2005";
+  $page_title="HowTo write KOffice Documentation";
   $site_root='../';
   include("koffice.inc");
   include("header.inc");
 ?>
 
-    <p>January 2006 targets</p>
-<p>It is prefered to use KOffice svn trunk code to work on JJ, which is the next coming 1.5 release. You can find some information on how to build it on the <a href="../download/unstable.php">Install KOffice unstable from svn page</a>.</p>
-<p><a href="mailto:raphael.langerhorst@kdemail.net">Raphael Langerhorst</a> is the documentation coordinator for KOffice documentation.</p>
+<p>Here are some tips about your editor.
 
-<p>
-<table cellspacing="0" cellpadding="3" border="1">
-  <tr>
-    <th>Task Description</th>
-    <th>Contact</th>
-    <th>Date Target</th>
-    <th>Name of Applicant</th>
-  </tr>
-</table>
+<ul>
+    <li>set your editor to wrap lines at 80 characters</li>
+    <li>open and save as utf8 in your editor</li>
+    <li>choose the docbook syntax highlighting</li>
+</p>
 
-<p>If you want to take a JJ, please email your name to the contact and CC to <a href="mailto:annemarie.mahfouf@free.fr">Anne-Marie</a> so the task can be assigned to you.<br />
-When you complete the task, please either attach the patch to the bug report or send a patch to your JJ contact name or send it to the KOffice mailing list.</p>
+<p>Screenshots: theme, style and color in KControl</p>
 
 <br />
  <br />
