d4b79f647b5bfd7bcc2759c6332b07a071267a65
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-11-16 21:01:36 +0000

    assign myself this task as I started it
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@480853 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- getinvolved/junior-jobs.php
+++ getinvolved/junior-jobs.php
@@ -109,7 +109,7 @@
     <td><b>KSpread</b>: Add QWhatsThis help to the Configure dialog and update doc section at the same time. <a href="http://bugs.kde.org/show_bug.cgi?id=115926">http://bugs.kde.org/show_bug.cgi?id=115926</a></td>
     <td><a href="mailto:raphael.langerhorst () kdemail ! net">Raphael Langerhorst</a></td>
     <td>10th December 2005</td>
-    <td>-</td>
+    <td>Anne-Marie Mahfouf</td>
   </tr>
 </table>
 
