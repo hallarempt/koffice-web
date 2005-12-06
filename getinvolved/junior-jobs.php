206a090b01a93d1609674ddc568a95e4333f1d0f
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-12-06 13:09:50 +0000

    update
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@485999 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- getinvolved/junior-jobs.php
+++ getinvolved/junior-jobs.php
@@ -90,19 +90,19 @@
   <tr>
     <td><b>KPresenter</b>:  Usability: Make "Keep Ratio" for pictures easiert to reach. <a href="http://bugs.kde.org/show_bug.cgi?id=112306">http://bugs.kde.org/show_bug.cgi?id=112306</a></td>
     <td><a href="mailto:montel () kde ! org">Laurent Montel</a></td>
-    <td>10th December 2005</td>
     <td><b>DONE</b></td>
+    <td>Thomas Zander</td>
   </tr>
   <tr>
     <td><b>KSpread</b>: change all "document" reference to "workbook" in GUI, tooltips, qwhatsthis and doc. <a href="http://bugs.kde.org/show_bug.cgi?id=116450">http://bugs.kde.org/show_bug.cgi?id=116450</a></td>
     <td><a href="mailto:robertknight () gmail ! com">Robert Knight</a></td>
-    <td>10th December 2005</td>
+    <td>on hold</td>
     <td>Henry Kay</td>
   </tr>
   <tr>
     <td><b>KSpread</b>: Add QWhatsThis help to the cell format configuration dialog.</td>
     <td><a href="mailto:raphael.langerhorst () kdemail ! net">Raphael Langerhorst</a></td>
-    <td>10th December 2005</td>
+    <td>in good progress - 10th December 2005</td>
     <td>Anne-Marie Mahfouf</td>
   </tr>
   <tr>
