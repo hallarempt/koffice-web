ad768e03dbb64a66b7e41dc17fd8447ec1ebf2af
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-12-22 20:06:04 +0000

    improve
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@490690 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- competition/guiKOffice2.php
+++ competition/guiKOffice2.php
@@ -16,13 +16,18 @@
 
 <h3>Deadline</h3>
 <p>All submissions should be sent by <b>15th February 2006</b> 11:59pm GMT to
-<a href="mailto:koffice-competition AT kde DOT org">koffice-competition@kde.org</a>. Please note that only the judges will see those submissions.</p>
+<a href="mailto:koffice-competition AT kde
+DOT org">koffice-competition@kde.org</a>. Please note that only the judges will
+see those submissions.</p>
+<p>You can send more than one entry.</p>
+<p>You can also send an updated version of your submission before the deadline!</p>
 
-<h3>Submission format</h3> 
-<p>The submissions should be in pdf or/and odt (Open Document text) format. See
+<h3>Submission Format</h3> 
+<p>The submissions should be in PDF or/and ODT (Open Document Text) format. See
 the examples below.<br />
 <p>Along with your mockup design you will add a description of your GUI and
 stress out the major points of your design by explaining their strengths.</p>
+<p>Please note that the current svn version of KWord can handle ODT!</p>
 
 <h3>Example</h3>
 Please see the example in each format:<br />
@@ -37,9 +42,9 @@ details in your application!
 <p>The judges will be picked from the KOffice developers. Currently, Inge Wallin and Boudewijn Rempt are appointed.</p>
 <p>Date of announcement of the winner: <b>March 1st 2005</b></p>
 
-<h3>Submitted material</h3>
+<h3>Submitted Material</h3>
 <p>All ideas, graphical elements, workflows, etc, should be free to be used in
-KOffice 2.0.</p>
+KOffice 2.0 and published under the GNU General Public License, GPL.</p>
 
 <p>You can use IRC on network <tt>irc.freenode.org</tt> port 6667, channel
 <tt>#koffice</tt> for all questions you might have.
