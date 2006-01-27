14ea5448e4e4d322ef2c03dca359274c0742d9b0
psn <psn@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-01-27 18:49:38 +0000

    oops
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@502993 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- kivio/index.php
+++ kivio/index.php
@@ -18,8 +18,8 @@
 <p><center>
   <?php
     $gallery = new ImageGallery("Kivio-Screenshot");
-    $gallery->addImage("pics/kivio_svn0601027_330.png",
-                       "pics/kivio_svn0601027.png",
+    $gallery->addImage("pics/kivio_svn060127_330.png",
+                       "pics/kivio_svn060127.png",
                        330, 248,
                        "[Screenshot]", "",
                        "Kivio");
