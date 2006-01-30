2459d476c3f6db6a3d0cecbf9e8b840c0b87aeae
ingwa <ingwa@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-01-30 22:16:57 +0000

    Add info about kplato
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@503976 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- announcements/changelog-1.5beta1.php
+++ announcements/changelog-1.5beta1.php
@@ -19,7 +19,7 @@
 </ul>
 
 <h3><a name="kword">KWord</a></h3>
-<em>New features:</em>
+<em>Features:</em>
 <ul>
 </ul>
 
@@ -35,6 +35,17 @@
 <ul>
 </ul>
 
+<h3><a name="kspread">KSpread</a></h3>
+<ul>
+</ul>
+
+<em>Features:</em>
+<ul>
+</ul>
+
+<em>New functions:</em><br />
+
+
 <h3><a name="kpresenter">KPresenter</a></h3>
 <em>Features:</em>
 <ul>
@@ -44,21 +55,6 @@
 <ul>
 </ul>
 
-
-<h3><a name="kspread">KSpread</a></h3>
-<ul>
-</ul>
-
-<em>New functions:</em><br />
-
-
-<h3><a name="kchart">KChart</a></h3>
-<ul>
- <li>OpenDocument is now the default file format (not working in the beta)</li>
- <li>new chart type: combined bars and lines (not working in the beta)</li>
- <li>support for first row or column as labels</li>
- <li>support for changing area when charting data from KSpread</li>
-</ul>
 
 <h3><a name="kivio">Kivio</a></h3>
 <ul>
@@ -79,11 +75,50 @@
 <ul>
 </ul>
 
+
 <h3><a name="krita">Krita</a></h3>
 
-<h3><a name="kformula">KFormula</a></h3>
-
 <h3><a name="karbon">Karbon</a></h3>
+
+<h3><a name="kplato">KPlato</a></h3>
+<em>Features:</em>
+<ul>
+ <li>Resource management</li>
+ <li>Hierarchical calendars</li>
+ <li>Tasks with dependencies and subtasks.</li>
+ <li><em>Various charts for visualization, like:</em>
+  <ul>
+   <li>Gantt</li>
+   <li>Pert</li>
+  </ul>
+ </li>
+ <li>Reports generation</li>
+ <li><em>Project scheduling</em>
+  <ul>
+   <li>Pessimistic,</li>
+   <li>Expected,</li>
+   <li>Optimistic.</li>
+  </ul>
+ </li>
+ <li>Work breakdown structure (WBS).</li>
+ <li>Critical path and resources.</li>
+</ul>
+
+
+<h3><a name="kchart">KChart</a></h3>
+<em>Features:</em>
+<ul>
+ <li>OpenDocument is now the default file format (not working in the beta)</li>
+ <li>new chart type: combined bars and lines (not working in the beta)</li>
+ <li>support for first row or column as labels</li>
+ <li>support for changing area when charting data from KSpread</li>
+</ul>
+<em>Bugfixes:</em>
+<ul>
+ <li>All the graphics related bugs in bugs.kde.org have been fixed</li>
+</ul>
+
+<h3><a name="kformula">KFormula</a></h3>
 
 
 <h3><a name="filters">Filters</a></h3>
