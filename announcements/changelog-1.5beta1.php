258ca0c4ec3d10de0b89596acc5a480d4ef2c375
rempt <rempt@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-01-30 09:40:52 +0000

    Template changelog for 1.5 beta 1
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@503759 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ announcements/changelog-1.5beta1.php
@@ -0,0 +1,101 @@
+<?php
+  $page_title ='KOffice 1.5-beta1 Changelog';
+  $site_root = '../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+<p>
+  The following summarizes the major changes to the KOffice Suite since the
+  release of KOffice 1.4.2 (latest stable version).
+</p>
+
+<h3><a name="koffice_libs">KOffice Libraries</a></h3>
+<ul>
+</ul>
+
+<h3><a name="koshell">KOffice Workspace (koshell)</a></h3>
+<ul>
+</ul>
+
+<h3><a name="kword">KWord</a></h3>
+<em>New features:</em>
+<ul>
+</ul>
+
+<em>Changes:</em>
+<ul>
+</ul>
+
+<em>Bugfixes:</em>
+<ul>
+</ul>
+
+<em>Crashes:</em>
+<ul>
+</ul>
+
+<h3><a name="kpresenter">KPresenter</a></h3>
+<em>Features:</em>
+<ul>
+</ul>
+
+<em>Changes:</em>
+<ul>
+</ul>
+
+
+<h3><a name="kspread">KSpread</a></h3>
+<ul>
+</ul>
+
+<em>New functions:</em><br />
+
+
+<h3><a name="kchart">KChart</a></h3>
+<ul>
+</ul>
+
+<h3><a name="kivio">Kivio</a></h3>
+<ul>
+</ul>
+
+
+<h3><a name="kexi">Kexi</a></h3>
+
+<em>Overview</em>
+<ul>
+</ul>
+
+<em>Data Processing and Access</em>
+<ul>
+</ul>
+
+<em>Database Storage</em>
+<ul>
+</ul>
+
+<h3><a name="krita">Krita</a></h3>
+
+<h3><a name="kformula">KFormula</a></h3>
+
+<h3><a name="karbon">Karbon</a></h3>
+
+
+<h3><a name="filters">Filters</a></h3>
+<em>KWord:</em>
+<ul>
+</ul>
+
+<em>KPresenter:</em>
+<ul>
+</ul>
+
+<em>KSpread:</em>
+<ul>
+</ul>
+
+
+<?php
+  include("footer.inc");
+?>
