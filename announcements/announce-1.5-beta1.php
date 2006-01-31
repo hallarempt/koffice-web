712222cd1798e51b5d93287367e3eb453f79e83f
jriddell <jriddell@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-01-31 21:57:17 +0000

    Use kubuntu page, not direct link to packages
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@504374 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- announcements/announce-1.5-beta1.php
+++ announcements/announce-1.5-beta1.php
@@ -519,7 +519,7 @@ For a list of features supported by KPlato, see
 
 <ul>
   <li><a href="http://download.kde.org/unstable/koffice-1.5-beta1/SuSE/">SuSE Linux 9.3 and 10.0</a></li>
-  <li><a href="http://download.kde.org/unstable/koffice-1.5-beta1/kubuntu/">Kubuntu</a></li>
+  <li><a href="http://kubuntu.org/announcements/koffice-15beta1.php">Kubuntu</a></li>
   <li><a href="http://pkg-kde.alioth.debian.org/koffice-1.5-sarge/">Debian Sarge (these are
 packages updated to Jan. 31st, and include fixes not in beta1)</a></li>
   <li><a href="http://pkg-kde.alioth.debian.org/koffice-1.5/">Debian Sid (these are
