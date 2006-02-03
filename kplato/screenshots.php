a5203b186da4d53de76f141b4c552c0be59a008c
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-02-03 02:17:44 +0000

    add screenshot page to KPlato webpages sorry Dag it took me long because yesterday I could not view your file correctly! CCMAIL=danders@get2net.dk
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@505089 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ kplato/screenshots.php
@@ -0,0 +1,28 @@
+<?php
+  $title="KPlatoScreenshots";
+  $page_title = 'KPlato - Screenshots';
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+<?php
+$gallery = new ImageGallery("KPlato Screenshots");
+$gallery->addImage("pics/accounts_2006_01_220.png", "pics/accounts_2006_01.png", "220", "81", "[Screenshot]", 0 , "KPato: accounts view");
+$gallery->addImage("pics/taskappointments_2006_01_220.png", "pics/taskappointments_2006_01.png", "220", "128", "[Screenshot]", 0 , "Task appointments view");
+$gallery->startNewRow();
+$gallery->addImage("pics/gantt_full_2006_01_220.png", "pics/gantt_full_2006_01.png", "220", "128", "[Screenshot]", 0 , "Gantt full view");
+$gallery->addImage("pics/gantt_small_2006_01_220.png", "pics/gantt_small_2006_01.png", "220", "81", "[Screenshot]", 0 , "Gantt small view");
+$gallery->startNewRow();
+$gallery->addImage("pics/resource_2006_01_220.png", "pics/resource_2006_01.png", "220", "121", "[Screenshot]", 0 , "Resources view");
+$gallery->show();
+?>
+
+ <br />
+ <hr width="30%" align="center" />
+ <p>
+ Last update: <?php echo date ("Y-m-d", filemtime(__FILE__)); ?>
+ </p>
+<?php include("footer.inc"); ?>
+
+
