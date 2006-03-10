a5970568e125aa6ca89bb06efc5425e2b70962d9
ingwa <ingwa@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-03-10 22:23:07 +0000

    fixes
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@517410 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- releases/1.5-beta2-release.php
+++ releases/1.5-beta2-release.php
@@ -30,7 +30,7 @@ align="left">MD5&nbsp;Sum</th></tr>
 href="http://download.kde.org/unstable/koffice-1.5-beta2/src/koffice-1.4.95.tar.
 bz2">
 koffice-1.4.95.tar.bz2</a></td>
-<td><tt>insert-the-md5-sum-here</tt></td></tr>
+<td><tt>ddeca5c989e892baf4871a443fd2daea</tt></td></tr>
 </table></li>
       <li><a href="/download/">Binary packages</a></li>
       </ul>
