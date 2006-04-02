75f7875e708227133c98321250351edb3b523045
ingwa <ingwa@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-04-02 16:16:02 +0000

    remove all traces of rc1 and add text about ODF
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@525690 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- releases/1.5-release.php
+++ releases/1.5-release.php
@@ -1,23 +1,55 @@
 <?php
-  $page_title='KOffice 1.5 rc1 Release Notes';
+  $page_title='KOffice 1.5 Release Notes';
   $site_root='../';
   include("koffice.inc");
   include("header.inc");
 ?>
 
- <p>The KOffice team is proud to announce KOffice 1.5 Release Candidate 1.</p>
- 
- <h2>Announcement</h2>
+ <h1>The KOffice Announces KOffice version 1.5.</h1>
+
+ <p>
+ The KOffice team is proud to announce what might be the most important 
+ Open Source Software release for a long time: KOffice 1.5.  You might ask 
+ yourself why this is so.  Read on...
+ </p>
+
+ <h2>Native Support for the OpenDocument File Format</h2>
+<p>
+ The spreading of the <a href="http://en.wikipedia.org/wiki/OpenDocument">OASIS OpenDocument file format (ODF)</a> is widely regarded as one of the most important developments in the whole IT industry right now. It will give users world-wide the possibility to control the future of their own documents and also ensure that all documents will always be possible to read at any time in the future.
+</p>
+<p>
+ KOffice 1.5 is the second office suite to announce support for OpenDocument as the default file format, and the first one not based on the OpenOffice.org code base, thus making ODF into a true standard. KOffice 1.5 is therefore the version that lets enterprises and organizations all over the world choose the office suite that fits their needs best.</p>
+<p>
+ KOffice 1.5 offers native ODF support for the important productivity applications KWord (odt), KSpread (ods), and KPresenter (odp).  In addition to that, the supporting applications KChart (.odc) and KFormula (.odf) also supports ODF, although not fully yet.</p>
+<p>
+ Great care has been taken to ensure interoperability with other office software that also use OpenDocument, most notably OpenOffice.org.  We acknowledge, however, that the support and interoperability is not perfect yet. We hope to be able to quickly recognize and fix the incompatibilities that do exist in the upcoming 1.5.1 and 1.5.2 bugfix releases.
+</p>
+
+ <h2>Killer Applications</h2>
+ <p> 
+ </p>
+
+ <h2>The Fastest Development Team in the West</h2>
+ <p> 
+ </p>
+
+ <p> 
+ </p>
+ <p> 
+ </p>
+
+
+ <h3>Announcement</h3>
  <ul>
   <li><a href="/announcements/announce-1.5.php">Announcement</a></li>
  </ul>
  
- <h2>Changelog</h2>
+ <h3>Changelog</h3>
  <ul>
-  <li><a href="/announcements/changelog-1.5rc1.php">Changelog</a></li>
+  <li><a href="/announcements/changelog-1.5.php">Changelog</a></li>
  </ul>
 
- <h2>Download</h2>
+ <h3>Download</h3>
  <p>Both source code and binary packages are available.</p>
  <ul>
   <li>Source code of Koffice 1.5 release candidate 1: koffice-1.5.0.tar.bz2
@@ -27,13 +59,13 @@
           <th align="left">MD5&nbsp;Sum</th></tr>
       <tr><td>General</td>
           <td><a href="http://download.kde.org/unstable/koffice-1.5/src/koffice-1.5.0.tar.bz2">koffice-1.5.0.tar.bz2</a></td>
-          <td><tt>e9346977d18c67185d5b50f49ee8225b</tt></td></tr>
+          <td><tt>deadbeefdeadbeefdeadbeefdeadbeef</tt></td></tr>
      </table>
   </li>
   <li><a href="http://download.kde.org/unstable/koffice-1.5/">Binary packages</a></li>
  </ul>
 
- <h2>MD5 sums</h2>
+ <h3>MD5 sums</h3>
  <p>
  The MD5 sums for the tarballs are:
  </p>
