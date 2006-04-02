b3ff7c044b4096f30f6443822e2785d99dd979be
ingwa <ingwa@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-04-02 15:30:43 +0000

    initial revision
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@525668 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ releases/1.5-release.php
@@ -0,0 +1,47 @@
+<?php
+  $page_title='KOffice 1.5 rc1 Release Notes';
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+ <p>The KOffice team is proud to announce KOffice 1.5 Release Candidate 1.</p>
+ 
+ <h2>Announcement</h2>
+ <ul>
+  <li><a href="/announcements/announce-1.5.php">Announcement</a></li>
+ </ul>
+ 
+ <h2>Changelog</h2>
+ <ul>
+  <li><a href="/announcements/changelog-1.5rc1.php">Changelog</a></li>
+ </ul>
+
+ <h2>Download</h2>
+ <p>Both source code and binary packages are available.</p>
+ <ul>
+  <li>Source code of Koffice 1.5 release candidate 1: koffice-1.5.0.tar.bz2
+      <table border="0" cellpadding="4" cellspacing="0">
+      <tr><th align="left">Description</th>
+          <th align="left">Location</th>
+          <th align="left">MD5&nbsp;Sum</th></tr>
+      <tr><td>General</td>
+          <td><a href="http://download.kde.org/unstable/koffice-1.5/src/koffice-1.5.0.tar.bz2">koffice-1.5.0.tar.bz2</a></td>
+          <td><tt>e9346977d18c67185d5b50f49ee8225b</tt></td></tr>
+     </table>
+  </li>
+  <li><a href="http://download.kde.org/unstable/koffice-1.5/">Binary packages</a></li>
+ </ul>
+
+ <h2>MD5 sums</h2>
+ <p>
+ The MD5 sums for the tarballs are:
+ </p>
+<?php
+include "md5-koffice-1.5.inc"
+?>
+ <p>
+ Also provided: <a href="md5-pgp-koffice-1.5.txt">a PGP-signed version of the MD5 sums</a>.
+ </p>
+
+<?php include("footer.inc"); ?>
