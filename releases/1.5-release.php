87017c5d553614270d33edbcfafef691a628a30c
ingwa <ingwa@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-04-04 20:38:04 +0000

    Improved first paragraph; extracted downloading instructions onto it's own page
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@526521 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- releases/1.5-release.php
+++ releases/1.5-release.php
@@ -1,14 +1,14 @@
 <?php
-  $page_title='The KOffice Project Announces version 1.5"';
+  $page_title='Press Release: KOffice Version 1.5"';
   $site_root='../';
   include("koffice.inc");
   include("header.inc");
 ?>
 
  <p>
- The KOffice team is proud to announce what might be the most important 
- Open Source Software release for a long time: KOffice 1.5.  If you think
- that is taking things too far, then read on...
+ The KOffice team is proud to announce KOffice version 1.5. With this release,
+ KOffice starts its ascend into the office suite hall of fame in the company
+ of OpenOffice.org and MS Office.  Read on for our reason to say so...
  </p>
 
  <h2>Native Support for the OpenDocument File Format</h2>
@@ -99,30 +99,11 @@
  </ul>
 
  <h3>Download</h3>
- <p>Both source code and binary packages are available.</p>
  <ul>
-  <li>Source code of Koffice 1.5 release candidate 1: koffice-1.5.0.tar.bz2
-      <table border="0" cellpadding="4" cellspacing="0">
-      <tr><th align="left">Description</th>
-          <th align="left">Location</th>
-          <th align="left">MD5&nbsp;Sum</th></tr>
-      <tr><td>General</td>
-          <td><a href="http://download.kde.org/unstable/koffice-1.5/src/koffice-1.5.0.tar.bz2">koffice-1.5.0.tar.bz2</a></td>
-          <td><tt>deadbeefdeadbeefdeadbeefdeadbeef</tt></td></tr>
-     </table>
+  <li>The <a href="/announcements/1.5-download.php">Download</a>
+      page contains information about downloading KOffice 1.5 in source
+      and binary formats.
   </li>
-  <li><a href="http://download.kde.org/unstable/koffice-1.5/">Binary packages</a></li>
  </ul>
-
- <h3>MD5 sums</h3>
- <p>
- The MD5 sums for the tarballs are:
- </p>
-<?php
-include "md5-koffice-1.5.inc"
-?>
- <p>
- Also provided: <a href="md5-pgp-koffice-1.5.txt">a PGP-signed version of the MD5 sums</a>.
- </p>
 
 <?php include("footer.inc"); ?>
