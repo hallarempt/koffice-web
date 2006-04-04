72481407beba1cca4a00fa016667be2a5a666d5c
ingwa <ingwa@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-04-04 20:35:52 +0000

    initial revision
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@526519 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ releases/1.5-download.php
@@ -0,0 +1,37 @@
+<?php
+  $page_title='Downloading KOffice Version 1.5"';
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+ <h2>Downloading</h2>
+
+ <h3>Packages</h3>
+ <p>Both source code and binary packages are available.</p>
+ <ul>
+  <li>Source code of Koffice 1.5 release candidate 1: koffice-1.5.0.tar.bz2
+      <table border="0" cellpadding="4" cellspacing="0">
+      <tr><th align="left">Description</th>
+          <th align="left">Location</th>
+          <th align="left">MD5&nbsp;Sum</th></tr>
+      <tr><td>General</td>
+          <td><a href="http://download.kde.org/unstable/koffice-1.5/src/koffice-1.5.0.tar.bz2">koffice-1.5.0.tar.bz2</a></td>
+          <td><tt>deadbeefdeadbeefdeadbeefdeadbeef</tt></td></tr>
+     </table>
+  </li>
+  <li><a href="http://download.kde.org/unstable/koffice-1.5/">Binary packages</a></li>
+ </ul>
+
+ <h3>MD5 sums</h3>
+ <p>
+ The MD5 sums for the tarballs are:
+ </p>
+<?php
+include "md5-koffice-1.5.inc"
+?>
+ <p>
+ Also provided: <a href="md5-pgp-koffice-1.5.txt">a PGP-signed version of the MD5 sums</a>.
+ </p>
+
+<?php include("footer.inc"); ?>
