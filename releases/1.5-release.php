e5fa54fdc6ae40d3d9c89133ced3adc8e372df23
ingwa <ingwa@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-04-09 09:28:38 +0000

    make it more press releasey
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@527743 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- releases/1.5-release.php
+++ releases/1.5-release.php
@@ -14,10 +14,12 @@
  <p>
  The KOffice team is proud to announce KOffice version 1.5. With this release,
  KOffice starts its ascent into the office suite hall of fame in the company
- of OpenOffice.org and MS Office.  Read on for our reason to say so...
+ of OpenOffice.org and MS Office.  
  </p>
 
  <h2>Native Support for the OpenDocument File Format</h2>
+<p>
+ KOffice was the first to support, and now with 1.5 the second office suite to announce full support for OpenDocument as the default file format. It is also the first one not based on the OpenOffice.org code base, which makes ODF into a true industry standard. KOffice 1.5 is therefore the version that lets enterprises and organizations all over the world choose the office suite that fits their needs best.</p>
 <p>
  The spread of the <a href="http://en.wikipedia.org/wiki/OpenDocument">OASIS
  OpenDocument File format (ODF)</a> is widely regarded as one of the most
@@ -27,16 +29,14 @@
  time in the future.
 </p>
 <p>
- KOffice was the first to support, and now with 1.5 the second office suite to announce full support for OpenDocument as the default file format. It is also the first one not based on the OpenOffice.org code base, which makes ODF into a true industry standard. KOffice 1.5 is therefore the version that lets enterprises and organizations all over the world choose the office suite that fits their needs best.</p>
+ KOffice 1.5 offers native ODF support for the important productivity applications <a href="http://www.koffice.org/kword/">KWord</a> (.odt), <a href="http://www.koffice.org/kspread/">KSpread</a> (.ods), and <a href="http://www.koffice.org/kpresenter/">KPresenter</a> (.odp).  In addition to that, the supporting applications <a href="http://www.koffice.org/kchart/">KChart</a> (.odc) and <a href="http://www.koffice.org/kformula/">KFormula</a> (.odf) also support ODF, although not yet fully.</p>
 <p>
- KOffice 1.5 offers native ODF support for the important productivity applications KWord (odt), KSpread (ods), and KPresenter (odp).  In addition to that, the supporting applications KChart (.odc) and KFormula (.odf) also support ODF, although not yet fully.</p>
-<p>
- Great care has been taken to ensure interoperability with other office software that supports OpenDocument, most notably OpenOffice.org.  We acknowledge, however, that the ODF support and interoperability is not perfect yet. We hope to be able to quickly identify and fix the incompatibilities that do exist in the upcoming 1.5.1 and 1.5.2 bugfix releases.
+ Great care has been taken to ensure interoperability with other office software that supports OpenDocument, most notably OpenOffice.org.  We acknowledge, however, that the ODF support and interoperability is not yet perfect. We hope to be able to quickly identify and fix the incompatibilities that do exist in the upcoming 1.5.1 and 1.5.2 bugfix releases.
 </p>
 
  <h2>Killer Applications</h2>
  <p>
-  KOffice is the most comprehensive office suite in existence. In addition to the aforementioned basic productivity applications, it also includes a creativity application suite featuring <a href="http://www.koffice.org/krita/">Krita</a>, a paint application with lots of image manipulation features, <a href="http://www.koffice.org/karbon/">Karbon</a>, a vector graphics application, and <a href="http://www.koffice.org/kivio/">Kivio</a>, a Visio&reg; style flowcharting application. Other components are <a href="http://www.koffice.org/kexi/">Kexi</a>, an integrated environment for creating database applications, and <a href="http://www.koffice.org/kplato/">KPlato</a>, a program for project management.</p>
+  KOffice is already the most comprehensive office suite in existence. In addition to the basic productivity applications mentioned above, it also includes a creativity application suite featuring <a href="http://www.koffice.org/krita/">Krita</a>, a paint application with lots of image manipulation features, <a href="http://www.koffice.org/karbon/">Karbon</a>, a vector graphics application, and <a href="http://www.koffice.org/kivio/">Kivio</a>, a Visio&reg; style flowcharting application. Other components are <a href="http://www.koffice.org/kexi/">Kexi</a>, an integrated environment for creating database applications, and <a href="http://www.koffice.org/kplato/">KPlato</a>, a program for project management.</p>
 
  <p> 
  Even as parts of a suite, some of these applications are outstanding in their field:
@@ -66,7 +66,7 @@
   When needed, MySQL or PostgreSQL servers can be used for storage, or 
   existing projects can be imported from MySQL or PostgreSQL servers or 
   MS Access files. The final version 
-  focuses on the data exchangeability and server support.
+  focuses on data exchangeability and server support.
   </p>
   <p>
   Kexi is the first KOffice component to be <a href="http://www.kexi.pl/wiki/index.php/Kexi_for_MS_Windows">ported to Windows</a> and is also commercially supported by <a href="http://www.openoffice.com.pl/en/kexi">OpenOffice Polska</a>.
@@ -74,7 +74,7 @@
  </li>
  </ul>
 
- <h2>Extremely Fast Paced Development</h2>
+ <h2>Fast Paced Development</h2>
  <p> 
   The KOffice team has been able to keep development speed very high. The reason for this is a very sound and easy to work with code base. It also builds on a solid foundation made up of the Qt library and the KDE base libraries.
  </p>
@@ -82,7 +82,7 @@
   Despite having more components than any other office suite, things are improving very quickly.  Version 1.5 features OpenDocument as the default file format, introduction of a unified scripting framework, enhanced accessibility for users with disabilities, the first major release of Kexi and a technology preview of KPlato, a new project management application.  You can read more about it in the <a href="/announcements/announce-1.5.php">full announcement</a>.
  </p>
  <p>
-  KOffice has doubled the size of its developer base from around 15 at the time of KOffice 1.4 to around 30 at the time of KOffice 1.5. We continue to see a healthy growth in our developer community, which bodes well for the future. If you want to make a difference, then consider joining the fun and excitement that is the KOffice development team.  This offer does not only apply for coders; we also need a healthy mix of documentors, graphics artists, usability and accessibility experts and web designers.
+  KOffice has doubled the size of its developer base from around 15 at the time of KOffice 1.4 to around 30 at the time of KOffice 1.5. We continue to see a healthy growth in our developer community, which bodes well for the future. 
  </p>
  <p> 
   If we allow ourselves a look into the future, KOffice 2.0 will be released around the new year 2006/2007. It will build on the same platform as KDE 4.0, which means immediate portability to Windows and MacOS X. KOffice 2.0 will also include improved font handling, change management as well as improved integration.
@@ -112,5 +112,71 @@
       and binary formats.
   </li>
  </ul>
+
+<h2>About KOffice and KDE</2>
+  <p align="justify">
+  KOffice is an integrated office suite for KDE which utilizes free and
+  open standards for its document formats, component communication and
+  component embedding.
+</p>
+<p align="justify">
+    This release includes:
+</p>
+<ul>
+  <li>a frame-based, full-featured word processor
+    (<a href="http://www.koffice.org/kword/">KWord</a>);
+  </li>
+  <li>a spreadsheet application
+    (<a href="http://www.koffice.org/kspread/">KSpread</a>);
+  </li>
+  <li>a presentation application
+    (<a href="http://www.koffice.org/kpresenter/">KPresenter</a>);
+  </li>
+  <li>a flowchart application
+    (<a href="http://www.thekompany.com/projects/kivio/">Kivio</a>);
+  </li>
+  <li>an integrated database application
+    (<a href="http://www.koffice.org/kexi/">Kexi</a>);
+  </li>
+  <li>a pixel based image editing and paint application
+    (<a href="http://www.koffice.org/krita/">Krita</a>);
+  </li>
+  <li>a vector-drawing application
+    (<a href="http://www.koffice.org/karbon/">Karbon14</a>).
+  </li>
+  <li>a technology preview of a new project management application
+    (<a href="http://www.koffice.org/kplato/">KPlato</a>).
+  </li>
+</ul>
+<p>
+  Additionally, KOffice includes robust embeddable objects:
+</p>
+<ul>
+  <li>business quality reporting software
+    (<a href="http://www.thekompany.com/projects/kugar/">Kugar</a>)
+  </li>
+  <li>full-featured charting engine
+    (<a href="http://www.koffice.org/kchart/">KChart</a>)
+  </li>
+  <li>mathematical formula handling
+    (<a href="http://www.koffice.org/kformula/">KFormula</a>)
+  </li>
+</ul>
+<p>
+  as well as a built-in thesaurus (KThesaurus)
+  and support for many different 
+    <a href="http://www.koffice.org/filters/1.5/">file formats</a>.
+</p>
+<p align="justify">
+  KOffice 1.5 is a subproject within KDE, a powerful and easy-to-use Internet-enabled
+    desktop for Linux and other UNIXes.  Together, KDE and KOffice constitute the
+  only Free Software project to provide a complete desktop and productivity
+  environment for Linux/UNIX.
+</p>
+<p align="justify">
+  KOffice and all its components (including KDE) are available
+  <em><strong>for free</strong></em> under Open Source licenses from the KDE
+  <a href="http://download.kde.org/unstable/koffice-1.5/src/">http and ftp</a> mirrors.
+</p>
 
 <?php include("footer.inc"); ?>
