4dd5c5efe32d52af964d5d5672f3d720943ec235
staniek <staniek@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-04-10 08:31:11 +0000

    version ++ splitting screenshots
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@528128 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ kexi/older_screenshots.php
@@ -0,0 +1,29 @@
+<?php
+  $page_title='Older Kexi Screenshots';
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+For actual screenshots, see <a href="http://www.kexi-project.org/screenshots.html">Kexi Project Page</a>.
+
+<a href="/kexi/screenshots.html">&lt; Back to screenshots page</a>.
+
+<h2>Kexi 0.1 Screenshots</h2>
+
+<?php
+$gallery = new ImageGallery("Kexi Screenshots");
+$gallery->addImage("pics/t_relations.png", "pics/relations.png", "250", "197", "[Screenshot]", 0 , "Defining database relations");
+$gallery->startNewRow();
+$gallery->addImage("pics/t_alter.png", "pics/alter.png", "250", "175", "[Screenshot]", 0 , "Change table structure");
+$gallery->addImage("pics/t_manipulation.png", "pics/manipulation.png", "251", "188", "[Screenshot]", 0 , "Data manipulation with completion");
+$gallery->startNewRow();
+$gallery->addImage("pics/t_query-design.png", "pics/query-design.png", "250", "197", "[Screenshot]", 0 , "Create queries with drag and drop");
+$gallery->addImage("pics/t_query-sql.png", "pics/query-sql.png", "250", "197", "[Screenshot]", 0 , "SQL statements can still be used");
+$gallery->startNewRow();
+$gallery->addImage("pics/t_form-scripting.png", "pics/form-scripting.png", "251", "188", "[Screenshot]", 0 , "Create forms and add functionality by scripting");
+$gallery->addImage("pics/t_reports.png", "pics/reports.png", "250", "175", "[Screenshot]", 0 , "");
+$gallery->show();
+?>
+
+<?php include("footer.inc"); ?>
+
--- kexi/screenshots.php
+++ kexi/screenshots.php
@@ -4,23 +4,12 @@
   include("koffice.inc");
   include("header.inc");
 ?>
+<p>
 For actual screenshots, see <a href="http://www.kexi-project.org/screenshots.html">Kexi Project Page</a>.
-
-<h2>Older screenshots</h2>
-<?php
-$gallery = new ImageGallery("Kexi Screenshots");
-$gallery->addImage("pics/t_relations.png", "pics/relations.png", "250", "197", "[Screenshot]", 0 , "Defining database relations");
-$gallery->startNewRow();
-$gallery->addImage("pics/t_alter.png", "pics/alter.png", "250", "175", "[Screenshot]", 0 , "Change table structure");
-$gallery->addImage("pics/t_manipulation.png", "pics/manipulation.png", "251", "188", "[Screenshot]", 0 , "Data manipulation with completion");
-$gallery->startNewRow();
-$gallery->addImage("pics/t_query-design.png", "pics/query-design.png", "250", "197", "[Screenshot]", 0 , "Create queries with drag and drop");
-$gallery->addImage("pics/t_query-sql.png", "pics/query-sql.png", "250", "197", "[Screenshot]", 0 , "SQL statements can still be used");
-$gallery->startNewRow();
-$gallery->addImage("pics/t_form-scripting.png", "pics/form-scripting.png", "251", "188", "[Screenshot]", 0 , "Create forms and add functionality by scripting");
-$gallery->addImage("pics/t_reports.png", "pics/reports.png", "250", "175", "[Screenshot]", 0 , "");
-$gallery->show();
-?>
+</p>
+<p>
+<a href="/kexi/older_screenshots.html">Older screenshots</a>
+</p>
 
 <?php include("footer.inc"); ?>
 
