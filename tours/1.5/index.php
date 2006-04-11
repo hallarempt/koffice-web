f39c267f24fea609da54ef38c29375ad545390bc
raphael <raphael@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-04-11 10:15:42 +0000

    Remove moved koffice1.5-640.png picture (it's now in media/images/hotspots/) Add skeleton of tour, still much to write/include.
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@528524 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ tours/1.5/index.php
@@ -0,0 +1,139 @@
+<?php
+  $page_title='Release Highlights: KOffice 1.5 Tour';
+  $site_root='../../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+<p align="justify">
+  DATELINE APRIL 11, 2006<br />
+  <!-- FOR IMMEDIATE RELEASE -->
+  EMBARGO - DO NOT PUBLISH
+</p>
+
+ <p>
+ This tour accompanies the KOffice 1.5 release and shows its most important highlights
+ and gorgeous features. It is a comprehensive tour through the major changes since
+ the release of KOffice 1.4.
+ </p>
+ <p>
+ You can find the <a href="/announcements/announce-1.5.php">full announcement of KOffice 1.5 here</a>.
+ </p>
+
+ <!-- the following is by no means complete, just a skeleton, will be improved later -->
+
+ <h2>Core Technology Highlights</h2>
+
+ <h3>Native OASIS OpenDocument Format support</h3>
+
+ <p>OASIS OpenDocument is now the default format for all major KOffice applications.</p>
+
+ <h3>New Startup Dialog</h3>
+
+ <p>Usability Experts worked together to completely redesign the KOffice startup dialog.</p>
+
+ <h3>Enhanced Accessibility</h3>
+
+ <p>All major KOffice applications have improved accessibility features, including text to speech.</p>
+
+ <h3>Unified Scripting Support</h3>
+
+ <p>KROSS now allows scripting of some KOffice applications using Python and Ruby.</p>
+
+
+ <h2>KWord</h2>
+
+ <h2>KSpread</h2>
+
+ <h2>KPresenter</h2>
+
+ <h2>Kexi</h2>
+
+ <h2>Kivio</h2>
+
+ <h2>Karbon14</h2>
+
+ <h2>Krita</h2>
+
+ <h2>KPlato</h2>
+
+<!--
+
+ <h2>KChart</h2>
+
+ <h2>KFormula</h2>
+
+ <h2>Kugar</h2>
+
+-->
+
+<!-- UP TO HERE -->
+
+
+<h2>About KOffice and KDE</h2>
+  <p align="justify">
+  KOffice is an integrated office suite for KDE which utilizes free and
+  open standards for its document formats, component communication and
+  component embedding.
+</p>
+<p align="justify">
+    This release includes:
+</p>
+<ul>
+  <li>a frame-based, full-featured word processor
+    (<a href="http://www.koffice.org/kword/">KWord</a>);
+  </li>
+  <li>a spreadsheet application
+    (<a href="http://www.koffice.org/kspread/">KSpread</a>);
+  </li>
+  <li>a presentation application
+    (<a href="http://www.koffice.org/kpresenter/">KPresenter</a>);
+  </li>
+  <li>a flowchart application
+    (<a href="http://www.thekompany.com/projects/kivio/">Kivio</a>);
+  </li>
+  <li>an integrated database application
+    (<a href="http://www.koffice.org/kexi/">Kexi</a>);
+  </li>
+  <li>a pixel based image editing and painting application
+    (<a href="http://www.koffice.org/krita/">Krita</a>);
+  </li>
+  <li>a vector-drawing application
+    (<a href="http://www.koffice.org/karbon/">Karbon14</a>).
+  </li>
+  <li>a technology preview of a new project management application
+    (<a href="http://www.koffice.org/kplato/">KPlato</a>).
+  </li>
+</ul>
+<p>
+  Additionally, KOffice includes robust embeddable objects:
+</p>
+<ul>
+  <li>business quality reporting software
+    (<a href="http://www.thekompany.com/projects/kugar/">Kugar</a>)
+  </li>
+  <li>full-featured charting engine
+    (<a href="http://www.koffice.org/kchart/">KChart</a>)
+  </li>
+  <li>mathematical formula handling
+    (<a href="http://www.koffice.org/kformula/">KFormula</a>)
+  </li>
+</ul>
+<p>
+  as well as a built-in thesaurus (KThesaurus)
+  and support for many different
+    <a href="http://www.koffice.org/filters/1.5/">file formats</a>.
+</p>
+<p align="justify">
+  KOffice 1.5 is a subproject within KDE, a powerful and easy-to-use Internet-enabled
+    desktop for Linux and other UNIXes.  Together, KDE and KOffice constitute the
+  only Free Software project to provide a complete desktop and productivity
+  environment for Linux/UNIX.
+</p>
+<p align="justify">
+  KOffice and all its components (including KDE) are available
+  <em><strong>for free</strong></em> under Open Source licenses from the KDE
+  <a href="http://download.kde.org/stable/koffice-1.5.0/src/">http and ftp</a> mirrors.
+</p>
+
+<?php include("footer.inc"); ?>
