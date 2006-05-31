88a84a03cb785d3efaf967d536f6b4c759f7c131
zander <zander@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-05-31 11:15:41 +0000

    Updates
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@546907 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- getinvolved/junior-jobs.php
+++ getinvolved/junior-jobs.php
@@ -1,5 +1,5 @@
 <?php
-  $page_title="Devel Junior Jobs 2005";
+  $page_title="Devel Junior Jobs 2006";
   $site_root='../';
   include("koffice.inc");
   include("header.inc");
@@ -11,8 +11,7 @@
 	]
 </div>
 
-    <p>December 2005 targets</p>
-<p>It is prefered to use <b>KOffice svn trunk code</b> to work on JJ, which is the next coming 1.5 release. You can find some information on how to build it on the <a href="../download/unstable.php">Install KOffice unstable from svn page</a>.</p>
+<p>It is prefered to use <b>KOffice svn trunk code</b> to work on JJ, which is the next coming 2.0 release. You can find some information on how to build it on the <a href="../download/unstable.php">Install KOffice unstable from svn page</a>.</p>
 <p>If you need help, you can join members of the KOffice team on IRC, server <tt>irc.freenode.org</tt>, port 6667, channel <tt>#koffice</tt>.</p>
 
 <table cellspacing="0" cellpadding="3" border="1">
@@ -25,31 +24,31 @@
   <tr>
     <td><b>Karbon</b>: <a href="http://bugs.kde.org/show_bug.cgi?id=109189">http://bugs.kde.org/show_bug.cgi?id=109189</a></td>
     <td><a href="mailto:buis () kde ! org">Rob Buis</a></td>
-    <td>30th January 2006</td>
+    <td>30th January December</td>
     <td>-</td>
   </tr>
   <tr>
     <td><b>Karbon</b>:  fix units <a href="http://bugs.kde.org/show_bug.cgi?id=115010">http://bugs.kde.org/show_bug.cgi?id=115010</a></td>
     <td><a href="mailto:tbscope () gmail ! com">Tim Beaulen</a></td>
-    <td>30th January 2006</td>
+    <td>30th December 2006</td>
     <td>-</td>
   </tr>
   <tr>
     <td><b>Karbon</b>:  Usability: The color and opacity bars in the color chooser palette are backwards. <a href="http://bugs.kde.org/show_bug.cgi?id=115213">http://bugs.kde.org/show_bug.cgi?id=115213</a></td>
     <td><a href="mailto:tbscope () gmail ! com">Tim Beaulen</a></td>
-    <td>30th January 2006</td>
+    <td>30th December 2006</td>
     <td>-</td>
   </tr>
   <tr>
     <td><b>KChart</b>: Code structure: put the wizard and config dialog in a "dialogs" subdirectory. <a href="http://bugs.kde.org/show_bug.cgi?id=117960">http://bugs.kde.org/show_bug.cgi?id=117960</a></td>
     <td><a href="mailto:inge () lysator ! liu ! se">Inge Wallin</a></td>
-    <td>30th January 2006</td>
+    <td>30th December 2006</td>
     <td>-</td>
   </tr>
   <tr>
     <td><b>KPresenter</b>:  Usability: Most basic resizing operation for pictures are not possible. <a href="http://bugs.kde.org/show_bug.cgi?id=112310">http://bugs.kde.org/show_bug.cgi?id=112310</a></td>
     <td><a href="mailto:montel () kde ! org">Laurent Montel</a></td>
-    <td>30th January 2006</td>
+    <td>30th December 2006</td>
     <td>-</td>
   </tr>
   <tr>
@@ -61,7 +60,7 @@
   <tr>
     <td><b>KSpread</b>: Add QWhatsThis help to the cell format configuration dialog.</td>
     <td><a href="mailto:raphael.langerhorst () kdemail ! net">Raphael Langerhorst</a></td>
-    <td>in good progress - 30th January 2006</td>
+    <td>in good progress - 30th December 2006</td>
     <td>Anne-Marie Mahfouf</td>
   </tr>
   <tr>
