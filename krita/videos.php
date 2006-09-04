ecd2828985b152a0374d0f1fa8202c4c5362fd69
coppens <coppens@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-09-04 16:55:25 +0000

    Add the videos of Krita to the site, and add Cyrilles new perspective cloning movie at the same time. Not yet linked to from the outside, let's look how this works out first.
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@580860 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ krita/videos.php
@@ -0,0 +1,36 @@
+<?php
+  $page_title="Krita Videos";
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+<?php
+$gallery = new ImageGallery("Krita Videos");
+// $gallery->startNewRow(); should not be used before first image as it prevents validation
+$gallery->addImage("videos/krita_perspectivecloning_sm.png",
+                   "videos/krita_perspectivecloning.mpeg","272", "205",
+                   "[Screenshot]", 0 , "Perspective Cloning (1.6 Beta, September 2006)");
+
+$gallery->startNewRow();
+$gallery->addImage("videos/krita_rc1_sm.png",
+                   "videos/krita_rc1.avi","273", "183",
+                   "[Screenshot]", 0 , "General Preview (Pre-1.5, June 2005)");
+$gallery->addImage("videos/krita_preview2_sm.png",
+                   "videos/krita_preview2.mpeg","271", "215",
+                   "[Screenshot]", 0 , "General Preview (Pre-1.4, September 2006)");
+$gallery->startNewRow();
+
+$gallery->addImage("videos/krita_preview1_sm.png",
+                   "videos/krita_preview1.mpeg","272", "233",
+                   "[Screenshot]", 0 , "General Preview (Pre-1.4, August 2006)");
+
+$gallery->show();
+?>
+
+<?php
+  include("footer.inc");
+?>
+
+
+
