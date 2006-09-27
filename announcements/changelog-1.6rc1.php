0dc40420aaed6cb548e9b2889e03b0e0a9b165b7
staniek <staniek@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-09-27 12:28:57 +0000

    update for Kexi 1.1 rc1
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@589007 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- announcements/changelog-1.6rc1.php
+++ announcements/changelog-1.6rc1.php
@@ -6,12 +6,30 @@
 ?>
 <p>
   The following summarizes the major changes to the KOffice Suite since the
-  release of KOffice 1.6.0 RC 1 (latest unstable version).
+  <a href="changelog-1.6beta1.php">release of KOffice 1.6.0 Beta 1</a>.
 </p>
 <ul>
+<li><a href="#koffice_libs">KOffice Libraries</a></li>
 <li><a href="#kspread">KSpread</a></li>
+<li><a href="#kexi">Kexi</a></li>
 <li><a href="#krita">Krita</a></li>
 <li><a href="#kformula">KFormula</a></li>
+</ul>
+
+<h3 id="koffice_libs">KOffice Libraries</h3>
+<ul>
+  <li><em>KoProperty Library</em>
+  <ul>
+  <li>Properties are not changed when new and old value are both null</li>
+  <li>Spin boxes for integer and double values: deleting the contents
+  now sets null value</li>
+  </ul>
+  </li>
+  <!-- li><em>Kross Scripting Library</em>
+  <ul>
+  <li></li>
+  </ul>
+  </li -->
 </ul>
 
 <h3 id="kspread">KSpread</h3>
@@ -20,7 +38,7 @@
     <li> Formula engine / functions:
     <ul>
         <li> Fix references to sheets with names beginning with a number (bug:124549)
-        <li> fixes for various database functions
+        <li> Fixes for various database functions
     </ul>
 
 <li> UI:
@@ -28,6 +46,59 @@
         <li> Don't jump to chosen cell (range) on entering the formula (bug:134276)
     </ul>
 </ul>
+
+
+<h3 id="kexi">Kexi</h3>
+
+<em>Table Designer</em>
+<ul>
+ <li>Set "false" as default value for columns with Yes/No types</li>
+ <li>When column type changes, default value is updated to match the new type
+  (e.g. casted from integer to text; incompatible values are removed)</li>
+ <li>Fixed inserting fields of type "Object"</li>
+</ul>
+
+<em>Forms</em>
+<ul>
+  <li>Hide unused radio button widget</li>
+  <li>Fixed tab/backtab key handling</li>
+  <li>Fixed displaying custom label text when "Auto Label" property is off</li>
+  <li>Handling of global shortcuts like CTRL+PgDown is now shared with 
+   Table View (accesibility: it is now possible to move to next/previous 
+   record using keyboard)</li>
+  <li>Moving to new row sets the focus in the first data-aware widget 
+   (in tab order)</li>
+  <li>Fixed leaving from "editing" state on "accept row changes" or moving 
+   to other record</li>
+  <li>Custom widget factories can be now loaded properly</li>
+</ul>
+
+<em>Data Table View</em>
+<ul>
+ <li>More shortcuts added, shortcuts are now usable and a bit MSAcces-like</li>
+   <ul>
+   <li>CTRL+End now moves to the last field in the last record 
+    (previously: the last record)</li>
+   <li>CTRL+Home now moves to the first field in the first record 
+    (previously: the first record)</li>
+   <li>CTRL+PgDown now moves to the next record</li>
+   <li>CTRL+PgUp now moves to the previous record</li>
+   <li>CTRL+Plus or CTRL+= now moves to the new record</li>
+  </ul>
+  <li>Fixed repainting after "Edit->Clear Table Contents" action</li>
+</ul>
+
+<em>Database Support Library (KexiDB)</em>
+<ul>
+ <li>Do not try to insert default values into a column with unique 
+  flag declared (or even primary key)</li>
+</ul>
+
+<em>Documentation</em>
+<ul>
+ <li>Added chapter about forms translated to english.</li>
+</ul>
+
 
 <h3 id="krita">Krita</h3>
 <em>Bugfixes</em>
@@ -37,10 +108,10 @@
     <li> Fix changing the adjustment of brightness-contrast adjustment layers
     <li>UI:
     <ul>
-        <li>simplify acquiring screen shots (Bug:134527)
+        <li>Simplify acquiring screen shots (Bug:134527)
         <li>Remember opacity setting in Drop Shadow (Bug:134563)
         <li>Fix so that the rightly marked filters got displayed in the filters gallery dialog
-        <li>new icons set for the too
+        <li>New icons set for the too
     </ul>
     <li>Selections:
     <ul>
@@ -68,9 +139,9 @@
     <li>Tools:
     <ul>
         <li>Better interraction with the perspective transform tool
-        <li>bezier tools don't work in openGL mode (Bug: 134477)
-        <li>optimization of the duplicate tool, and fix healing and perspective
-        <li>fix the tool crop behaviour
+        <li>Bezier tools don't work in openGL mode (Bug: 134477)
+        <li>Optimization of the duplicate tool, and fix healing and perspective
+        <li>Fix the tool crop behaviour
     </ul>
 </ul>
 
