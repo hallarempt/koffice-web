fa0d97d7c78cf0b4ecfe6a603d98dbb07a57c1e1
skoning <skoning@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-09-28 15:38:17 +0000

    Update
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@589756 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- getinvolved/howto-write-doc.php
+++ getinvolved/howto-write-doc.php
@@ -5,18 +5,17 @@
   include("header.inc");
 ?>
 
-<p><a href="mailto:raphael.langerhorst@kdemail.net">Raphael Langerhorst</a>
-coordinates KOffice documentation work, please contact him for any
-questions or feedback.</p>
+<h3>KOffice Documentation Guidelines</h3>
 
-<h3>KOffice Documentation Cookbook</h3>
+There are many things that are common to all KOffice handbooks. A KOffice
+handbook should:
 
-<p>A cookbook for writing KOffice documentation including long term plans
-is currently being prepared and will be available here when it is ready,
-contact Raphael if you want a preliminary version.
-<br />
-Note: you can also find the current draft version in the doc subdirectory
-of <a href="http://www.koffice.org/download/source.php">koffice svn</a>.</p>
+<ul>
+    <li>introduce the user to the workflow of each component</li>
+    <li>fully describe the functionality, including examples and short tutorials</li>
+    <li>use screenshots wherever appropriate - visually appealing documentation is better than just text</li>
+    <li>contain a command reference that explains all menu items (including context menus)</li>
+</ul>
 
 <h3>Tips and Tricks</h3>
 
@@ -26,10 +25,13 @@ of <a href="http://www.koffice.org/download/source.php">koffice svn</a>.</p>
     <li>set your editor to wrap lines at 80 characters</li>
     <li>open and save as utf8 in your editor</li>
     <li>choose the docbook syntax highlighting</li>
+    <li>do not indent your docbook files</li>
 </ul>
 
-<p><b>Screenshots</b>: theme, style and color can be set in KControl. Theme and style are <tt>Plastik</tt> and color is KDE default.<br />
-Please use those KDE defaults as much as possible for consistent screenshots along with standard fonts.</p>
+<p><b>Screenshots</b>: theme, style and color can be set in KControl. Theme and
+style are <tt>Plastik</tt> and color is KDE default.<br />
+Please use those KDE defaults as much as possible for consistent screenshots
+along with standard fonts.</p>
 
 <h3>Information on KDE Documentation</h3>
 
