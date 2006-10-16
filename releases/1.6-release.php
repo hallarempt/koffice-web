bc628019f8bcbd395f3b8e9e02e263685d966579
ingwa <ingwa@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-10-16 18:48:15 +0000

    initial revision
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@596112 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ releases/1.6-release.php
@@ -0,0 +1,53 @@
+<?php
+  $page_title='KOffice 1.6 RC1 Release Notes';
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+<p align="justify">
+  DATELINE OCTOBER 16, 2006<br />
+  FOR IMMEDIATE RELEASE
+</p>
+
+      <p>The KOffice team is proud to announce KOffice 1.6.</p>
+
+      <h2>Announcement</h2>
+      <ul>
+      <li><a href="/announcements/announce-1.6.php">Announcement</a></li>
+      </ul>
+
+      <h2>Changelog</h2>
+      <ul>
+      <li><a href="/announcements/changelog-1.6.php">Changelog</a></li>
+      </ul>
+
+      <h2>Download</h2>
+      <ul>
+      <li>Source code of KOffice 1.6:
+	<table border="0" cellpadding="4" cellspacing="0">
+	  <tr>
+	    <th align="left">Description</th>
+	    <th align="left">Location</th>
+	  <tr>
+	    <td>KOffice 1.6 (source code)</td>
+	    <td><a href="http://download.kde.org/unstable/koffice-1.6/src/koffice-1.6.tar.bz2">koffice-1.6.tar.bz2</a></td>
+	  </tr>
+	</table>
+      </li>
+      <li>
+	<a href="http://download.kde.org/unstable/koffice-1.6/">Binary packages</a></li>
+      </li>
+      </ul>
+
+      <h2>MD5 sums</h2>
+      <p>
+       The MD5 sums for the tarballs are:
+      </p>
+<?php
+include "md5-koffice-1.6.inc"
+?>
+      <p>
+       Also provided: <a href="md5-pgp-koffice-1.6.txt">a PGP-signed version of the MD5 sums</a>.
+      </p>
+
+<?php include("footer.inc"); ?>
