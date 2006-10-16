274b6e4519978586a7f94b0dd662105efbcf50ea
berger <berger@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-10-16 20:32:40 +0000

    add a skeleton page about the koffice LiveCD
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@596168 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ download/kofficelivecd.php
@@ -0,0 +1,18 @@
+<?php
+  $page_title="Download KOffice Live CDe";
+  $site_root = '../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+
+<h2><a name="requirements">Download the KOffice 1.6.0 Live CD</a></h2>
+<p>Click <a href="koffice16live.iso">here</a> to download a KOffice Live CD and test the latest version without installing anything on your computer.</p>
+
+
+<br />
+ <br />
+ <hr width="30%" align="center" />
+ <p>
+ Last update: <?php echo date ("Y-m-d", filemtime(__FILE__)); ?>
+ </p>
+<?php include("footer.inc"); ?>
