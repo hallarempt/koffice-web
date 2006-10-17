27bdf581c820725d9d370b99f598818d30b6c432
dfaure <dfaure@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-10-17 07:53:03 +0000

    Another 404 handler
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@596318 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- 404.php
+++ 404.php
@@ -13,6 +13,7 @@ $handler->add("/supporters.phtml", "/support/supporters.php");
 $handler->add("/filters/status.php", "/developer/filters/status.php");
 $handler->add("/killustrator/index.php", "/karbon/index.php");
 $handler->add("/kontour/index.php", "/karbon/index.php");
+$handler->add("/kontour", "/karbon");
 $handler->add("/filters/status.phtml", "/filters/1.2");
 $handler->add("/developer/news.rdf", "/news_rss2.php");
 
