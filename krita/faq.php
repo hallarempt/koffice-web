ccec89340399c526985d40209899ca6188f5c8e3
rempt <rempt@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2006-10-19 20:07:01 +0000

    Add two very frequently asked questions.
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@597281 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- krita/faq.php
+++ krita/faq.php
@@ -7,6 +7,12 @@
 
 <?php
 $faq = new FAQ();
+$faq->addQuestion("Why is Krita part of KOffice?", "Krita started out as KImageShop inside KOffice because the KOffice libraries give us things for free we would have to
+    code ourselves otherwise, like filter handling, rich text tool and so on. Look at KOffice this side: text, numbers and images, both sides of your brain, creative
+    and analytic!");
+$faq->addQuestion("But it's such a burden! I have to install not just all of KDE, but also all of KOffice. Just to get Krita!", "Well, first of all: strictly speaking
+    you need just the kde and koffice libraries, but that's not the point. Would you rather have all that functionality duplicated in Krita (and every other application
+    on your system?) Code reuse is good, library sharing is better. It all saves us time to spend on cool features.");
 $faq->addQuestion("Why does Krita tell me my image has the sRGB (LCMS internal) profile? I
 didn't know it had a profile!", "Krita cannot work without color profiles, so on loading
 an RGB image without a profile, Krita assign it a default profile. sRGB is what most
