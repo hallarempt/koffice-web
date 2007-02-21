f6c3b519d419f0e857b6c5262a5c80c842ad3a51
berger <berger@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2007-02-21 21:02:49 +0000

    release notes for koffice 1.6.2
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@636076 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ releases/1.6.2-release.php
@@ -0,0 +1,129 @@
+<?php
+  $page_title='KOffice 1.6.2 Release Notes';
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+<p align="justify">
+  DATELINE FEBRUARY 21, 2006<br />
+  FOR IMMEDIATE RELEASE
+</p>
+
+      <p>The KOffice team is proud to announce KOffice 1.6.2.</p>
+
+      <h2>Announcement</h2>
+      <ul>
+      <li><a href="/announcements/announce-1.6.2.php">Announcement</a></li>
+      </ul>
+
+      <h2>Changelog</h2>
+      <ul>
+      <li><a href="/announcements/changelog-1.6.2.php">Changelog</a></li>
+      </ul>
+
+      <h2>Download</h2>
+      <ul>
+      <li>Source code of KOffice 1.6.2:
+	<table border="0" cellpadding="4" cellspacing="0">
+	  <tr>
+	    <th align="left">Description</th>
+	    <th align="left">Location</th>
+	  <tr>
+	    <td>KOffice 1.6.2 (source code)</td>
+	    <td><a href="http://download.kde.org/stable/koffice-1.6.2/src/koffice-1.6.2.tar.bz2">koffice-1.6.2.tar.bz2</a></td>
+	  </tr>
+	  <tr>
+	    <td>Patch for Kexi 1.1.2 for user of dash</td>
+	    <td><a href="http://download.kde.org/stable/koffice-1.6.2/src/kexi-dash.patch">kexi-dash.patch</a></td>
+	  </tr>
+	  <tr>
+	    <td>KexiMDB 1.1.0 (MS Access import plugin)</td>
+	    <td><a href="http://download.kde.org/stable/koffice-1.6.2/src/keximdb-1.1.0.tar.gz">keximdb-1.1.0.tar.gz</a></td>
+	  </tr>
+	</table>
+      </li>
+      <li>
+	<a href="http://download.kde.org/stable/koffice-1.6.2/">Binary packages</a>, currently available for <a href="http://download.kde.org/stable/koffice-1.6.2/kubuntu/">Kubuntu</a> or <a href="http://download.kde.org/stable/koffice-1.6.2/SuSE/">SuSE</a>.
+      </li>
+<!--      <li>
+        <a href="http://www.koffice.org/download/kofficelivecd.php">Live CD</a>
+      </li>
+-->
+      </ul>
+
+      <h2>MD5 sums</h2>
+      <p>
+       The MD5 sums for the tarballs are:
+      </p>
+<?php
+include "md5-koffice-1.6.2.inc"
+?>
+      <p>
+       Also provided: <a href="md5-pgp-koffice-1.6.2.txt">a PGP-signed version of the MD5 sums</a>.
+      </p>
+
+<a name="compiling"></a><h2>
+  Compiling KOffice 1.6.2
+</h2>
+<p align="justify">
+  <a name="source_code-library_requirements"></a><strong>Library
+  Requirements</strong>:
+  KOffice 1.6.2 requires the following libraries:
+</p>
+<ul>
+  <li>
+    <div align="justify">
+    kdelibs 3.3, 3.4 or 3.5, which can be downloaded following the
+    <a href="http://www.kde.org/info/3.5.php">instructions</a>
+    at the KDE website (for more information on these kdelibs releases, please
+    see the <a href="http://www.kde.org/announcements/announce-3.5.php">KDE
+    3.5 press release</a>);
+    </div>
+  </li>
+  <li>
+    <div align="justify">
+    Qt-3.3, which is available in source code from Trolltech as
+    <a href="ftp://ftp.trolltech.com/qt/source/qt-x11-free-3.3.0.tar.gz">qt-x11-free-3.3.0.tar.gz</a>
+    (<a href="ftp://ftp.trolltech.com/qt/source/qt-x11-free-3.3.5.tar.gz">qt-x11-free-3.3.5.tar.gz</a>
+    is recommended);
+    and
+    </div>
+  </li>
+  <li>
+    <div align="justify">
+    <a href="http://xmlsoft.org/">libxml2</a> &gt;= 2.4.9 and
+    <a href="http://xmlsoft.org/XSLT/">libxslt</a> &gt;= 1.0.7.
+    for reading help pages and other KOffice documentation,
+    </div>
+  </li>
+  <li>
+    <div align="justify">
+    <a href="http://www.littlecms.com/">lcms</a> &gt;= 1.15 for handling color
+    spaces.  1.14 should not be used due to severe bugs.
+    </div>
+  </li>
+</ul>
+<p align="justify">
+  <strong>Compiler Requirements</strong>:
+  Please note that KOffice 1.6.2 will not
+  compile with older versions of <a href="http://gcc.gnu.org/">gcc/egcs</a>,
+  such as egcs-1.1.2 or gcc-2.7.2.  At a minimum gcc-2.95-* is required.
+</p>
+<p align="justify">
+  <a name="source_code"></a><strong>Source Code</strong>:
+  The complete source code for KOffice 1.6.2 is available for free download
+  via one of the KDE
+  <a href="http://download.kde.org/stable/koffice-1.6.2/">http</a>
+  or <a href="http://www.kde.org/ftpmirrors.html">ftp</a> mirrors.
+  <!--Additionally, source rpms are available for the following distributions:-->
+</p>
+<!-- INCLUDE PHP FILE HERE -->
+<p align="justify">
+  <strong>Further Information</strong>:
+  For further instructions on compiling and installing KOffice, please consult
+  the KOffice <a href="http://www.koffice.org/download/source.php">installation
+  instructions</a>.  For
+  problems with SRPMs, please contact the person listed in the applicable
+  .spec file.
+</p>
+<?php include("footer.inc"); ?>
