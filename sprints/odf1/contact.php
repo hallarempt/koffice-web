ef143d39d44e6e74dfbd1a76ac10f9fb3278f0d5
pmurdoch <pmurdoch@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2007-04-25 20:24:49 +0000

    added contact page
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@658061 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ sprints/odf1/contact.php
@@ -0,0 +1,19 @@
+<?php
+  $page_title='KOffice ODF Weekend - Contact';
+  $site_root='../../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+<p>
+The following information can be used to contact the event organisers.
+</p>
+<h4>Inge Wallin</h4>
+<p>
+Email: <a href="mailto:inge@lysator.liu.se">inge@lysator.liu.se</a><br />
+Tel (Mobile): +46 707 26 08 53
+</p>
+<h4>Claire Lotion</h4>
+<p>
+Email: <a href="mailto:clairelotion@kde.nl">clairelotion@kde.nl</a><br />
+</p>
+<?php include("footer.inc"); ?>
