941593f586222fc40251ea782daabd5bb3f465e8
pmurdoch <pmurdoch@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2007-05-09 19:54:43 +0000

    added irc nick column
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@663001 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- sprints/odf1/participants.php
+++ sprints/odf1/participants.php
@@ -9,99 +9,119 @@ The following people are participating in the ODF Weekend:
 </p>
 <table border="1" width="75%">
   <tr>
-    <th colspan="2">Maintainers</th>
+    <th colspan="3">Maintainers</th>
   </tr>
   <tr>
     <th>Name</th>
     <th>Representing</th>
+    <th>IRC Nickname</th>
   </tr>
   <tr>
     <td>David Faure</td>
     <td>KoLibs</td>
+    <td>dfaure</td>
   </tr>
   <tr>
     <td>Thomas Zander</td>
     <td>KWord</td>
+    <td>ThomasZ</td>
   </tr>
   <tr>
     <td>Ariya Hidayat</td>
     <td>KSpread</td>
+    <td>ariya</td>
   </tr>
   <tr>
     <td>Thorsten Zachmann</td>
     <td>KPresenter, Kivio</td>
+    <td>Zagge</td>
   </tr>
   <tr>
     <td>Jan Hambrecht</td>
     <td>Karbon</td>
+    <td>jaham</td>
   </tr>
   <tr>
     <td>Boudewijn Rempt</td>
     <td>Krita</td>
+    <td>boud</td>
   </tr>
   <tr>
     <td>Inge Wallin</td>
     <td>KChart</td>
+    <td>ingwa</td>
   </tr>
   <tr>
     <td>Alfredo Beaumont</td>
     <td>KFormula</td>
+    <td>abeaumont</td>
   </tr>
 </table>
 <br />
 <table border="1" width="75%">
   <tr>
-    <th colspan="2">Other Developers</th>
+    <th colspan="3">Other Developers</th>
   </tr>
   <tr>
     <th>Name</th>
     <th>Representing</th>
+    <th>IRC Nickname</td>
   </tr>
   <tr>
     <td>Martin Pfeiffer</td>
     <td>KFormula</td>
+    <td>hubipete</td>
   </tr>
   <tr>
     <td>Sebastian Sauer</td>
     <td>Kross</td>
+    <td>dipesh</td>
   </tr>
   <tr>
     <td>Philip Rodriques</td>
     <td>Documentation, testing</td>
+    <td>philrod</td>
   </tr>
   <tr>
     <td>Anne-Marie Mahfouf</td>
     <td>Website, testing, documentation</td>
+    <td>annma</td>
   </tr>
   <tr>
     <td>Tobias Koenig</td>
     <td>Okular</td>
+    <td>tokoe</td>
   </tr>
   <tr>
     <td>Aaron Seigo</td>
     <td>KDElibs</td>
+    <td>aseigo</td>
   </tr>
 </table>
 <br />
 <table border="1" width="75%">
   <tr>
-    <th colspan="2">Misc. Participants</th>
+    <th colspan="3">Misc. Participants</th>
   </tr>
   <tr>
     <th>Name</th>
     <th>Representing</th>
+    <td>IRC Nickname</td>
   </tr>
   <tr>
     <td>Claire Lotion</td>
     <td>Event organiser</td>
+    <td>claire</td>
   </tr>
   <tr>
     <td>Inge Wallin</td>
     <td>Event organiser</td>
+    <td>ingwa</td>
   </tr>
   <tr>
     <td>Jos Poortlivet</td>
     <td>Press coverage</td>
+    <td>-</td>
   </tr>
 </table>
 <?php include("footer.inc"); ?>
