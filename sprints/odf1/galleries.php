a236d5823e7ca688206395c312ef1e3805dde562
annma <annma@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2007-05-12 21:30:18 +0000

    update
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@663985 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- sprints/odf1/galleries.php
+++ sprints/odf1/galleries.php
@@ -11,10 +11,15 @@ Enjoy the great press reports, pictures galleries and presentations:
 <li>press reports</li>
 <li>blogs</li>
 	<ul>
-	<li><a href="http://www.valdyas.org/fading/index.cgi/2007/05/11#berlin1">Boudewin Rempt : Friday</a></li>
+	<li><a href="http://www.valdyas.org/fading/index.cgi/2007/05/11#berlin1">Boudewin Rempt: Friday</a></li>
+    <li><a href="http://nowwhatthe.blogspot.com/2007/05/dont-computers-suck.html">Jos Poortvliet: Saturday</a></li>
+    <li><a href="http://annma.blogspot.com/2007/05/odf-saturday.html">Anne-Marie Mahfouf: Saturday</a></li>
 	</ul>
 <li>photos galleries</li>
-<li>presentations</li>
+<li>links to documents</li>
+	<ul>
+	<li><a href="http://docs.oasis-open.org/office/v1.1/OS/OpenDocument-v1.1.pdf">OASIS Open Document Format Specification v1.1 in pdf</a></li>
+	</ul>
 </ul>
 
  <p>
