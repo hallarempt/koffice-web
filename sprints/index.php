a8ac07f4464c4fe4d1ed60ab68c00645c45122ef
pmurdoch <pmurdoch@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2007-05-22 16:41:39 +0000

    added past sprints section
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@667372 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- sprints/index.php
+++ sprints/index.php
@@ -8,4 +8,6 @@
 In order to further the development of KOffice and increase both functionality and usability, the developers of KOffice hold sporadic meetings known as "sprints", and this is where you can find out the who, why and where of them.
 </p>
 <h3>Most recent sprint: <a href="odf1/index.php">ODF Hackathon</a></h3>
+<h4>Past sprints</h4>
+<p><em>None to show</em></p>
 <?php include("footer.inc"); ?>
