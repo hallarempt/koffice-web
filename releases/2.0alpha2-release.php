095e9e540027295cce7402ba094186ce8887f677
zander <zander@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2007-08-02 09:44:34 +0000

    Add a simple release page. Remove the compile instructions as they were outdated.
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@695515 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ releases/2.0alpha2-release.php
@@ -0,0 +1,38 @@
+<?php
+  $page_title='KOffice 2.0-alpha2 Release Notes';
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+<p align="justify">
+  DATELINE AUGUSTUS 2, 2007<br />
+  FOR IMMEDIATE RELEASE
+</p>
+
+  <p>The KOffice team is proud to announce the second KOffice alpha for the upcoming 2.0 release.</p>
+
+  <h2>Announcement</h2>
+  <ul>
+  <li><a href="/announcements/ announce-2.0alpha2.php">Announcement</a></li>
+  </ul>
+
+  <h2>Download</h2>
+  <ul>
+    <li>Source code of KOffice 2.0-alpha-2:
+      <table border="0" cellpadding="4" cellspacing="0">
+      <tr>
+        <th align="left">Description</th>
+        <th align="left">Location</th>
+      <tr>
+        <td>KOffice 2.0-alpha-2 (source code)</td>
+        <td><a href="http://download.kde.org/unstable/koffice-1.9.92/src/koffice-1.9.92.tar.bz2">koffice-1.9.92.tar.bz2</a></td>
+    </tr>
+    <tr>
+      <td>diffs between alpha1 and alpha2</td>
+      <td><a href="http://download.kde.org/unstable/koffice-1.9.92/src/koffice-1.9.91-1.9.92.tar.xdelta">koffice-1.9.91-1.9.92.tar.xdelta</a></td>
+    </tr>
+   </table>
+  </li>
+  </ul>
+
+<?php include("footer.inc"); ?>
