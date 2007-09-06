d661532fe00ebea8fdb032d6ee4f640b15799eeb
zander <zander@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2007-09-06 09:50:25 +0000

    preparing for new release
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@708997 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ releases/2.0alpha3-release.php
@@ -0,0 +1,83 @@
+<?php
+  $page_title='KOffice 2.0-alpha3 Release Notes';
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+<p align="justify">
+  DATELINE SEPTEMBER 6, 2007<br />
+  FOR IMMEDIATE RELEASE
+</p>
+
+  <p>The KOffice team is proud to announce the third KOffice alpha for the upcoming 2.0 release.</p>
+
+  <h2>Announcement</h2>
+  <ul>
+  <li><a href="/announcements/announce-2.0alpha3.php">Announcement</a></li>
+  </ul>
+
+  <h2>Download</h2>
+  <ul>
+    <li>Source code of KOffice 2.0-alpha-3:
+      <table border="0" cellpadding="4" cellspacing="0">
+      <tr>
+        <th align="left">Description</th>
+        <th align="left">Location</th>
+      <tr>
+        <td>KOffice 2.0-alpha-3 (source code)</td>
+        <td><a href="http://download.kde.org/unstable/koffice-1.9.93/src/koffice-1.9.93.tar.bz2">koffice-1.9.93.tar.bz2</a></td>
+    </tr>
+    <tr>
+      <td>diffs between alpha2 and alpha3</td>
+      <td><a href="http://download.kde.org/unstable/koffice-1.9.93/src/koffice-1.9.92-1.9.93.tar.xdelta">koffice-1.9.92-1.9.93.tar.xdelta</a></td>
+    </tr>
+   </table>
+  </li>
+  </ul>
+
+<u><a name="binary">Binary packages</a></u>
+
+<p>
+  Some Linux/UNIX OS vendors have kindly provided binary packages of
+  KOffice 1.93 for some versions of their distribution, and in other cases
+  community volunteers have done so.
+</p>
+
+<p>
+  Currently pre-compiled packages are available for:
+</p>
+
+<ul>
+<!-- li>
+ <a href="http://kubuntu.org/">Kubuntu</a>
+    <ul type="disc">
+      <li>
+         <a href="http://kubuntu.org/announcements/kde4-beta2.php">Packages for 7.04 (Feisty) and Gutsy</a>
+      </li>
+    </ul>
+  <p />
+</li>
+<li>
+ <a href="http://www.opensuse.org/">openSUSE</a>
+    <ul type="disc">
+      <li>
+         <a href="http://en.opensuse.org/KDE4">Packages</a>
+      </li>
+      <li>
+         <a href="http://home.kde.org/~binner/kde-four-live/">openSUSE based KDE4 Live CD</a>
+      </li>
+    </ul>
+  <p />
+</li>
+<li>
+ <a href="http://www.mandriva.com/">Mandriva</a>
+    <ul type="disc">
+      <li>
+         2007: <a href="http://download.kde.org/binarydownload.html?url=/unstable/3.93/Mandriva/">Intel i386</a>
+      </li>
+    </ul>
+  <p />
+</li -->
+</ul>
+
+<?php include("footer.inc"); ?>
