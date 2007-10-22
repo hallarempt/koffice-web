b3c0e518f8d599c60797b5e108df66a7bc5a73b1
rempt <rempt@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2007-10-22 18:55:44 +0000

    Change in hotel
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@728224 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- sprints/spitnpolish/travel.php
+++ sprints/spitnpolish/travel.php
@@ -13,12 +13,9 @@ Germany
 </p>
 <h3>Accommodation</h3>
 <p>
-Motel One Berlin-Mitte<br />
-Prinzenstraße 40 / Moritzplatz<br />
-10969 Berlin<br />
-Tel.: +49/30/70 07 98 00<br />
-Fax: +49/30/70 07 98 01<br />
-<a href="mailto:berlin-mitte@motel-one.com">berlin-mitte@motel-one.com</a>
+<a href="http://www.ibishotel.com/ibis/fichehotel/gb/ibi/3108/fiche_hotel.shtml">Ibis Berlin Ostbahnhof</a><br />
+An der Schillingbrucke 2 - 10243 - BERLIN - GERMANY<br />
+Tel : (+49)30/257600 Fax : (+49)30/25760333</p>
 </p>
 <p>
 <h3>Directions &amp; Maps</h3>
@@ -26,6 +23,7 @@ Fax: +49/30/70 07 98 01<br />
 <a href="motel.jpg">Click here</a> for a map of the general area surrounding the motel and offices.
 </p>
 <p>
+<!--
 <strong>Tegel Airport to Motel:</strong> From the airport, take the number 128 bus to the U Osloer Str underground station (a map of where the airport's buses are located can be found <a href="http://www.berlin-airport.de/DE/ReisendeUndBesucher/AnUndAbreise/AnfahrtBusUndBahn/TXL/OeffentlicheTXL.html">here</a>). Here you take the U8 to Hermannstr. These trains arrive every five to ten minutes, and the journey to the U Moritzplatz station from there is approximately fifteen minutes. From there, it is roughly a fifty metre walk to the Motel One Berlin-Mitte.
 </p>
 <p>
@@ -44,5 +42,5 @@ Fax: +49/30/70 07 98 01<br />
  2545 EV The Hague<br /> 
  The Netherlands<br />
 along with the original receipts by snail mail.</p>
-
+-->
 <?php include("footer.inc"); ?>
