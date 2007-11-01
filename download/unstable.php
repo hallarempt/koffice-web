d585d805c96d71369bda567ec562d17ff168db06
pmurdoch <pmurdoch@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2007-11-01 11:23:37 +0000

    updating to reflect v2.0 building
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@731644 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- download/unstable.php
+++ download/unstable.php
@@ -5,49 +5,26 @@
   include("header.inc");
 ?>
 
-<p><b>Note</b>: <i>trunk</i> is now for the <i>Qt4/KDE4</i> port and does not compile right now. If you want to work on that, please send a mail to the koffice-devel mailing list.</p>
+    <p><b>Note</b>: <i>trunk</i> is for KDE4 and therefore requires the latest working development copy of KDE4. For information on how to set up a KDE4 development environment, see the KDE TechBase <a href=http://techbase.kde.org/Getting_Started/Build/KDE4">Building KDE4</a> page.</p>
 
-<p>KOffice unstable is the bugfix branch 1.6. You might want to get it in order to test it or to develop on it or to write docs. This is for advanced users.
+<p>KOffice unstable is in the <i>trunk</i> branch. You might want to get it in order to test it, develop or to write docs. This is for advanced users.
 </p>
-<p>For KOffice <?php echo $currentversion; ?> from svn you additionally need:
-<a href="ftp://ftp.gnu.org/pub/gnu/automake/">Automake 1.6.1</a> and
-<a href="ftp://ftp.gnu.org/pub/gnu/autoconf/">Autoconf 2.53</a>.</p>
-<p>You need qt >3.3.0 + headers (devel or dev packages if you use qt from your distribution), kdelibs >= 3.4.0 + headers (devel or dev packages if you use kdelibs from your distribution), arts >=1.4
-</p>
-<p><tt>svn</tt> is in the subversion package from your distribution.</p>
-<p>Type <tt>configure --help</tt> if you want to use configure options.</p>
-<p><b>Note</b>: after you finish <tt>./configure</tt>, look at the output and install all missing packages (+ devel or dev)  mentioned and re-run  <tt>./configure</tt>.</p>
-<p>Please refer to <a href="http://mail.kde.org/pipermail/kexi/2005-October/000025.html">http://mail.kde.org/pipermail/kexi/2005-October/000025.html</a> to know more about setting some environment variable in order not to crush your existing KOffice installation and to <a href="http://developer.kde.org/documentation/other/developer-faq.html#q2.18.2">http://developer.kde.org/documentation/other/developer-faq.html#q2.18.2</a>.</p>
-
+<p>Also, note that some distributions such as Ubuntu come with unstable KOffice packages in the repositories, which is useful if you only wish to test or write documentation. Please see your distribution's documentation for more details.</p>
+    
+    <p>Providing you have set up your environment as outlined in <a href="http://techbase.kde.org/Getting_Started/Build/KDE4">Getting Started/Build KDE4</a>, you should be able to build KOffice trunk using the following commands:</p>
 <pre>
-    $ svn co svn://anonsvn.kde.org/home/kde/branches/koffice/1.6/koffice/
+    $ svn co svn://anonsvn.kde.org/home/kde/trunk/koffice
     $ cd koffice
-    $ make -f Makefile.cvs
-    $ ./configure 
-    $ make
-    $ su -c 'make install'
+    $ cmakekde
 </pre>
 
-<i>To maintain your copy up-to-date</i>, you will do:
+<i>To keep your local copy up-to-date</i>, you must perform:
 
 <pre>
     $ cd koffice
     $ svn up
-    $ make
-    $ su -c 'make install'
+    $ cmakekde
 </pre>
-
-After that, if you get some error message in the make stage, rerun:
-<pre>
-    $ make -f Makefile.cvs
-    $ ./configure 
-    $ make
-    $ su -c 'make install'
-</pre>
-
-
-
-
 
 <br />
 <br />
