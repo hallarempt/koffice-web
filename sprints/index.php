82a5a2f0cc21b41d754d2845d9dda2ddef0fe607
zachmann <zachmann@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2008-01-11 20:05:24 +0000

    o fix the tag
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@760046 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- sprints/index.php
+++ sprints/index.php
@@ -9,5 +9,5 @@ In order to further the development of KOffice and increase both functionality a
 </p>
 <h3>Current sprint: <a href="spitnpolish/index.php">KOffice 2 Spit'n'Polish</a></h3>
 <h4>Past sprints</h4>
-<h3<a href="odf1/index.php">ODF Hackathon</a></h3>
+<h3><a href="odf1/index.php">ODF Hackathon</a></h3>
 <?php include("footer.inc"); ?>
