310385e7fbfed367fdf69d933e59ebfa8bbc5723
rempt <rempt@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2008-04-17 18:52:53 +0000

    Initial checkin, no links yet
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@798206 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ artwork/index.php
@@ -0,0 +1,21 @@
+<?php
+  $site_root = "./";
+  $site_menus = 2;
+  include("koffice.inc");
+  include("header.inc");
+?>
+<div style="margin: 20px; font-family: 'courier new',arial; font-size: 12px; color: #AAA;line-height: 20px;">
+<a href="koffice.svg"><img style="margin: 10px;" src="koffice.png"></a><br />
+<a href="kword.svg"><img style="margin: 10px;" src="kword.png"></a><br />
+<a href="kspread.png"><img style="margin: 10px;" src="kspread.png"></a><br />
+<a href="kpresenter.svg"><img style="margin: 10px;" src="kpresenter.png"></a><br />
+<a href="kexi.svg"><img style="margin: 10px;" src="kexi.png"></a><br />
+<a href="kivio.svg"><img style="margin: 10px;" src="kivio.png"></a><br />
+<a href="karbon14.svg"><img style="margin: 10px;" src="karbon14.png"></a><br />
+<a href="krita.svg"><img style="margin: 10px;" src="krita.png"></a><br />
+<a href="kplato.svg"><img style="margin: 10px;" src="kplato.png"></a><br />
+<a href="kchart.svg"><img style="margin: 10px;" src="kchart.png"></a><br />
+<a hrefe="kforumula.svg"><img style="margin: 10px;" src="kformula.png"></a><br />
+<!--img style="margin: 10px;" src="kugar.png"><br /-->
+
+</div>
