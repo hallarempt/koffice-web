0edcaa2aa2e6c4ae61122e1e7e06fa2bd0fb737b
staniek <staniek@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2008-04-28 19:36:27 +0000

    SVN_SILENT typo
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@802194 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- artwork/logos/index.php
+++ artwork/logos/index.php
@@ -5,17 +5,17 @@
   include("header.inc");
 ?>
 <div style="font-family: 'courier new',arial; font-size: 12px; color: #AAA;line-height: 20px; text-align: center;">
-<a href="koffice.svg"><img style="paddding: 2em;" src="koffice.png"></a><br />
-<a href="kword.svg"><img style="paddding: 2em;" src="kword.png"></a><br />
-<a href="kspread.png"><img style="paddding: 2em;" src="kspread.png"></a><br />
-<a href="kpresenter.svg"><img style="paddding: 2em;" src="kpresenter.png"></a><br />
-<a href="kexi.svg"><img style="paddding: 2em;" src="kexi.png"></a><br />
-<a href="kivio.svg"><img style="paddding: 2em;" src="kivio.png"></a><br />
-<a href="karbon14.svg"><img style="paddding: 2em;" src="karbon14.png"></a><br />
-<a href="krita.svg"><img style="paddding: 2em;" src="krita.png"></a><br />
-<a href="kplato.svg"><img style="paddding: 2em;" src="kplato.png"></a><br />
-<a href="kchart.svg"><img style="paddding: 2em;" src="kchart.png"></a><br />
-<a href="kforumula.svg"><img style="paddding: 2em;" src="kformula.png"></a><br />
-<!--img style="paddding: 2em;" src="kugar.png"><br /-->
+<a href="koffice.svg"><img style="padding: 2em;" src="koffice.png"></a><br />
+<a href="kword.svg"><img style="padding: 2em;" src="kword.png"></a><br />
+<a href="kspread.png"><img style="padding: 2em;" src="kspread.png"></a><br />
+<a href="kpresenter.svg"><img style="padding: 2em;" src="kpresenter.png"></a><br />
+<a href="kexi.svg"><img style="padding: 2em;" src="kexi.png"></a><br />
+<a href="kivio.svg"><img style="padding: 2em;" src="kivio.png"></a><br />
+<a href="karbon14.svg"><img style="padding: 2em;" src="karbon14.png"></a><br />
+<a href="krita.svg"><img style="padding: 2em;" src="krita.png"></a><br />
+<a href="kplato.svg"><img style="padding: 2em;" src="kplato.png"></a><br />
+<a href="kchart.svg"><img style="padding: 2em;" src="kchart.png"></a><br />
+<a href="kforumula.svg"><img style="padding: 2em;" src="kformula.png"></a><br />
+<!--img style="padding: 2em;" src="kugar.png"><br /-->
 
 </div>
