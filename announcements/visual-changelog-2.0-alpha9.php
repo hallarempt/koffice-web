a6633f66a3994db1355adef1ef27a60812b851e5
berger <berger@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2008-07-12 15:36:37 +0000

    * fix the php (hopefully) * add a screenshot for the paragraph tool
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@831385 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- announcements/visual-changelog-2.0-alpha9.php
+++ announcements/visual-changelog-2.0-alpha9.php
@@ -11,14 +11,21 @@ happen since the release ofKOffice 2.0 Alpha 8.
 </p>
 
 <?php
+
+$generalGallery = new ImageGallery("General");
+$generalGallery->addImage("pics/2008_07_16_kotext_paragraph_tool_sm.png", "pics/2008_07_16_kotext_paragraph_tool.png", "200","121", "[Screenshot]", 0, "An experimental paragraph tool for visual editing of the layout of a paragraph.");
+$generalGallery->show();
+
+
 $kexiGallery = new ImageGallery("Kexi");
 $kexiGallery->addImage("pics/2008_07_16_kexi_to_kspread_sm.png", "pics/2008_07_16_kexi_to_kspread.png", "200","125", "[Screenshot]", 0, "The report plugins of kexi can export to a spread sheet file that is then loaded in KSpread");
 $kexiGallery->addImage("pics/2008_07_16_kexi_to_html_sm.png", "pics/2008_07_16_kexi_to_html.png", "200", "125", "[Screenshot]", 0, "The report plugins of kexi can export to HTML.");
-$kexiGallery->show()
+$kexiGallery->show();
 
 $kritaGallery = new ImageGallery("Krita");
 $kritaGallery->addImage("pics/2008_07_16_krita_shapelayer_with_filter_layer_sm.png", "pics/2008_07_16_krita_shapelayer_with_filter_layer.png", "200", "119", "[Screenshot]", 0, "In Krita, it is possible to have a filter layer on top of a vector layer, this give access to a wide range of effects on vectors.");
 $kritaGallery->addImage("pics/2008_07_16_krita_sumi-e_sm.png", "pics/2008_07_16_krita_sumi-e.png", "200", "121", "[Screenshot]", 0, "The new sumi-e painting operation.");
+$kritaGallery->show();
 
 ?>
 
