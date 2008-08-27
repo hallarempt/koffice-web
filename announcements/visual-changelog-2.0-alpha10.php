8c9e881f68e4c6d0f2e5dffde76b14f3c75f22dc
berger <berger@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2008-08-27 19:02:11 +0000

    add visual changelog for Alpha 10
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@853462 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ announcements/visual-changelog-2.0-alpha10.php
@@ -0,0 +1,63 @@
+<?php
+  $page_title ='KOffice 2.0 Alpha 9 Visual Changelog';
+  $site_root = '../';
+  include("koffice.inc");
+  include("header.inc");
+  ?>
+
+<p>
+The following screenshots allow to visualize some of the major changes that have
+happen since the release ofKOffice 2.0 Alpha 8.
+</p>
+
+<ul>
+<li><a href="#koffice_libs">KOffice Libraries</a></li>
+<li><a href="#kword">KWord</a></li>
+<!-- <li><a href="#kspread">KSpread</a></li> -->
+<li><a href="#kpresenter">KPresenter</a></li>
+<!-- <li><a href="#kexi">Kexi</a></li> -->
+<!-- <li><a href="#kplato">KPlato</a></li> -->
+<li><a href="#krita">Krita</a></li>
+<li><a href="#karbon">Karbon</a></li>
+</ul>
+
+<h3 id="koffice_libs">KOffice Libraries</h3>
+<?php
+
+$generalGallery = new ImageGallery("KOffice Libraries");
+$generalGallery->addImage("pics/2008_08_26_shape_styles_docker_sm.png", "pics/2008_08_26_shape_styles_docker.png", "200","157", "[Screenshot]", 0, "The shape styles docker is available in all applications.");
+$generalGallery->show();
+?>
+
+<h3 id="kword">KWord</h3>
+<?
+$kwordGallery = new ImageGallery("KWord");
+$kwordGallery->addImage("pics/2008_08_26_styles_treeview_sm.png", "pics/2008_08_26_styles_treeview.png", "200", "108", "[Screenshot]", 0, "Paragraph styles docker.");
+$kwordGallery->show();
+?>
+
+<h3 id="kpresenter">KPresenter</h3>
+<?
+$kpresenterGallery = new ImageGallery("KPresenter");
+$kpresenterGallery->addImage("pics/2008_08_26_presenter_view_main_sm.png", "pics/2008_08_26_presenter_view_main.png", "200", "150", "[Screenshot]", 0, "Presenter view with notes.");
+$kpresenterGallery->addImage("pics/2008_08_26_presenter_view_slides_sm.png", "pics/2008_08_26_presenter_view_slides.png", "200", "150", "[Screenshot]", 0, "Presenter view with list of slides.");
+
+
+$kpresenterGallery->show();
+?>
+
+
+<h3 id="krita">Krita</h3>
+<?
+$kritaGallery = new ImageGallery("Krita");
+$kritaGallery->addImage("pics/2008_08_26_sumi-e_coolness_sm.jpg", "pics/2008_08_26_sumi-e_coolness.jpg", "200", "150", "[Screenshot]", 0, "Sumi-e coolness.");
+$kritaGallery->show();
+
+?>
+
+<h3 id="karbon">Karbon</h3>
+<?
+$karbonGallery = new ImageGallery("Karbon");
+$karbonGallery->addImage("pics/2008_08_26_karbon_page_layout_sm.png", "pics/2008_08_26_karbon_page_layout.png", "200", "121", "[Screenshot]", 0, "Page layout dialog.");
+$karbonGallery->show();
+?>
