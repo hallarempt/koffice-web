3069c23e7b934eac6baf58ff452a69417ecf6491
jsimon <jsimon@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2008-09-23 15:01:59 +0000

    Next KOffice sprint site, attempt 2: Yes, I know. That's what happens if lazy people like me simply copy a directory, without removing the .svn subfolder ;-)
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@863936 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ sprints/releaseprep/participants.php
@@ -0,0 +1,62 @@
+<?php
+  $page_title='KOffice 2.0 Release Preparation Sprint - Participants';
+  $site_root='../../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+<p>
+The following people are participating in the ODF Weekend:
+</p>
+<table border="1" width="75%">
+  <tr>
+    <th colspan="3">Maintainers</th>
+  </tr>
+  <tr>
+    <th>Name</th>
+    <th>Representing</th>
+    <th>IRC Nickname</th>
+  </tr>
+  <tr>
+    <td>Johannes Simon</td>
+    <td>KChart</td>
+    <td>josim</td>
+  </tr>
+</table>
+<br />
+<table border="1" width="75%">
+  <tr>
+    <th colspan="3">Other Developers</th>
+  </tr>
+  <tr>
+    <th>Name</th>
+    <th>Representing</th>
+    <th>IRC Nickname</th>
+  </tr>
+  <tr>
+    <td>Inge Wallin</td>
+    <td>KChart</td>
+    <td>ingwa</td>
+  </tr>
+</table>
+<br />
+<table border="1" width="75%">
+  <tr>
+    <th colspan="3">Misc. Participants</th>
+  </tr>
+  <tr>
+    <th>Name</th>
+    <th>Representing</th>
+    <th>IRC Nickname</th>
+  </tr>
+  <tr>
+    <td>Inge Wallin</td>
+    <td>Event organiser</td>
+    <td>ingwa</td>
+  </tr>
+  <tr>
+    <td>Johannes Simon</td>
+    <td>Event organiser</td>
+    <td>josim</th>
+  </tr>
+</table>
+<?php include("footer.inc"); ?>
--- sprints/spitnpolish/participants.php
+++ sprints/spitnpolish/participants.php
@@ -1,5 +1,5 @@
 <?php
-  $page_title='KOffice 2.0 Release Preparation Sprint - Participants';
+  $page_title='KOffice ODF Weekend - Participants';
   $site_root='../../';
   include("koffice.inc");
   include("header.inc");
@@ -17,10 +17,35 @@ The following people are participating in the ODF Weekend:
     <th>IRC Nickname</th>
   </tr>
   <tr>
-    <td>Johannes Simon</td>
-    <td>KChart</td>
-    <td>josim</td>
+    <td>Thorsten Zachmann</td>
+    <td>KPresenter, Kivio</td>
+    <td>Zagge</td>
   </tr>
+  <tr>
+    <td>Jan Hambrecht</td>
+    <td>Karbon</td>
+    <td>jaham</td>
+  </tr>
+  <tr>
+    <td>Boudewijn Rempt</td>
+    <td>Krita</td>
+    <td>boud</td>
+  </tr>
+  <tr>
+    <td>Inge Wallin</td>
+    <td>KChart</td>
+    <td>ingwa</td>
+  </tr>
+  <tr>
+    <td>Thomas Zander</td>
+    <td>KWord</td>
+    <td>ThomasZ</td>
+  </tr>
+  <tr>
+    <td>Jaroslaw Staniek</td>
+    <td>Kexi</td>
+    <td>JStaniek</td>
+  <tr>
 </table>
 <br />
 <table border="1" width="75%">
@@ -33,9 +58,24 @@ The following people are participating in the ODF Weekend:
     <th>IRC Nickname</th>
   </tr>
   <tr>
-    <td>Inge Wallin</td>
-    <td>KChart</td>
-    <td>ingwa</td>
+    <td>Sebastian Sauer</td>
+    <td>Kross</td>
+    <td>dipesh</td>
+  </tr>
+  <tr>
+    <td>Pierre Ducroquet</td>
+    <td>KWord</td>
+    <td>Pinaraf</td>
+  </tr>
+  <tr>
+   <td>Marijn Kruisselbrink</td>
+   <td>Music Flake, OS X Guru</td>
+   <td>Mek</td>
+  </tr>
+  <tr>
+    <td>Sven Langkamp</td>
+    <td>Krita, Karbon, Create infrastructure</td>
+    <td>Slangkamp</td>
   </tr>
 </table>
 <br />
@@ -46,17 +86,12 @@ The following people are participating in the ODF Weekend:
   <tr>
     <th>Name</th>
     <th>Representing</th>
-    <th>IRC Nickname</th>
+    <td>IRC Nickname</td>
   </tr>
   <tr>
     <td>Inge Wallin</td>
     <td>Event organiser</td>
     <td>ingwa</td>
-  </tr>
-  <tr>
-    <td>Johannes Simon</td>
-    <td>Event organiser</td>
-    <td>josim</th>
   </tr>
 </table>
 <?php include("footer.inc"); ?>
