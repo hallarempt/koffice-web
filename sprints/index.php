1f6f1aaba094abdff27be08af8e0e4edf6ffc2d7
jsimon <jsimon@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2008-10-01 19:24:47 +0000

    Link web page for new sprint
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@866728 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- sprints/index.php
+++ sprints/index.php
@@ -7,7 +7,8 @@
 <p>
 In order to further the development of KOffice and increase both functionality and usability, the developers of KOffice hold sporadic meetings known as "sprints", and this is where you can find out the who, why and where of them.
 </p>
-<h3>Current sprint: <a href="spitnpolish/index.php">KOffice 2 Spit'n'Polish</a></h3>
+<h3>Current sprint: <a href="releaseprep/index.php">KOffice 2 Release Preparation</a></h3>
 <h4>Past sprints</h4>
+<h3><a href="spitnpolish/index.php">KOffice 2 Spit'n'Polish</a></h3>
 <h3><a href="odf1/index.php">ODF Hackathon</a></h3>
 <?php include("footer.inc"); ?>
