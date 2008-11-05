8450117cec73ae686483b38e41ba8c40541fc850
jsimon <jsimon@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2008-11-05 20:58:06 +0000

    Update contact info
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@880558 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- sprints/releaseprep/contact.php
+++ sprints/releaseprep/contact.php
@@ -12,7 +12,9 @@ The following information can be used to contact the event organisers.
 Email: <a href="mailto:inge@lysator.liu.se">inge@lysator.liu.se</a><br />
 Tel (Mobile): +46 707 26 08 53
 </p>
-<h4>KDAB Offices</h4>
-Tel: +49 (0)30 521 325 470
-<em>Extension <b>72</b> will reach Till Adam</em>
+<h4>Johannes Simon</h4>
+<p>
+Email: <a href="mailto:johannes@kdab.net">johannes@kdab.net</a><br />
+Tel (Mobile): +49 163 308 4357
+</p>
 <?php include("footer.inc"); ?>
