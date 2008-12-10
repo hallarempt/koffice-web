43e04e52ea33ef1370459a86a5adf06331687cab
berger <berger@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2008-12-10 21:03:25 +0000

    add release notes
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@895484 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ releases/2.0beta4-release.php
@@ -0,0 +1,81 @@
+<?php
+  $page_title='KOffice 2.0 Beta 3 Release Notes';
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+<p align="justify">
+  DATELINE DECEMBER 10, 2008<br />
+  FOR IMMEDIATE RELEASE
+</p>
+
+  <p>The KOffice team is proud to announce the fourth KOffice beta for the upcoming 2.0 release.</p>
+
+  <h2>Announcement</h2>
+  <ul>
+  <li><a href="/announcements/announce-2.0beta4.php">Announcement</a></li>
+  </ul>
+    
+  <h2>Changelog</h2>
+  <ul>
+  <li><a href="/announcements/changelog-2.0-beta4.php">Changelog</a></li>
+  </ul>
+
+<!--
+  <h2>Visual changelog</h2>
+  <ul>
+  <li><a href="/announcements/visual-changelog-2.0-beta3.php">Visual changelog</a></li>
+  </ul>
+-->
+  <h2>Download</h2>
+  <ul>
+    <li>Source code of KOffice 2.0-beta4:
+      <table border="0" cellpadding="4" cellspacing="0">
+      <tr>
+        <th align="left">Description</th>
+        <th align="left">Location</th>
+      <tr>
+        <td>KOffice 2.0-beta-4 (source code)</td>
+        <td><a href="http://download.kde.org/unstable/koffice-1.9.98.3/src/koffice-1.9.98.3.tar.bz2">koffice-1.9.98.3.tar.bz2</a></td>
+    </tr>
+    <tr>
+    <td>KAtelier 2.0-beta-4 (source code, with just Krita and Karbon)</td>
+    <td><a href="http://download.kde.org/unstable/koffice-1.9.98.3/src/katelier-1.9.98.3.tar.bz2">katelier-1.9.98.3.tar.bz2</a></td>
+    </tr>
+    </table>
+  </li>
+  </ul>
+
+<u><a name="binary">Binary packages</a></u>
+
+<p>
+  Some Linux/UNIX OS vendors have kindly provided binary packages of
+  KOffice 1.9.98.3 for some versions of their distribution, and in other cases
+  community volunteers have done so.
+</p>
+
+
+<p>
+  Currently pre-compiled packages are available for:
+</p>
+
+<ul>
+<!--
+<li>Windows <a rel="nofollow" href="http://winkde.org/pub/kde/ports/win32/installer/">installer</a> </li>
+<li>Mac OS X <a href="http://mac.kde.org/?id=download">installer</a></li>-->
+<li>Debian packages are available in the <i>experimental</i> repository.</li>
+<!--<li>
+ <a rel="nofollow" href="http://kubuntu.org/">Kubuntu</a>
+    <ul type="disc">
+      <li>
+      <a rel="nofollow" href="http://www.kubuntu.org/news/koffice-2-beta-4">Packages for Intrepid</a>
+      </li>
+    </ul>
+  <p />
+</li>-->
+<li>openSUSE packages are available for 10.3, 11.0 and Factory from the openSUSE
+    Build Service within KDE:KDE4:Factory:Extra-Apps repo.
+</li>
+</ul>
+
+<?php include("footer.inc"); ?>
