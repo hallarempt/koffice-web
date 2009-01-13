4018d710c21582f15ff96458072c197328522078
ingwa <ingwa@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2009-01-13 17:20:34 +0000

    Skeleton for changelog
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@910579 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ announcements/changelog-2.0-beta5.php
@@ -0,0 +1,95 @@
+<?php
+  $page_title ='KOffice 2.0 Beta 4 Changelog';
+  $site_root = '../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+<p>
+  The following summarizes the major changes to the KOffice Suite since the
+  release of KOffice 2.0 Beta 4.
+</p>
+<ul>
+<li><a href="#koffice_general">General</li>
+<li><a href="#koffice_libs">KOffice Libraries</a></li>
+<li><a href="#kword">KWord</a></li>
+<li><a href="#kspread">KSpread</a></li>
+<li><a href="#kpresenter">KPresenter</a></li>
+<!-- <li><a href="#kexi">Kexi</a></li> -->
+<!-- <li><a href="#kchart">KChart</a></li> -->
+<li><a href="#kplato">KPlato</a></li>
+<li><a href="#krita">Krita</a></li>
+<li><a href="#karbon">Karbon</a></li>
+<li><a href="#plugins">Plugins</a></li>
+</ul>
+
+<h3 id="koffice_general">General</h3>
+<ul>
+  <li>First change.</li>
+</ul>
+
+<h3 id="koffice_libs">KOffice Libraries</h3>
+<ul>
+  <li>First change.</li>
+  <li>Second change..</li>
+  <li><em>KoPageApp</em>
+    <ul>
+      <li>foo.</li>
+    </ul>
+  </li>
+  <li><em>Flake</em>
+    <ul>
+      <li>bar</li>
+    </ul>
+  </li>
+</ul>
+
+<h3 id="kword">KWord</h3>
+<ul>
+  <li>Fix numero uno</li>
+</ul>
+
+<h3 id="kspread">KSpread</h3>
+<ul>
+  <li>fuuuuum.</li>
+</ul>
+
+<h3 id="kpresenter">KPresenter</h3>
+<ul>
+  <li>Fix is described here.</li>
+</ul>
+
+<!--<h3 id="kchart">KChart</h3>
+<ul>
+  <li></li>
+</ul>-->
+
+<h3 id="kplato">KPlato</h3>
+<ul>
+  <li>Fix!!1!lol.</li>
+</ul>
+
+<h3 id="krita">Krita</h3>
+<ul>
+  <li>Krita can haz fixessss.</li>
+</ul>
+
+<h3 id="karbon">Karbon</h3>
+<ul>
+  <li>El Karbon est la fixed.</li>
+</ul>
+
+<h3 id="plugins">Plug-ins</h3>
+<ul>
+  <li>Fixxx.</li>
+  <li><em>Text Shape</em>
+    <ul>
+      <li>Die Fixen, ja?.</li>
+    </ul>
+  </li>
+</ul>
+
+
+
+<?php
+  include("footer.inc");
+?>
