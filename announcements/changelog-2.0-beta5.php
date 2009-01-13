227e1bc8955b47e93cf1e75498f92243ef18bf28
ingwa <ingwa@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2009-01-13 18:28:35 +0000

    Add fixed issues for Krita
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@910592 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- announcements/changelog-2.0-beta5.php
+++ announcements/changelog-2.0-beta5.php
@@ -70,7 +70,31 @@
 
 <h3 id="krita">Krita</h3>
 <ul>
-  <li>Krita can haz fixessss.</li>
+  <li>Fixes to the convolution code (which helps with many filters)</li>
+  <li>Show a preview of the current brush stroke definition</li>
+  <li>Fix handling of buildup and wash modes (and other settings in brush presets)</li>
+  <li>Improvements to flatten image</li>
+  <li>Fix cloning of image hose brushes (which unfortunately showed up another bug that will be fixed in the next beta)</li>
+  <li>Do not use shaders if use of shaders is disabled, not even for HDR preview (fixes a crash on intel gma graphics)</li>
+  <li>Fix merging of layers</li>
+  <li>Fix building with newer versions of exiv2</li>
+  <li>Finish porting the chalk paintop</li>
+  <li>Fix opacity handling in the gradient dialog</li>
+  <li>Improve performance when painting freehand</li>
+  <li>Fix crash when painting with the sumi-e brush using a tablet (17755)</li>
+  <li>Make the cubism filter work in any colorspace</li>
+  <li>Fix crash on autosave (<a href="http://bugs.kde.org/178235">178235</a>)</li>
+  <li>Fix saving and loading in .kra file format: everything is now saved and loaded except for metadata and vector selections. Unfortunately, there is still a bug when loading vector layers.</li>
+  <li>Fix display of layers with a transparency mask</li>
+  <li>Fix startup crashes caused by a bug in the small color docker</li>
+  <li>Fix wave filter</li>
+  <li>Fix blur filter display when used in a filter mask</li>
+  <li>Fix crash on shear layer</li>
+  <li>Fixing Crash on clicking on category name (<a href="http://bugs.kde.org/177164">177164</a>)</li>
+  <li>Show progress for Scale, Rotate and Shear (<a href="http://bugs.kde.org/176808">176808</a>)</li>
+  <li>Crash on startup when compiling with -march=core2 (<a href="http://bugs.kde.org/177133">177133</a>)</li>
+  <li>Fix painting on a filter mask</li>
+  <li>Fix locking/unlocking and showing/hiding masks</li>
 </ul>
 
 <h3 id="karbon">Karbon</h3>
