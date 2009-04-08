4348f42b608d53da88235c7a351c14593bc8f2fc
berger <berger@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2009-04-08 08:50:41 +0000

    add release notes
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@950952 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ releases/2.0rc1-release.php
@@ -0,0 +1,84 @@
+<?php
+  $page_title='KOffice 2.0 RC1 Release Notes';
+  $site_root='../';
+  include("koffice.inc");
+  include("header.inc");
+?>
+<p align="justify">
+  DATELINE APRIL 08, 2009<br />
+  FOR IMMEDIATE RELEASE
+</p>
+
+  <p>The KOffice team is proud to announce the first KOffice Release Candidate for the upcoming 2.0 release.</p>
+
+  <h2>Announcement</h2>
+  <ul>
+  <li><a href="/announcements/announce-2.0rc1.php">Announcement</a></li>
+  </ul>
+    
+  <h2>Changelog</h2>
+  <ul>
+  <li><a href="/announcements/changelog-2.0-rc1.php">Changelog</a></li>
+  </ul>
+
+<!--
+  <h2>Visual changelog</h2>
+  <ul>
+  <li><a href="/announcements/visual-changelog-2.0-beta4.php">Visual changelog</a></li>
+  </ul>
+-->
+  <h2>Download</h2>
+  <ul>
+    <li>Source code of KOffice 2.0-rc1:
+      <table border="0" cellpadding="5" cellspacing="0">
+      <tr>
+        <th align="left">Description</th>
+        <th align="left">Location</th>
+      <tr>
+        <td>KOffice 2.0-RC-1 (source code)</td>
+        <td><a href="http://download.kde.org/unstable/koffice-1.9.99.0/src/koffice-1.9.99.0.tar.bz2">koffice-1.9.99.0.tar.bz2</a></td>
+    </tr>
+    <tr>
+    <td>KAtelier 2.0-RC-1 (source code, with just Krita and Karbon)</td>
+    <td><a href="http://download.kde.org/unstable/koffice-1.9.99.0/src/katelier-1.9.99.0.tar.bz2">katelier-1.9.99.0.tar.bz2</a></td>
+    </tr>
+    </table>
+  </li>
+  </ul>
+
+<u><a name="binary">Binary packages</a></u>
+
+<p>
+  Some Linux/UNIX OS vendors have kindly provided binary packages of
+  KOffice 1.9.99.0 for some versions of their distribution, and in other cases
+  community volunteers have done so.
+</p>
+
+<p>
+  Currently pre-compiled packages are available for (this list is not exhaustive, and is only based on information that was provided to us):
+</p>
+
+<ul>
+<!--
+<li>Windows <a rel="nofollow" href="http://winkde.org/pub/kde/ports/win32/installer/">installer</a> </li>
+-->
+<li>Mac OS X via <a href="http://trac.macports.org/changeset/47839">macports</a></li>
+<li>Debian packages are available in the <i>experimental</i> repository.</li>
+<!--<li>
+ <a rel="nofollow" href="http://kubuntu.org/">Kubuntu</a>
+    <ul type="disc">
+      <li>
+      <a rel="nofollow" href="http://www.kubuntu.org/news/koffice-2-beta-6">Packages for Intrepid</a>
+      </li>
+    </ul>
+  <p />
+</li>-->
+<li>openSUSE packages are available for 10.3, 11.0, 11.1 and Factory from the openSUSE
+    Build Service within <a href="http://download.opensuse.org/repositories/KDE:/KDE4:/Factory:/Extra-Apps/">KDE:KDE4:Factory:Extra-Apps</a> repo.
+</li>
+<li>PLD linux packages are available in the <i>test</i> repository (as well as in a <a href="http://livecd.pld-linux.pl">LiveCD</a>).</li>
+<!-- <li>Binaries for the Nokia n810 Internet Tablet has been provided by <a href="http://www.kdedevelopers.org/node/3879">Marijn Kruisselbrink</a>. -->
+</li>
+</ul>
+
+<?php include("footer.inc"); ?>
