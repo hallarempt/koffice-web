9e3554a2e49d339dc72a0434953bc5dd3878326f
berger <berger@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2009-04-08 09:02:14 +0000

    beta7 is 1.9.98.7
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/www/areas/koffice@950957 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- announcements/announce-2.0beta7.php
+++ announcements/announce-2.0beta7.php
@@ -188,7 +188,7 @@ and earlier announcements or in the <a href="http://www.koffice.org/releases/2.0
 <p align="justify">
   KOffice and all its components (including KDE) are available
   <em><strong>for free</strong></em> under Open Source licenses from the KDE
-  <a href="http://download.kde.org/unstable/koffice-1.9.98.6/src/">http and ftp</a> mirrors.
+  <a href="http://download.kde.org/unstable/koffice-1.9.98.7/src/">http and ftp</a> mirrors.
 </p>
 <hr />
 
